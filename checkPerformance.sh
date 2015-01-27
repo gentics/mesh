4502985c539177c7eb254cb681490d207c592aa3
Johannes Schüth <j.schueth@gentics.com>
2015-01-27 17:53:39 +0100

    Add performance check script

---
+++ checkPerformance.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SID=$(curl -v --silent  http://joe1:test123@localhost:8080/api/v1/nav/get 2>&1 | grep "Set-Cookie" | sed -e 's/.*cailun\.session=\(.*\);.*/\1/')
+
+ab -n 10000 -c 15 -A joe1:test123  -C cailun.session=$SID http://localhost:8080/api/v1/nav/get
