882e1be4b13ee88f33b2b8651e8881ee93d0fed3
Johannes Schüth <git@jotschi.de>
2015-02-01 19:52:32 +0100

    Switch to handlebar template engine

--- examples/demo-java/src/main/java/com/gentics/cailun/demo/verticle/StaticContentVerticle.java
+++ examples/demo-java/src/main/java/com/gentics/cailun/demo/verticle/StaticContentVerticle.java
@@ -3,9 +3,10 @@ package com.gentics.cailun.demo.verticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.ext.apex.addons.HandlebarsTemplateEngine;
 import io.vertx.ext.apex.addons.StaticServer;
 import io.vertx.ext.apex.addons.TemplateEngine;
-import io.vertx.ext.apex.addons.ThymeleafTemplateEngine;
+import io.vertx.ext.apex.addons.TemplateHandler;
 import io.vertx.ext.apex.core.Router;
 import lombok.NoArgsConstructor;
 
@@ -17,18 +18,17 @@ public class StaticContentVerticle extends AbstractVerticle {
 
 		Router router = Router.router(vertx);
 
-		 StaticServer staticServer = StaticServer.staticServer();
-//		 staticServer.setDirectoryListing(true);
+		StaticServer staticServer = StaticServer.staticServer();
+		// staticServer.setDirectoryListing(true);
 
-		// 1. Handle static resources
-		 router.route().handler(staticServer);
-
-		TemplateEngine engine = ThymeleafTemplateEngine.create();
+		router.route("/js").handler(staticServer);
+		router.route("/img").handler(staticServer);
+		router.route("/css").handler(staticServer);
 
 		// All other requests handled by template engine
-		//router.handler(TemplateHandler.templateHandler(engine, "templates/post.html", "text/html"));
+		TemplateEngine engine = HandlebarsTemplateEngine.create();
 
-		// Example content
+		// // Example content
 		router.route("/test.html").handler(context -> {
 			context.put("cailun.page.id", "1");
 			context.put("cailun.page.title", "My title");
@@ -36,11 +36,18 @@ public class StaticContentVerticle extends AbstractVerticle {
 			context.put("cailun.page.content", "My content");
 			context.next();
 		});
-
+		router.route("/test2.html").handler(context -> {
+			context.put("cailun.page.id", "1");
+			context.put("cailun.page.title", "My title");
+			context.put("cailun.page.teaser", "My teaser");
+			context.put("cailun.page.content", "My content");
+			context.next();
+		});
+		
+		router.route().handler(TemplateHandler.templateHandler(engine, "templates/post", "text/html"));
 		HttpServer server = vertx.createHttpServer(new HttpServerOptions().setPort(8081));
 		server.requestHandler(router::accept);
 		server.listen();
 
 	}
-
 }
