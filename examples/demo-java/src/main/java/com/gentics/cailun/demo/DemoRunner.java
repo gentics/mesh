fb1e2a15fb9b9a6c020712b6e06e2e1101143939
Johannes Schüth <j.schueth@gentics.com>
2015-02-02 14:22:31 +0100

    Fix maven build errors

--- examples/demo-java/src/main/java/com/gentics/cailun/demo/DemoRunner.java
+++ examples/demo-java/src/main/java/com/gentics/cailun/demo/DemoRunner.java
@@ -1,16 +1,18 @@
 package com.gentics.cailun.demo;
 
 import static com.gentics.cailun.util.DeploymentUtils.deployAndWait;
-import io.vertx.core.Vertx;
 
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 
 import com.gentics.cailun.cli.CaiLun;
+import com.gentics.cailun.core.verticle.AuthenticationVerticle;
+import com.gentics.cailun.core.verticle.PageVerticle;
+import com.gentics.cailun.core.verticle.TagVerticle;
 import com.gentics.cailun.demo.verticle.CustomerVerticle;
-import com.gentics.cailun.demo.verticle.StaticContentVerticle;
-import com.gentics.cailun.verticle.admin.AdminGUIVerticle;
+import com.gentics.cailun.nav.NavigationVerticle;
+import com.gentics.cailun.tagcloud.TagCloudVerticle;
 
 /**
  * Main runner that is used to deploy a preconfigured set of verticles.
@@ -29,22 +31,21 @@ public class DemoRunner {
 
 		cailun.setCustomLoader((vertx) -> {
 			deployAndWait(vertx, CustomerVerticle.class);
+			deployAndWait(vertx, AuthenticationVerticle.class);
+			deployAndWait(vertx, NavigationVerticle.class);
+			deployAndWait(vertx, TagCloudVerticle.class);
+			deployAndWait(vertx, PageVerticle.class);
+			deployAndWait(vertx, TagVerticle.class);
 			//deployAndWait(vertx, StaticContentVerticle.class);
-			deployAndWait(vertx, AdminGUIVerticle.class);
+			//deployAndWait(vertx, AdminGUIVerticle.class);
 			
 		});
-//			deployAndWait(vertx, AdminVerticle.class);
-//			deployAndWait(vertx, AuthenticationVerticle.class);
 //			// DeploymentOptions options = new DeploymentOptions();
 //			// vertx.deployVerticle("service:com.gentics.vertx:cailun-rest-navigation:0.1.0-SNAPSHOT",options, dh -> {
 //			// if (dh.failed()) {
 //			// System.out.println(dh.cause());
 //			// }
 //			// });
-//			deployAndWait(vertx, NavigationVerticle.class);
-//			deployAndWait(vertx, TagCloudVerticle.class);
-//			deployAndWait(vertx, PageVerticle.class);
-//			deployAndWait(vertx, TagVerticle.class);
 //			// deployAndWait(vertx, "", "TestJSVerticle.js");
 //		});
 		cailun.run();
