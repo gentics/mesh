0fd2168e7ef97e16d740d14619c266f4eedb58ad
Johannes Schüth <j.schueth@gentics.com>
2015-02-09 13:07:20 +0100

    Remove bogus code and fix some errors in test

--- core/src/test/java/com/gentics/cailun/linkrenderer/LinkRendererTest.java
+++ core/src/test/java/com/gentics/cailun/linkrenderer/LinkRendererTest.java
@@ -6,82 +6,50 @@ import java.util.concurrent.ExecutionException;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gentics.cailun.core.link.CaiLunLinkResolver;
 import com.gentics.cailun.core.link.CaiLunLinkResolverFactoryImpl;
 import com.gentics.cailun.core.link.LinkReplacer;
-import com.gentics.cailun.core.link.LinkResolverFactory;
+import com.gentics.cailun.core.repository.PageRepository;
+import com.gentics.cailun.core.rest.model.Page;
+import com.gentics.cailun.test.Neo4jSpringTestConfiguration;
 
+@ContextConfiguration(classes = { Neo4jSpringTestConfiguration.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class LinkRendererTest {
-	String content = "some bla START<a href=\"${Page(2)}\">Test</a>   dasasdg <a href=\"${Page(3)}\">Test</a>DEN";
+
+	final String content = "some bla START<a href=\"${Page(2)}\">Test</a>   dasasdg <a href=\"${Page(3)}\">Test</a>DEN";
+
+	@Autowired
+	CaiLunLinkResolverFactoryImpl<CaiLunLinkResolver> resolverFactory;
+
+	@Autowired
+	PageRepository pageRepository;
 
 	@Test
 	public void testNodeReplace() throws IOException, InterruptedException, ExecutionException {
-		// String allContent = "";
-		// for (int i = 0; i < 100; i++) {
-		// allContent += content;
-		// }
-		LinkResolverFactory factory = new CaiLunLinkResolverFactoryImpl<CaiLunLinkResolver>();
-		LinkReplacer<CaiLunLinkResolver> replacer = new LinkReplacer(factory);
-		// for (int i = 0; i < 2000; i++) {
-		// System.out.println(allContent);
-//		String out = replacer.replace(content);
-//		System.out.println(out);
-		// }
-	}
 
-	//
-	// @Test
-	// public void testPLinkReplacer2() throws IOException {
-	// String allContent = "";
-	// for (int i = 0; i < 100; i++) {
-	// allContent += content;
-	// }
-	// for (int i = 0; i < 20000; i++) {
-	// PLinkInputStream pl = new PLinkInputStream(IOUtils.toInputStream(allContent));
-	// String out = IOUtils.toString(pl);
-	// // System.out.println(out);
-	// }
-	// }
-	//
-	// @Test
-	// public void testPLinkReplacer() throws IOException {
-	// String allContent = "";
-	// for (int i = 0; i < 100; i++) {
-	// allContent += content;
-	// }
-	// ByteArrayOutputStream bos = new ByteArrayOutputStream(allContent.length() * 2000);
-	// PLinkOutputStream os = new PLinkOutputStream(bos);
-	// for (int i = 0; i < 20000; i++) {
-	// os.write(allContent.getBytes());
-	// }
-	// os.flush();
-	// }
+		// Create some dummy content
+		Page page1 = new Page("test");
+		pageRepository.save(page1);
+		Page page2 = new Page("test2");
+		page2.setContent(content);
+		pageRepository.save(page2);
+
+		LinkReplacer<CaiLunLinkResolver> replacer = new LinkReplacer(resolverFactory);
+		String out = replacer.replace(content);
+		System.out.println(out);
+	}
 
 	private String getUrlForPage(Long id) {
 		return "/var/www/index.html";
 	}
-
-	// @Test
-	// public void testStringReplace() throws InterruptedException {
-	// String allContent = "";
-	// for (int i = 0; i < 100; i++) {
-	// allContent += content;
-	// }
-	// for (int i = 0; i < 2000; i++) {
-	//
-	// // int s = 0;
-	// // while (s != -1) {
-	// // s = content.indexOf("${", s + 1);
-	// // int e = content.indexOf("}", s);
-	// // //System.out.println(content.substring(s,e));
-	// // }
-	// for (int e = 0; e < 300; e++) {
-	// String out = StringUtils.replace(allContent, "${Page(2)}", "atom.xml");
-	// }
-	// }
-	// // System.out.println(out);
-	// }
 
 	// @Test
 	public void testRendering() throws IOException {
