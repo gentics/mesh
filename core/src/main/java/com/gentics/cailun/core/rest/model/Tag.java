5a94e4eb215266ffd1a8c2fc587c41efc291de21
Johannes Schüth <j.schueth@gentics.com>
2015-02-09 10:21:13 +0100

    Add basic test for tag objects

--- core/src/main/java/com/gentics/cailun/core/rest/model/Tag.java
+++ core/src/main/java/com/gentics/cailun/core/rest/model/Tag.java
@@ -1,5 +1,6 @@
 package com.gentics.cailun.core.rest.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import lombok.NoArgsConstructor;
@@ -11,6 +12,12 @@ import org.springframework.data.neo4j.annotation.RelatedTo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+/**
+ * A tag is a basic container for other generic nodes. It can be used as a folder that holds contents in the form of various pages.
+ * 
+ * @author johannes2
+ *
+ */
 @NodeEntity
 @NoArgsConstructor
 public class Tag extends GenericNode {
@@ -19,7 +26,7 @@ public class Tag extends GenericNode {
 
 	@Fetch
 	@RelatedTo(type = BasicRelationships.TAGGED, direction = Direction.INCOMING, elementClass = GenericNode.class)
-	private Set<GenericNode> contents;
+	private Set<GenericNode> contents = new HashSet<>();
 
 	// @Fetch
 	// @RelatedToVia(type = "HAS_PERMISSION", direction = Direction.BOTH, elementClass = Permission.class)
@@ -27,6 +34,15 @@ public class Tag extends GenericNode {
 
 	public Tag(String name) {
 		setName(name);
+	}
+
+	/**
+	 * Adds a content in the form of a generic node to this tag.
+	 * 
+	 * @param node
+	 */
+	public void addContent(GenericNode node) {
+		this.contents.add(node);
 	}
 
 	@JsonIgnore
@@ -40,6 +56,16 @@ public class Tag extends GenericNode {
 
 	public void setContents(Set<GenericNode> contents) {
 		this.contents = contents;
+	}
+
+	/**
+	 * Removes the given node from the list of contents.
+	 * 
+	 * @param node
+	 * @return true, when the node could be removed. Otherwise false.
+	 */
+	public boolean removeContent(GenericNode node) {
+		return this.contents.remove(node);
 	}
 
 }
