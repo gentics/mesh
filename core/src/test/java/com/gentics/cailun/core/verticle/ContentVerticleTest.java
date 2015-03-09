17517b2b1f9ff83da61097698018c888478eafa1
Johannes Schüth <j.schueth@gentics.com>
2015-03-09 11:11:55 +0100

    Fix content assertion

--- core/src/test/java/com/gentics/cailun/core/verticle/ContentVerticleTest.java
+++ core/src/test/java/com/gentics/cailun/core/verticle/ContentVerticleTest.java
@@ -32,39 +32,39 @@ public class ContentVerticleTest extends AbstractRestVerticleTest {
 	@Test
 	public void testCreateContentByPath() throws Exception {
 		UserInfo info = data().getUserInfo();
-		String responseJson = "{\"uuid\":\"uuid-value\",\"author\":null,\"properties\":{\"filename\":\"new-page.html\",\"name\":\"english content name\",\"content\":\"blessed mealtime again!\"},\"type\":\"content\",\"language\":\"en_US\"}";
 		String requestJson = "{\"type\": \"content\",\"properties\":{\"filename\":\"new-page.html\",\"name\":\"english content name\",\"content\":\"blessed mealtime again!\"},\"language\":\"en_US\"}";
-		String response = request(info, HttpMethod.POST, "/api/v1/" + TestDataProvider.PROJECT_NAME
-				+ "/contents/subtag/newpage.html", 200, "OK", requestJson);
-		assertEqualsSanitizedJson(responseJson, response);
+		String response = request(info, HttpMethod.POST, "/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/subtag/newpage.html", 200, "OK",
+				requestJson);
+		String responseJson = "{\"uuid\":\"uuid-value\",\"author\":null,\"properties\":{\"filename\":\"new-page.html\",\"name\":\"english content name\",\"content\":\"blessed mealtime again!\"},\"type\":\"content\",\"language\":\"en_US\"}";
+		assertEqualsSanitizedJson("The response json did not match the expected one", responseJson, response);
 	}
 
-	private void assertEqualsSanitizedJson(String expectedJson, String unsanitizedResponseJson) throws JsonGenerationException, JsonMappingException,
-			IOException {
-		ContentResponse responseObject = new ObjectMapper().readValue(unsanitizedResponseJson, ContentResponse.class);
-		assertNotNull(responseObject);
-		// Update the uuid and compare json afterwards
-		responseObject.setUuid("uuid-value");
-		String sanitizedJson = new ObjectMapper().writeValueAsString(responseObject);
-		assertEquals("The response json did not match the expected one.", expectedJson, sanitizedJson);
-	}
+	//
+	// private void assertEqualsSanitizedJson(String expectedJson, String unsanitizedResponseJson) throws JsonGenerationException, JsonMappingException,
+	// IOException {
+	// ContentResponse responseObject = new ObjectMapper().readValue(unsanitizedResponseJson, ContentResponse.class);
+	// assertNotNull(responseObject);
+	// // Update the uuid and compare json afterwards
+	// responseObject.setUuid("uuid-value");
+	// String sanitizedJson = new ObjectMapper().writeValueAsString(responseObject);
+	// assertEquals("The response json did not match the expected one.", expectedJson, sanitizedJson);
+	// }
 
 	@Test
 	public void testReadContentByValidPath() throws Exception {
 		UserInfo info = data().getUserInfo();
+		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/subtag/english.html", 200, "OK");
 		String json = "{\"uuid\":\"uuid-value\",\"author\":{\"lastname\":\"Doe\",\"firstname\":\"Joe\",\"username\":\"joe1\",\"emailAddress\":\"j.doe@gentics.com\"},\"properties\":{\"filename\":\"english.html\",\"name\":\"english content name\",\"content\":\"blessed mealtime!\"},\"type\":\"content\",\"language\":\"en_US\"}";
-		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME
-				+ "/contents/subtag/english.html", 200, "OK");
-		assertEqualsSanitizedJson(json, response);
+		assertEqualsSanitizedJson("The response json did not match the expected one", json, response);
 
 	}
 
 	@Test
 	public void testReadContentByInvalidPath() throws Exception {
 		UserInfo info = data().getUserInfo();
+		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/subtag/subtag2/no-valid-page.html",
+				404, "Not Found");
 		String json = "{\"message\":\"Content not found for path {subtag/subtag2/no-valid-page.html}\"}";
-		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME
-				+ "/contents/subtag/subtag2/no-valid-page.html", 404, "Not Found");
 		assertEquals(json, response);
 
 	}
@@ -72,9 +72,9 @@ public class ContentVerticleTest extends AbstractRestVerticleTest {
 	@Test
 	public void testReadContentByInvalidPath2() throws Exception {
 		UserInfo info = data().getUserInfo();
-		String json = "{\"message\":\"Content not found for path {subtag/subtag-no-valid-tag/no-valid-page.html}\"}";
 		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME
 				+ "/contents/subtag/subtag-no-valid-tag/no-valid-page.html", 404, "Not Found");
+		String json = "{\"message\":\"Content not found for path {subtag/subtag-no-valid-tag/no-valid-page.html}\"}";
 		assertEquals(json, response);
 
 	}
@@ -82,20 +82,18 @@ public class ContentVerticleTest extends AbstractRestVerticleTest {
 	@Test
 	public void testReadContentByUUID() throws Exception {
 		UserInfo info = data().getUserInfo();
-		String json = "{\"uuid\":\"uuid-value\",\"author\":{\"lastname\":\"Doe\",\"firstname\":\"Joe\",\"username\":\"joe1\",\"emailAddress\":\"j.doe@gentics.com\"},\"properties\":{\"filename\":\"english.html\",\"name\":\"english content name\",\"content\":\"blessed mealtime!\"},\"type\":\"content\",\"language\":\"en_US\"}";
 		Content content = data().getContentLevel1A1();
-		String response = request(info, HttpMethod.GET,
-				"/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/" + content.getUuid(), 200, "OK");
-		assertEqualsSanitizedJson(json, response);
+		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/" + content.getUuid(), 200, "OK");
+		String json = "{\"uuid\":\"uuid-value\",\"author\":{\"uuid\":\"uuid-value\",\"lastname\":\"Stark\",\"firstname\":\"Tony\",\"username\":\"dummy_user\",\"emailAddress\":\"t.stark@spam.gentics.com\",\"groups\":[\"dummy_user_group\"]},\"properties\":{\"filename\":\"test_1.en.html\",\"name\":\"test_1 english\",\"content\":\"Blessed Mealtime 1!\"},\"type\":\"content\",\"language\":\"en_US\"}";
+		assertEqualsSanitizedJson("The response json did not match the expected one", json, response);
 
 	}
 
 	@Test
 	public void testReadContentByBogusUUID() throws Exception {
 		UserInfo info = data().getUserInfo();
+		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/bogusUUID", 404, "Not Found");
 		String json = "{\"message\":\"Content not found for path {bogusUUID}\"}";
-		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/bogusUUID",
-				404, "Not Found");
 		assertEquals(json, response);
 
 	}
@@ -104,8 +102,8 @@ public class ContentVerticleTest extends AbstractRestVerticleTest {
 	public void testReadContentByInvalidUUID() throws Exception {
 		UserInfo info = data().getUserInfo();
 		String json = "{\"message\":\"Content not found for uuid {dde8ba06bb7211e4897631a9ce2772f5}\"}";
-		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME
-				+ "/contents/dde8ba06bb7211e4897631a9ce2772f5", 404, "Not Found");
+		String response = request(info, HttpMethod.GET, "/api/v1/" + TestDataProvider.PROJECT_NAME + "/contents/dde8ba06bb7211e4897631a9ce2772f5",
+				404, "Not Found");
 		assertEquals(json, response);
 	}
 
