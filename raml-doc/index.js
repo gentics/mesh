71372818b2a9d3dd625b6ac663cd4e4b6c914343
Johannes Schüth <j.schueth@gentics.com>
2015-03-13 14:21:16 +0100

    Cleanup renderer javascript

--- raml-doc/index.js
+++ raml-doc/index.js
@@ -1,24 +1,30 @@
 var raml2html = require('raml2html');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
-var outputDir = "target/raml2html/";
-var config = raml2html.getDefaultConfig(false);
 
-mkdirp(outputDir, function(err) { 
-	if(err) {
+var sourceDir = "src/raml/";
+var outputDir = "target/raml2html/";
+var source = sourceDir + "rest-spec-core.raml";
+var templateFile = sourceDir + "template.handlebars";
+var resourceTemplate = sourceDir + "resource.handlebars";
+var buildSource = outputDir + "spec.raml";
+
+mkdirp(outputDir, function(err) {
+	if (err) {
 		console.log(err);
-		process.exit(1); 
+		process.exit(1);
 	} else {
 		console.log("Create output directory " + outputDir);
-	}});
-
+	}
+});
 
 var onSuccess = function(html) {
-	console.log("OK");
-	fs.writeFile(outputDir + "index.html", html, function(err) {
-		if(err) {
+	var outputFile = outputDir + "index.html";
+	console.log("Rendering completed. Writing output to {" + outputFile + "}");
+	fs.writeFile(outputFile, html, function(err) {
+		if (err) {
 			console.log(err);
-			process.exit(1); 
+			process.exit(1);
 		} else {
 			console.log("The file was saved!");
 		}
@@ -27,26 +33,25 @@ var onSuccess = function(html) {
 
 var onError = function(e) {
 	console.log("Error: " + e.message);
-	process.exit(10); 
+	process.exit(10);
 }
 
 
 
-var source   = "src/raml/rest-spec-core.raml";
-var template = "src/raml/template.handlebars";
-var resourceTemplate = "src/raml/resource.handlebars";
-var buildSource = "target/raml2html/spec.raml";
 
-//TODO cleanup this code
-var resourceTemplateSource = ""; 
+var config = raml2html.getDefaultConfig(false);
+
+// TODO cleanup this code
+var resourceTemplateSource = "";
+console.log("Reading resource template file {" + resourceTemplate + "}");
 fs.readFile(resourceTemplate, 'utf8', function(err, data) {
-  resourceTemplateSource = data;
+	resourceTemplateSource = data;
 });
 
-fs.readFile(template, 'utf8', function(err, data) {
-  config['template'] = data;
-  config['partials']['resource'] = resourceTemplateSource;
-  fs.createReadStream(source).pipe(fs.createWriteStream(buildSource));
-  raml2html.render(buildSource, config, onSuccess, onError);
+console.log("Reading template file {" + templateFile + "}");
+fs.readFile(templateFile, 'utf8', function(err, data) {
+	config['template'] = data;
+	config['partials']['resource'] = resourceTemplateSource;
+	fs.createReadStream(source).pipe(fs.createWriteStream(buildSource));
+	raml2html.render(buildSource, config, onSuccess, onError);
 });
-
