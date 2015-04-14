1c5dc0a1c007d1300518543b46691dc1705ce528
Johannes Schüth <j.schueth@gentics.com>
2015-04-14 16:54:56 +0200

    Cleanup of webroot tests

--- core/src/test/java/com/gentics/cailun/core/verticle/WebRootVerticleTest.java
+++ core/src/test/java/com/gentics/cailun/core/verticle/WebRootVerticleTest.java
@@ -2,16 +2,20 @@ package com.gentics.cailun.core.verticle;
 
 import static com.gentics.cailun.demo.DemoDataProvider.PROJECT_NAME;
 import static io.vertx.core.http.HttpMethod.GET;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import io.vertx.core.http.HttpMethod;
 
 import org.junit.Test;
+import org.neo4j.graphdb.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.gentics.cailun.core.AbstractRestVerticle;
+import com.gentics.cailun.core.data.model.Tag;
 import com.gentics.cailun.core.data.model.auth.PermissionType;
+import com.gentics.cailun.core.rest.tag.response.TagResponse;
 import com.gentics.cailun.demo.UserInfo;
 import com.gentics.cailun.test.AbstractRestVerticleTest;
+import com.gentics.cailun.util.JsonUtils;
 
 public class WebRootVerticleTest extends AbstractRestVerticleTest {
 
@@ -30,53 +34,57 @@ public class WebRootVerticleTest extends AbstractRestVerticleTest {
 		roleService.addPermission(info.getRole(), data().getNews2015(), PermissionType.READ);
 
 		String englishPath = data().getPathForNews2015Tag(data().getEnglish());
-
+		Tag tag = data().getNews2015();
 		String response = request(info, GET, "/api/v1/" + PROJECT_NAME + "/webroot/" + englishPath, 200, "OK");
-		String json = "{\"uuid\":\"uuid-value\",\"schemaName\":\"tag\",\"order\":0,\"creator\":{\"uuid\":\"uuid-value\",\"lastname\":\"Stark\",\"firstname\":\"Tony\",\"username\":\"dummy_user\",\"emailAddress\":\"t.stark@spam.gentics.com\",\"groups\":[\"dummy_user_group\"]},\"properties\":{\"de\":{\"name\":\"ebene_2_a\"}}}";
-		assertEqualsSanitizedJson("Response json does not match the expected one.", json, response);
+		TagResponse restTag = JsonUtils.readValue(response, TagResponse.class);
+		test.assertTag(tag, restTag);
+		assertNull("The path leads to the english version of this tag thus the german properties should not be loaded", restTag.getProperties("de"));
+		assertNotNull("The path leads to the english version of this tag thus the english properties should be loaded.", restTag.getProperties("en"));
 	}
 
 	@Test
 	public void testReadTagWithBogusPath() throws Exception {
 
 		String response = request(info, GET, "/api/v1/" + PROJECT_NAME + "/webroot/blub", 404, "Not Found");
-		String json = "{\"message\":\"Could not find tag for path \\\"blub\\\".\"}";
-		assertEqualsSanitizedJson("Response json does not match the expected one.", json, response);
+		expectMessageResponse("tag_not_found_for_path", response, "blub");
 	}
 
 	@Test
 	public void testReadTagByPathWithoutPerm() throws Exception {
 		String englishPath = data().getPathForNews2015Tag(data().getEnglish());
+		Tag tag = data().getNews2015();
+		try (Transaction tx = graphDb.beginTx()) {
+			roleService.revokePermission(info.getRole(), tag, PermissionType.READ);
+			tx.success();
+		}
 		String response = request(info, GET, "/api/v1/" + PROJECT_NAME + "/webroot/" + englishPath, 403, "Forbidden");
-		String json = "{\"message\":\"Missing permission on object {" + data().getNews().getUuid() + "}\"}";
-		assertEqualsSanitizedJson("Response json does not match the expected one.", json, response);
+		expectMessageResponse("error_missing_perm", response, data().getNews().getUuid());
 	}
 
 	@Test
 	public void testReadContentByValidPath() throws Exception {
 		UserInfo info = data().getUserInfo();
-		String response = request(info, HttpMethod.GET, "/api/v1/" + PROJECT_NAME + "/webroot/subtag/english.html", 200, "OK");
-		String json = "{\"uuid\":\"uuid-value\",\"author\":{\"lastname\":\"Doe\",\"firstname\":\"Joe\",\"username\":\"joe1\",\"emailAddress\":\"j.doe@gentics.com\"},\"properties\":{\"filename\":\"english.html\",\"name\":\"english content name\",\"content\":\"blessed mealtime!\"},\"type\":\"content\",\"language\":\"en_US\"}";
-		assertEqualsSanitizedJson("The response json did not match the expected one", json, response);
-
+		String englishPath = data().getPathForNews2015Tag(data().getEnglish());
+		Tag tag = data().getNews2015();
+		String response = request(info, HttpMethod.GET, "/api/v1/" + PROJECT_NAME + "/webroot/" + englishPath, 200, "OK");
+		TagResponse restTag = JsonUtils.readValue(response, TagResponse.class);
+		test.assertTag(tag, restTag);
 	}
 
 	@Test
 	public void testReadContentByInvalidPath() throws Exception {
 		UserInfo info = data().getUserInfo();
-		String response = request(info, HttpMethod.GET, "/api/v1/" + PROJECT_NAME + "/webroot/subtag/subtag2/no-valid-page.html", 404, "Not Found");
-		String json = "{\"message\":\"Could not find tag for path \\\"subtag/subtag2/no-valid-page.html\\\".\"}";
-		assertEquals(json, response);
-
+		String invalidPath = "subtag/subtag2/no-valid-page.html";
+		String response = request(info, HttpMethod.GET, "/api/v1/" + PROJECT_NAME + "/webroot/" + invalidPath, 404, "Not Found");
+		expectMessageResponse("tag_not_found_for_path", response, invalidPath);
 	}
 
 	@Test
 	public void testReadContentByInvalidPath2() throws Exception {
 		UserInfo info = data().getUserInfo();
-		String response = request(info, HttpMethod.GET, "/api/v1/" + PROJECT_NAME + "/webroot/subtag/subtag-no-valid-tag/no-valid-page.html", 404,
-				"Not Found");
-		String json = "{\"message\":\"Could not find tag for path \\\"subtag/subtag-no-valid-tag/no-valid-page.html\\\".\"}";
-		assertEquals(json, response);
+		String invalidPath = "subtag/subtag-no-valid-tag/no-valid-page.html";
+		String response = request(info, HttpMethod.GET, "/api/v1/" + PROJECT_NAME + "/webroot/" + invalidPath, 404, "Not Found");
+		expectMessageResponse("tag_not_found_for_path", response, invalidPath);
 	}
 
 }
