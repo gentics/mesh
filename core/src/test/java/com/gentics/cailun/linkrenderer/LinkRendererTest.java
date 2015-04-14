508004a22fbe9c211cbfc0fcd8520835f5471a74
Johannes Schüth <j.schueth@gentics.com>
2015-04-14 17:05:15 +0200

    Cleanup of link rendering tests

--- core/src/test/java/com/gentics/cailun/linkrenderer/LinkRendererTest.java
+++ core/src/test/java/com/gentics/cailun/linkrenderer/LinkRendererTest.java
@@ -7,12 +7,11 @@ import java.util.concurrent.ExecutionException;
 import javax.transaction.NotSupportedException;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.neo4j.graphdb.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.gentics.cailun.core.data.model.Content;
 import com.gentics.cailun.core.data.model.Language;
@@ -21,11 +20,7 @@ import com.gentics.cailun.core.link.CaiLunLinkResolver;
 import com.gentics.cailun.core.link.CaiLunLinkResolverFactoryImpl;
 import com.gentics.cailun.core.link.LinkReplacer;
 import com.gentics.cailun.test.AbstractDBTest;
-import com.gentics.cailun.test.SpringTestConfiguration;
 
-@ContextConfiguration(classes = { SpringTestConfiguration.class })
-@RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
 public class LinkRendererTest extends AbstractDBTest {
 
 	final String content = "some bla START<a href=\"${Page(2)}\">Test</a>   dasasdg <a href=\"${Page(3)}\">Test</a>DEN";
@@ -36,32 +31,41 @@ public class LinkRendererTest extends AbstractDBTest {
 	@Autowired
 	private ContentService contentService;
 
+	@Before
+	public void setup() throws Exception {
+		setupData();
+	}
+
 	@Test
 	public void testNodeReplace() throws IOException, InterruptedException, ExecutionException, NotSupportedException {
 
-		Language german = languageService.findByName("german");
+		Language german = data().getGerman();
+		Language english = data().getEnglish();
 
-		
 		// Create some dummy content
 		Content content = new Content();
-		contentService.setName(content, german, "german name");
-		contentService.setFilename(content, german, "german.html");
-		contentService.save(content);
+		try (Transaction tx = graphDb.beginTx()) {
+			contentService.setName(content, german, "german name");
+			contentService.setFilename(content, german, "german.html");
+			contentService.save(content);
+			tx.success();
+		}
 
-		Language english = languageService.findByName("english");
-
-		
 		Content content2 = new Content();
-		contentService.setName(content2, english, "content 2 english");
-		contentService.setFilename(content2, english, "english.html");
-		contentService.save(content2);
+		try (Transaction tx = graphDb.beginTx()) {
+			contentService.setName(content2, english, "content 2 english");
+			contentService.setFilename(content2, english, "english.html");
+			contentService.save(content2);
+			tx.success();
+		}
 
 		LinkReplacer<CaiLunLinkResolver> replacer = new LinkReplacer(resolverFactory);
 		String out = replacer.replace("dgasd");
 		System.out.println(out);
 	}
 
-	// @Test
+	@Ignore("Disabled for now")
+	@Test
 	public void testRendering() throws IOException {
 		System.out.println(content);
 		int start = content.indexOf("#");
