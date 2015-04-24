58a523934379592862cb9a182a67159b1242ae16
Johannes Schüth <j.schueth@gentics.com>
2015-04-24 14:40:07 +0200

    Add accept header information to jmeter test

--- checkPerformance.sh
+++ checkPerformance.sh
@@ -4,8 +4,8 @@ ENDPOINT=$1
 curl -v http://joe1:test123@localhost:8080/$ENDPOINT
 
 CONC=$2
-SID=$(curl -v --silent  http://joe1:test123@localhost:8080/$ENDPOINT 2>&1 | grep "Set-Cookie" | sed -e 's/.*cailun\.session=\(.*\);.*/\1/')
-
+SID=$(curl -v -H 'Accept: application/json' --silent  http://joe1:test123@localhost:8080/$ENDPOINT 2>&1 | grep "Set-Cookie" | sed -e 's/.*cailun\.session=\(.*\);.*/\1/')
+echo "SID: $SID" 
 ab -n 100000 -c $CONC -A joe1:test123  -C cailun.session=$SID http://localhost:8080/$ENDPOINT
 #wrk -t 8  -d 10 -c 1000 -H "Cookie: cailun.session=$SID"  http://localhost:8080/$ENDPOINT
 
