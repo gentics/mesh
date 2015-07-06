a24405d5454b4ed0b4904d67d8ca95e6cb6a19bb
Johannes Schüth <j.schueth@gentics.com>
2015-07-06 18:43:25 +0200

    Remove bogus module

--- rest/src/main/java/com/gentics/mesh/core/rest/role/RoleListResponse.java
+++
@@ -1,9 +0,0 @@
-package com.gentics.mesh.core.rest.role;
-
-import com.gentics.mesh.core.rest.common.AbstractListResponse;
-
-public class RoleListResponse extends AbstractListResponse<RoleResponse> {
-
-	public RoleListResponse() {
-	}
-}
--- rest/src/main/java/com/gentics/mesh/core/rest/schema/SchemaListResponse.java
+++
@@ -1,9 +0,0 @@
-package com.gentics.mesh.core.rest.schema;
-
-import com.gentics.mesh.core.rest.common.AbstractListResponse;
-
-public class SchemaListResponse extends AbstractListResponse<SchemaResponse> {
-
-	public SchemaListResponse() {
-	}
-}
