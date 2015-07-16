90f3b2819f30a92712dca5878b0288865bbe811d
Johannes Schüth <j.schueth@gentics.com>
2015-07-16 12:07:44 +0200

    Add extra login method to auth verticle

---
+++ api/src/main/java/com/gentics/mesh/core/rest/auth/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.gentics.mesh.core.rest.auth;
+
+public class LoginRequest {
+
+	private String username;
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
--- core/src/main/java/com/gentics/mesh/core/verticle/AuthenticationVerticle.java
+++ core/src/main/java/com/gentics/mesh/core/verticle/AuthenticationVerticle.java
@@ -2,18 +2,29 @@ package com.gentics.mesh.core.verticle;
 
 import static com.gentics.mesh.util.RoutingContextHelper.getUser;
 import static io.vertx.core.http.HttpMethod.GET;
+import static io.vertx.core.http.HttpMethod.POST;
+import io.vertx.core.json.JsonObject;
 
 import org.jacpfx.vertx.spring.SpringVerticle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.gentics.mesh.core.AbstractCoreApiVerticle;
 import com.gentics.mesh.core.data.MeshAuthUser;
+import com.gentics.mesh.core.rest.auth.LoginRequest;
+import com.gentics.mesh.core.rest.common.GenericMessageResponse;
+import com.gentics.mesh.core.rest.error.HttpStatusCodeErrorException;
+import com.gentics.mesh.etc.MeshSpringConfiguration;
+import com.gentics.mesh.json.JsonUtil;
 
 @Component
 @Scope("singleton")
 @SpringVerticle()
 public class AuthenticationVerticle extends AbstractCoreApiVerticle {
+
+	@Autowired
+	MeshSpringConfiguration springConfiguration;
 
 	public AuthenticationVerticle() {
 		super("auth");
@@ -21,16 +32,30 @@ public class AuthenticationVerticle extends AbstractCoreApiVerticle {
 
 	@Override
 	public void registerEndPoints() throws Exception {
-		route("/*").handler(springConfiguration.authHandler());
+		route("/me").handler(springConfiguration.authHandler());
 		route("/me").method(GET).produces(APPLICATION_JSON).handler(rc -> {
 			MeshAuthUser requestUser = getUser(rc);
 			transformAndResponde(rc, requestUser);
 		});
-		route("/login").method(GET).consumes(APPLICATION_JSON).produces(APPLICATION_JSON).handler(rc -> {
-			//			GenericResponse<String> response = new GenericResponse<>();
-			//			response.setObject("OK");
-			//			rc.response().end(toJson(response));
-			});
+
+		route("/login").method(POST).consumes(APPLICATION_JSON).produces(APPLICATION_JSON).handler(rc -> {
+			LoginRequest request;
+			try {
+				request = JsonUtil.readValue(rc.getBodyAsString(), LoginRequest.class);
+				//TODO fail on missing field
+				JsonObject authInfo = new JsonObject().put("username", request.getUsername()).put("password", request.getPassword());
+				springConfiguration.authProvider().authenticate(authInfo, rh -> {
+					if (rh.failed()) {
+						rc.fail(new HttpStatusCodeErrorException(401, i18n.get(rc, "login_failed"), rh.cause()));
+					} else {
+						GenericMessageResponse message = new GenericMessageResponse("OK");
+						responde(rc, JsonUtil.toJson(message));
+					}
+				});
+			} catch (Exception e) {
+				rc.fail(new HttpStatusCodeErrorException(401, i18n.get(rc, "login_failed"), e));
+			}
+		});
 
 	}
 }
