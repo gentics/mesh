bff13a1d8d70f9395c8965dda42c7bcf5b03e82d
Johannes Schüth <j.schueth@gentics.com>
2015-08-07 09:45:01 +0200

    Add javadoc and remove orientdb dependency from mesh-server

--- rest-client/src/main/java/com/gentics/mesh/rest/method/NodeClientMethods.java
+++ rest-client/src/main/java/com/gentics/mesh/rest/method/NodeClientMethods.java
@@ -11,24 +11,98 @@ import com.gentics.mesh.core.rest.node.QueryParameterProvider;
 
 public interface NodeClientMethods {
 
+	/**
+	 * Find the node with the given uuid in the project with the given name. The query parameters can be utilized to set the desired language and expand field
+	 * settings.
+	 * 
+	 * @param projectName
+	 * @param uuid
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeResponse> findNodeByUuid(String projectName, String uuid, QueryParameterProvider... parameters);
 
+	/**
+	 * Create a node within the given project. The query parameters determine which language of the node will be returned.
+	 * 
+	 * @param projectName
+	 * @param nodeCreateRequest
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeResponse> createNode(String projectName, NodeCreateRequest nodeCreateRequest, QueryParameterProvider... parameters);
 
+	/**
+	 * Update the node with the given uuid.
+	 * 
+	 * @param projectName
+	 * @param uuid
+	 * @param nodeUpdateRequest
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeResponse> updateNode(String projectName, String uuid, NodeUpdateRequest nodeUpdateRequest, QueryParameterProvider... parameters);
 
+	/**
+	 * Delete the node with the given uuid.
+	 * 
+	 * @param projectName
+	 * @param uuid
+	 * @return
+	 */
 	Future<GenericMessageResponse> deleteNode(String projectName, String uuid);
 
+	/**
+	 * Find all nodes within the project with the given name. The query parameters can be used to set paging and language settings.
+	 * 
+	 * @param projectName
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeListResponse> findNodes(String projectName, QueryParameterProvider... parameters);
 
 	// Relations
 
+	/**
+	 * Find all child nodes of the given node with the given parentNodeUuid. The query parameters can be used to set paging and language settings.
+	 * 
+	 * @param projectName
+	 * @param parentNodeUuid
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeListResponse> findNodeChildren(String projectName, String parentNodeUuid, QueryParameterProvider... parameters);
 
+	/**
+	 * Find all nodes that were tagged by the tag with the given tagUuid. The query parameters can be used to set paging and language settings.
+	 * 
+	 * @param projectName
+	 * @param tagUuid
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeListResponse> findNodesForTag(String projectName, String tagUuid, QueryParameterProvider... parameters);
 
+	/**
+	 * Add with the given tagUuid to the node with the given nodeUuid. The query parameters can be used to set language settings.
+	 * 
+	 * @param projectName
+	 * @param nodeUuid
+	 * @param tagUuid
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeResponse> addTagToNode(String projectName, String nodeUuid, String tagUuid, QueryParameterProvider... parameters);
 
+	/**
+	 * Remove a tag with the given tagUuid from the node with the given nodeUuid. The query parameters can be used to set language settings.
+	 * 
+	 * @param projectName
+	 * @param nodeUuid
+	 * @param tagUuid
+	 * @param parameters
+	 * @return
+	 */
 	Future<NodeResponse> removeTagFromNode(String projectName, String nodeUuid, String tagUuid, QueryParameterProvider... parameters);
 
 	/**
