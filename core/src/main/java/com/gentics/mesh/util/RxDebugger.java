7f5548ebfa0d12c0c8582b0e289239d8a405e7bb
Johannes Schüth <j.schueth@gentics.com>
2016-07-06 11:03:21 +0200

    Update debugger tool

--- core/src/main/java/com/gentics/mesh/util/RxDebugger.java
+++ core/src/main/java/com/gentics/mesh/util/RxDebugger.java
@@ -1,42 +1,115 @@
 package com.gentics.mesh.util;
 
-import java.util.Collection;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import rx.Observer;
 import rx.plugins.DebugHook;
 import rx.plugins.DebugNotification;
+import rx.plugins.DebugNotification.Kind;
 import rx.plugins.DebugNotificationListener;
 import rx.plugins.RxJavaPlugins;
 
-public class RxDebugger {
-	private AtomicLong counter;
-	private Map<Long, DebugNotification> runningObservables;
+public class RxDebugger extends Thread implements Runnable {
 
-	public void start() {
-		counter = new AtomicLong();
-		runningObservables = new ConcurrentHashMap<>();
-		RxJavaPlugins.getInstance().registerObservableExecutionHook(new DebugHook<>(new DebugNotificationListener<Long>() {
+	private Map<String, RxEventInfo> runningObservables = new ConcurrentHashMap<>();
+	private long maxObsTimeInMs = 1000;
+	private long checkInterval = 2000;
+
+	private static final Logger log = LoggerFactory.getLogger(RxDebugger.class);
+
+	@Override
+	public void run() {
+
+		RxJavaPlugins.getInstance().registerObservableExecutionHook(new DebugHook<>(new DebugNotificationListener<String>() {
 			@Override
-			public <T> Long start(DebugNotification<T> n) {
-				long id = counter.getAndIncrement();
-				runningObservables.put(id, n);
-				return id;
+			public <T> String start(DebugNotification<T> n) {
+				Observer<?> obs = n.getObserver();
+				if (obs != null && n.getKind() == Kind.OnNext) {
+					String eventId = obs.toString() + "." + n.getKind().name();
+					if (log.isDebugEnabled()) {
+						log.debug("Got event: " + eventId);
+					}
+					try {
+						throw new Exception();
+					} catch (Exception e) {
+						RxEventInfo eventInfo = new RxEventInfo(System.currentTimeMillis(), n, e);
+						log.debug("Adding: " + eventId);
+						runningObservables.put(obs.toString(), eventInfo);
+						return eventId;
+					}
+				}
+				if (obs != null && n.getKind() == Kind.OnCompleted) {
+					return obs.toString() + "." + n.getKind().name();
+				}
+				return null;
 			}
 
 			@Override
-			public void complete(Long id) {
-				runningObservables.remove(id);
+			public void complete(String id) {
+				if (id != null && id.endsWith(Kind.OnCompleted.name())) {
+					String key = id.substring(0, id.lastIndexOf("."));
+					log.debug("Removing: " + key);
+					runningObservables.remove(key);
+				}
 			}
+
 		}));
+
+		while (true) {
+			try {
+				log.error("Checking for uncompleted observables..");
+				checkForBlockedObs();
+				Thread.sleep(checkInterval);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
-	public void stop() {
-		//RxJavaPlugins.getInstance().reset();
+	public void checkForBlockedObs() {
+		long now = System.currentTimeMillis();
+		for (Entry<String, RxEventInfo> entry : getRunningObservables().entrySet()) {
+			if (log.isDebugEnabled()) {
+				log.debug("Checking obserable " + entry.getKey());
+			}
+			long delta = now - entry.getValue().getTime();
+			if (delta > maxObsTimeInMs) {
+				log.error("Found observable which send last event {" + delta + "} ms ago.", entry.getValue().getThrowable());
+			}
+		}
 	}
 
-	public Collection<DebugNotification> getRunningObservables() {
-		return runningObservables.values();
+	public Map<String, RxEventInfo> getRunningObservables() {
+		return runningObservables;
 	}
+}
+
+class RxEventInfo {
+
+	private Throwable throwable;
+	private long time;
+	private DebugNotification<?> info;
+
+	public RxEventInfo(long time, DebugNotification<?> info, Throwable throwable) {
+		this.time = time;
+		this.info = info;
+		this.throwable = throwable;
+	}
+
+	public Throwable getThrowable() {
+		return throwable;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public DebugNotification<?> getInfo() {
+		return info;
+	}
+
 }
---
+++ core/src/test/java/com/gentics/mesh/test/RxDebuggerTest.java
@@ -0,0 +1,33 @@
+package com.gentics.mesh.test;
+
+import org.junit.Test;
+
+import com.gentics.mesh.util.RxDebugger;
+
+import rx.Observable;
+
+public class RxDebuggerTest {
+
+	@Test
+	public void testStuckObservable() throws InterruptedException {
+		new RxDebugger().start();
+		new Thread(() -> {
+			Observable<String> obs = Observable.create(sub -> {
+				for (int i = 0; i < 10; i++) {
+					sub.onNext("blub");
+				}
+				try {
+					Thread.sleep(4000);
+				} catch (Exception e) {
+				e.printStackTrace();
+				}
+				System.out.println("Complete");
+				sub.onCompleted();
+
+			});
+			obs.subscribe();
+		}).start();
+		Thread.sleep(10000);
+		System.out.println("Done waiting");
+	}
+}
