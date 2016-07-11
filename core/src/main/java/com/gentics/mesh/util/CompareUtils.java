887e43ad6f3d87eb360afe19bb5dd6a41deb37f6
Johannes Schüth <j.schueth@gentics.com>
2016-07-11 16:55:50 +0200

    Add additional compare method for number lists

--- core/src/main/java/com/gentics/mesh/util/CompareUtils.java
+++ core/src/main/java/com/gentics/mesh/util/CompareUtils.java
@@ -1,5 +1,8 @@
 package com.gentics.mesh.util;
 
+import java.util.List;
+import java.util.ListIterator;
+
 public final class CompareUtils {
 
 	/**
@@ -16,6 +19,41 @@ public final class CompareUtils {
 	 */
 	public static boolean equals(Object a, Object b) {
 		return (a == null && b == null) || (a != null && a.equals(b));
+	}
+
+	/**
+	 * Returns {@code true} if the numbers lists are equal to each other and {@code false} otherwise. Consequently, if both arguments are {@code null},
+	 * {@code true} is returned and if exactly one argument is {@code null}, {@code false} is returned. Otherwise, equality is determined by using the
+	 * {@link NumberUtils#compare} method by comparing each list item.
+	 *
+	 * @param a
+	 *            First number list
+	 * @param b
+	 *            Second number list to be compared with {@code a} for equality
+	 * @return {@code true} if the arguments are equal to each other and {@code false} otherwise
+	 * @see Object#equals(Object)
+	 */
+	public static boolean equals(List<Number> a, List<Number> b) {
+		if (a == null && b == null) {
+			return true;
+		}
+		if (a == b) {
+			return true;
+		}
+		if (a == null && b != null || a != null && b == null) {
+			return false;
+		}
+
+		ListIterator<Number> e1 = a.listIterator();
+		ListIterator<Number> e2 = b.listIterator();
+		while (e1.hasNext() && e2.hasNext()) {
+			Number o1 = e1.next();
+			Number o2 = e2.next();
+			if (!(o1 == null ? o2 == null : NumberUtils.compare(o1, o2) == 0))
+				return false;
+		}
+		return !(e1.hasNext() || e2.hasNext());
+
 	}
 
 	/**
