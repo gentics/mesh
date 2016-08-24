abb73ff4532dff9a5ab02f6bc252c1cdb17bd51a
Johannes Schüth <j.schueth@gentics.com>
2016-08-24 17:50:06 +0200

    Add draft

--- core/src/test/java/com/gentics/mesh/cli/DaggerTest.java
+++ core/src/test/java/com/gentics/mesh/cli/DaggerTest.java
@@ -1,28 +1,39 @@
 package com.gentics.mesh.cli;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.gentics.mesh.core.verticle.user.UserVerticle;
 import com.gentics.mesh.etc.DaggerMeshDagger;
 import com.gentics.mesh.etc.MeshDagger;
 import com.gentics.mesh.graphdb.NoTx;
 
 public class DaggerTest {
 
+	private InMemoryMeshDagger meshDagger;
+
+	private UserVerticle userVerticle;
+
+	@Before
+	public void setup() {
+		meshDagger = DaggerInMemoryMeshDagger.builder().build();
+		this.userVerticle = meshDagger.userVerticle();
+	}
 	@Test
 	public void testInMemory() {
-		InMemoryMeshDagger meshDagger = DaggerInMemoryMeshDagger.builder().build();
+		
 		try (NoTx noTx = meshDagger.database().noTx()) {
 			System.out.println("Found Groups: {" + meshDagger.boot().groupRoot().findAll().size() + "}");
 		}
 		System.out.println(meshDagger.userVerticle());
 		System.out.println(meshDagger.config().database());
-		System.out.println(meshDagger.userVerticle());
+		System.out.println(userVerticle);
 	}
 
 	@Test
 	public void test() {
-		MeshDagger meshDagger = DaggerMeshDagger.builder().build();
-		meshDagger.boot().groupRoot();
+//		MeshDagger meshDagger = DaggerMeshDagger.builder().build();
+//		meshDagger.boot().groupRoot();
 	}
 
 }
