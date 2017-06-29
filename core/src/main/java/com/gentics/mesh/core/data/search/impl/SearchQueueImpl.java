c2e3ceeb97759c8d3c416fa68e3a79d6de1c7fe8
Johannes Schüth <j.schueth@gentics.com>
2017-06-29 15:43:39 +0200

    Fix potential deadlock

--- core/src/main/java/com/gentics/mesh/core/data/search/impl/SearchQueueImpl.java
+++ core/src/main/java/com/gentics/mesh/core/data/search/impl/SearchQueueImpl.java
@@ -45,12 +45,10 @@ public class SearchQueueImpl extends ArrayBlockingQueue<SearchQueueBatch> implem
 	}
 
 	@Override
-	public boolean remove(Object o) {
+	public boolean remove(SearchQueueBatch o) {
 		boolean result = super.remove(o);
 		if (isEmpty()) {
-			synchronized (objectLock) {
-				objectLock.notify();
-			}
+			objectLock.notify();
 		}
 		return result;
 	}
