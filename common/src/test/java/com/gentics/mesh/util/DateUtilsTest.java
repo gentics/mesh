ba05c55c8cc70e2e83364033b13bc35dc5ae7fa6
Johannes Schüth <j.schueth@gentics.com>
2017-07-26 19:10:16 +0200

    Fix date handling for bogus inputs

--- common/src/test/java/com/gentics/mesh/util/DateUtilsTest.java
+++ common/src/test/java/com/gentics/mesh/util/DateUtilsTest.java
@@ -1,6 +1,7 @@
 package com.gentics.mesh.util;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -12,5 +13,9 @@ public class DateUtilsTest {
 		assertNotNull(DateUtils.fromISO8601("2017-07-25T12:40:00+00:00"));
 		assertNotNull(DateUtils.fromISO8601("2017-07-25T12:40:00+01:00"));
 		assertNotNull(DateUtils.fromISO8601("2017-07-25T12:40:00"));
+		assertNull(DateUtils.fromISO8601("Bogus"));
+		assertNull(DateUtils.fromISO8601("0"));
+		assertNull(DateUtils.fromISO8601("1"));
+		assertNull(DateUtils.fromISO8601("42"));
 	}
 }
