28a8923584ac091b62a38e93c4c5480268f86827
philippguertler <philippguertler@users.noreply.github.com>
2017-10-12 11:48:42 +0200

    GraphQL: Add nodes field to schema type (#159)
    GraphQL: Add nodes field to schema type - Fixes #157
    
    This lists all nodes of that schema in the current release.

--- verticles/graphql/src/main/java/com/gentics/mesh/graphql/type/SchemaTypeProvider.java
+++ verticles/graphql/src/main/java/com/gentics/mesh/graphql/type/SchemaTypeProvider.java
@@ -1,5 +1,6 @@
 package com.gentics.mesh.graphql.type;
 
+import static com.gentics.mesh.graphql.type.NodeTypeProvider.NODE_PAGE_TYPE_NAME;
 import static graphql.Scalars.GraphQLBoolean;
 import static graphql.Scalars.GraphQLString;
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
@@ -8,11 +9,16 @@ import static graphql.schema.GraphQLObjectType.newObject;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import com.gentics.mesh.core.data.ContainerType;
 import com.gentics.mesh.core.data.NamedElement;
+import com.gentics.mesh.core.data.NodeGraphFieldContainer;
+import com.gentics.mesh.core.data.node.NodeContent;
+import com.gentics.mesh.core.data.page.impl.DynamicStreamPageImpl;
 import com.gentics.mesh.core.data.schema.SchemaContainer;
 import com.gentics.mesh.core.data.schema.SchemaContainerVersion;
 import com.gentics.mesh.core.rest.schema.SchemaModel;
 import com.gentics.mesh.core.rest.schema.impl.SchemaModelImpl;
+import com.gentics.mesh.graphql.context.GraphQLContext;
 import com.gentics.mesh.json.JsonUtil;
 
 import graphql.schema.DataFetchingEnvironment;
@@ -20,6 +26,10 @@ import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLObjectType.Builder;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Singleton
 public class SchemaTypeProvider extends AbstractTypeProvider {
@@ -71,9 +81,37 @@ public class SchemaTypeProvider extends AbstractTypeProvider {
 			return model != null ? model.getSegmentField() : null;
 		}));
 
+		schemaType.field(newPagingFieldWithFetcherBuilder("nodes", "Load nodes with this schema", env -> {
+			GraphQLContext gc = env.getContext();
+			List<String> languageTags = getLanguageArgument(env);
+
+			Stream<? extends NodeContent> nodes = StreamSupport.stream(getSchemaContainerVersion(env).getNodes(
+					gc.getRelease().getUuid(),
+					gc.getUser(),
+					ContainerType.forVersion(gc.getVersioningParameters().getVersion())
+			).spliterator(), false)
+			.map(node -> {
+				NodeGraphFieldContainer container = node.findNextMatchingFieldContainer(gc, languageTags);
+				return new NodeContent(node, container);
+			});
+
+			return new DynamicStreamPageImpl(nodes, getPagingInfo(env));
+		}, NODE_PAGE_TYPE_NAME));
+
 		// TODO add fields
 
 		return schemaType.build();
+	}
+
+	private SchemaContainerVersion getSchemaContainerVersion(DataFetchingEnvironment env) {
+		Object source = env.getSource();
+		if (source instanceof SchemaContainerVersion) {
+			return (SchemaContainerVersion) source;
+		} else if (source instanceof SchemaContainer) {
+			return ((SchemaContainer) source).getLatestVersion();
+		} else {
+			throw new RuntimeException("Invalid type {" + source + "}.");
+		}
 	}
 
 	private SchemaModel loadModelWithFallback(DataFetchingEnvironment env) {
