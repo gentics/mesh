206ad284d09e39b1a33fd54a0df7181d60272051
Johannes Schüth <j.schueth@gentics.com>
2017-11-23 11:30:07 +0100

    Reduce ci timeout

--- test-common/src/main/java/com/gentics/mesh/test/util/MeshAssert.java
+++ test-common/src/main/java/com/gentics/mesh/test/util/MeshAssert.java
@@ -21,7 +21,7 @@ public final class MeshAssert {
 
 	private static final Logger log = LoggerFactory.getLogger(MeshAssert.class);
 
-	private static final Integer CI_TIMEOUT_SECONDS = 500;
+	private static final Integer CI_TIMEOUT_SECONDS = 100;
 
 	private static final Integer DEV_TIMEOUT_SECONDS = 10000;
 
