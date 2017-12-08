a66703ef3b614b6a2fac34585af4fd9cd7446afe
Johannes Schüth <git@jotschi.de>
2017-12-08 13:00:44 +0100

    Fix binary field handler test

--- core/src/test/java/com/gentics/mesh/core/webroot/node/BinaryFieldHandlerTest.java
+++ core/src/test/java/com/gentics/mesh/core/webroot/node/BinaryFieldHandlerTest.java
@@ -18,12 +18,14 @@ import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.gentics.mesh.Mesh;
 import com.gentics.mesh.context.InternalActionContext;
 import com.gentics.mesh.core.data.node.field.BinaryGraphField;
+import com.gentics.mesh.core.rest.error.GenericRestException;
 import com.gentics.mesh.core.verticle.node.BinaryFieldHandler;
 import com.gentics.mesh.etc.config.MeshUploadOptions;
 import com.gentics.mesh.storage.LocalBinaryStorage;
@@ -31,114 +33,139 @@ import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
 import com.syncleus.ferma.tx.Tx;
 
-import io.vertx.core.file.FileSystemException;
 import io.vertx.core.http.CaseInsensitiveHeaders;
 import io.vertx.ext.web.FileUpload;
 
 @MeshTestSetting(useElasticsearch = false, testSize = FULL, startServer = true)
-public class NodeFieldAPIHandlerTest extends AbstractMeshTest {
+public class BinaryFieldHandlerTest extends AbstractMeshTest {
 
 	private BinaryFieldHandler handler;
 
 	private MeshUploadOptions uploadOptions;
 
-	final String data = "bliblablub";
-	final String hash = "406d7d8188bb4556f7616628d1a5cd281ef6686034ddb3855b0ebb6affe6675e8ba9cde8f60f183341a0105223533e1ca09570e5d024cc8173d0b5087dfab4b5";
-	String segmentedPath = "some/path/to/file";
-
 	@Before
 	public void setup() throws Exception {
 		uploadOptions = Mesh.mesh().getOptions().getUploadOptions();
 		handler = meshDagger().nodeFieldAPIHandler();
+		File uploadFolder = getUploadFolder();
+		FileUtils.deleteDirectory(uploadFolder);
+	}
+
+	@After
+	public void cleanup() throws IOException {
+		File uploadFolder = getUploadFolder();
+		FileUtils.deleteDirectory(uploadFolder);
 	}
 
 	@Test
 	public void testFileUploadHandler() throws IOException {
+		File uploadFolder = getUploadFolder();
 		try (Tx tx = tx()) {
+			// Setup schema
 			prepareSchema(content(), null, "binaryField");
-			FileUpload fileUpload = mockUpload();
-			File uploadFolder = getUploadFolder();
-			InternalActionContext ac = mockContext(fileUpload);
 
-			ac.put("sourceFile", fileUpload);
-			assertFalse("Initially no upload folder should exist.", uploadFolder.exists());
+			// Mock upload request
+			InternalActionContext ac = mockContext(mockUpload("blibbla"));
 			CaseInsensitiveHeaders attributes = new CaseInsensitiveHeaders();
 			attributes.add("language", "en");
 			attributes.add("version", "1.0");
-			assertNull("Initially no binary field should be found.", content().getLatestDraftFieldContainer(english()).getBinary("binaryField"));
-			handler.handleUpdateField(ac, contentUuid(), "binaryField", attributes);
-			BinaryGraphField field = content().getLatestDraftFieldContainer(english()).getBinary("binaryField");
-			assertEquals("bla", field.getFileName());
-			assertEquals(0, field.getBinary().getSize());
-			assertEquals("text/plain", field.getMimeType());
 
+			// Assert initial condition
+			assertFalse("Initially no upload folder should exist.", uploadFolder.exists());
+			assertNull("Initially no binary field should be found.", content().getLatestDraftFieldContainer(english()).getBinary("binaryField"));
+
+			// Invoke request
+			handler.handleUpdateField(ac, contentUuid(), "binaryField", attributes);
+
+			// Assert result
+			BinaryGraphField field = content().getLatestDraftFieldContainer(english()).getBinary("binaryField");
+			assertEquals("Filename did not match.", "bla", field.getFileName());
+			assertEquals("Size of the file did not match.", 7, field.getBinary().getSize());
+			assertEquals("mimetype did not match.", "text/plain", field.getMimeType());
 			String uuid = field.getBinary().getUuid();
 			String path = LocalBinaryStorage.getFilePath(uuid);
 			assertTrue("The file should be placed in the local binary storage.", new File(path).exists());
-			FileUtils.deleteDirectory(uploadFolder);
+		}
 
-			fileUpload = mockUpload();
+		try (Tx tx = tx()) {
+			// Clear upload folder for next upload
+			FileUtils.deleteDirectory(uploadFolder);
 			assertThat(uploadFolder).as("The upload folder should have been created").doesNotExist();
-			ac.put("sourceFile", fileUpload);
-			attributes.set("version", "1.1");
-			handler.handleUpdateField(ac, contentUuid(), "binaryField", attributes);
-			// assertFalse("The upload file should have been moved.", new File(fileUpload.uploadedFileName()).exists());
-			assertTrue("The upload folder should have been created.", uploadFolder.exists());
+
+			// Prepare request
+			InternalActionContext ac2 = mockContext(mockUpload("blub123"));
+			CaseInsensitiveHeaders attributes = new CaseInsensitiveHeaders();
+			attributes.add("language", "en");
+			attributes.add("version", "1.1");
+
+			// Invoke second upload
+			handler.handleUpdateField(ac2, contentUuid(), "binaryField", attributes);
+
+			// Assert result
+			BinaryGraphField field = content().getLatestDraftFieldContainer(english()).getBinary("binaryField");
+			String uuid = field.getBinary().getUuid();
+			String path = LocalBinaryStorage.getFilePath(uuid);
+			assertTrue("The file should be placed in the local binary storage.", new File(path).exists());
+			assertTrue("The upload folder {" + uploadFolder.getAbsolutePath() + "} should have been created.", uploadFolder.exists());
 		}
 	}
 
 	@Test
 	public void testHandlerCase2() throws IOException {
+		File uploadFolder = getUploadFolder();
 		try (Tx tx = tx()) {
+			// Setup the schema & request
 			prepareSchema(content(), null, "binaryField");
-			segmentedPath = "/cdfb/34f9/598a/4173/bb34/f959/8ae1/7330/";
-			FileUpload fileUpload = mockUpload();
-			InternalActionContext ac = mockContext(fileUpload);
-			ac.put("sourceFile", fileUpload);
-			File uploadFolder = getUploadFolder();
-			assertFalse("Initially no upload folder should exist.", uploadFolder.exists());
-
 			CaseInsensitiveHeaders attributes = new CaseInsensitiveHeaders();
 			attributes.add("language", "en");
 			attributes.add("version", "1.0");
+			InternalActionContext ac = mockContext(mockUpload("blub123"));
+
+			// Assert initial condition
+			assertFalse("Initially no upload folder should exist.", uploadFolder.exists());
+
+			// Invoke the request
 			handler.handleUpdateField(ac, contentUuid(), "binaryField", attributes);
-			assertFalse("The upload file should have been moved.", new File(fileUpload.uploadedFileName()).exists());
+
+			// Assert result
 			assertThat(uploadFolder).as("The upload folder should have been created").exists();
-			FileUtils.deleteDirectory(uploadFolder);
 		}
 	}
 
-	@Test(expected = FileSystemException.class)
+	@Test(expected = GenericRestException.class)
 	public void testFileUploadWithNoUploadFile() throws Throwable {
 		try (Tx tx = tx()) {
+			// Setup the schema & request
 			prepareSchema(content(), null, "binaryField");
-			FileUpload fileUpload = mockUpload();
-			InternalActionContext ac = mockContext(fileUpload);
-			ac.put("sourceFile", fileUpload);
-
-			// Delete the file on purpose in order to invoke an error
-			new File(fileUpload.uploadedFileName()).delete();
+			FileUpload upload = mockUpload("blub123");
+			InternalActionContext ac = mockContext(upload);
 			CaseInsensitiveHeaders attributes = new CaseInsensitiveHeaders();
 			attributes.add("language", "en");
 			attributes.add("version", "1.0");
+
+			// Delete the file on purpose in order to invoke an error
+			new File(upload.uploadedFileName()).delete();
+
+			// Invoke the request
 			handler.handleUpdateField(ac, contentUuid(), "binaryField", attributes);
 		}
 	}
 
 	private File getUploadFolder() {
-		return new File(uploadOptions.getDirectory(), segmentedPath);
+		return new File(uploadOptions.getDirectory());
 	}
 
-	private FileUpload mockUpload() throws IOException {
+	private FileUpload mockUpload(String content) throws IOException {
 		FileUtils.forceDeleteOnExit(new File(uploadOptions.getDirectory()));
 		File sourceFile = new File("target/testfile_" + System.currentTimeMillis());
 		sourceFile.deleteOnExit();
 		sourceFile.createNewFile();
-		FileUtils.writeStringToFile(sourceFile, data);
+		FileUtils.writeStringToFile(sourceFile, content);
 
 		FileUpload fileUpload = mock(FileUpload.class);
 		when(fileUpload.fileName()).thenReturn("bla");
 		when(fileUpload.contentType()).thenReturn("text/plain");
+		when(fileUpload.size()).thenReturn((long) content.length());
 		when(fileUpload.uploadedFileName()).thenReturn(sourceFile.getAbsolutePath());
 		return fileUpload;
 	}
