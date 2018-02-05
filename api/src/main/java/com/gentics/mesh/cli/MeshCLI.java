cb2b6d487a747d0b4c4511e210f9e30e0103cd0d
Johannes Schüth <git@jotschi.de>
2018-02-05 22:51:58 +0100

    Add additional CLI arguments

--- api/src/main/java/com/gentics/mesh/cli/MeshCLI.java
+++ api/src/main/java/com/gentics/mesh/cli/MeshCLI.java
@@ -3,11 +3,14 @@ package com.gentics.mesh.cli;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public final class MeshCLI {
+
+	public static final String HELP = "help";
 
 	public static final String INIT_CLUSTER = "initCluster";
 
@@ -19,6 +22,12 @@ public final class MeshCLI {
 
 	public static final String RESET_ADMIN_PASSWORD = "resetAdminPassword";
 
+	public static final String ELASTICSEARCH_URL = "elasticsearchUrl";
+
+	public static final String EMBEDDED_ELASTICSEARCH = "embeddedElasticsearch";
+
+	public static final String DISABLE_ELASTICSEARCH = "disableElasticsearch";
+
 	/**
 	 * Parse the given command line arguments and return the parsed representation.
 	 * 
@@ -27,28 +36,50 @@ public final class MeshCLI {
 	 * @throws ParseException
 	 */
 	public static CommandLine parse(String... args) throws ParseException {
+		CommandLineParser parser = new BasicParser();
+		return parser.parse(options(), args);
+	}
+
+	public static Options options() {
 		Options options = new Options();
 
-		Option help = new Option("help", "print this message");
+		Option help = new Option(HELP, "This output");
 		options.addOption(help);
 
+		Option elasticsearch = new Option(EMBEDDED_ELASTICSEARCH, true, "Flag which can be used to disable the embeddded Elasticsearch server.");
+		elasticsearch.setArgName("flag");
+		options.addOption(elasticsearch);
+
+		Option disableElasticsearch = new Option(DISABLE_ELASTICSEARCH, "Flag which can be used to disable the Elasticsearch integration.");
+		options.addOption(disableElasticsearch);
+
+		Option elasticsearchUrl = new Option(ELASTICSEARCH_URL, true, "Elasticsearch URL to be used.");
+		elasticsearchUrl.setArgName("url");
+		options.addOption(elasticsearchUrl);
+
 		Option resetAdminPassword = new Option(RESET_ADMIN_PASSWORD, true, "Reset the admin password");
+		resetAdminPassword.setArgName("password");
 		options.addOption(resetAdminPassword);
 
 		Option initCluster = new Option(INIT_CLUSTER, false, "Flag which can be used to initialise the first instance of a cluster.");
 		options.addOption(initCluster);
 
 		Option nodeName = new Option(NODE_NAME, true, "Node instance name");
+		nodeName.setArgName("name");
 		options.addOption(nodeName);
 
 		Option clusterName = new Option(CLUSTER_NAME, true, "Cluster name");
+		clusterName.setArgName("name");
 		options.addOption(clusterName);
 
 		Option httpPort = new Option(HTTP_PORT, true, "Server HTTP port");
+		httpPort.setArgName("port");
 		options.addOption(httpPort);
+		return options;
+	}
 
-		CommandLineParser parser = new BasicParser();
-		CommandLine cmd = parser.parse(options, args);
-		return cmd;
+	public static void printHelp() {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("mesh.jar", options());
 	}
 }
