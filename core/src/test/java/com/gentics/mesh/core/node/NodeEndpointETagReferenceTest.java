50972d3ff7d88ae4f1648db98ba083021fcf81bb
Johannes Schüth <git@jotschi.de>
2018-02-17 20:12:11 +0100

    Fix formatting

--- core/src/test/java/com/gentics/mesh/core/node/NodeEndpointETagReferenceTest.java
+++ core/src/test/java/com/gentics/mesh/core/node/NodeEndpointETagReferenceTest.java
@@ -3,15 +3,20 @@ package com.gentics.mesh.core.node;
 import static com.gentics.mesh.http.HttpConstants.ETAG;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 import static com.gentics.mesh.test.TestSize.FULL;
-import static com.gentics.mesh.test.ClientHelper.call;
-import static com.gentics.mesh.test.ClientHelper.callETag;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.gentics.mesh.core.rest.micronode.MicronodeResponse;
 import com.gentics.mesh.core.rest.microschema.impl.MicroschemaCreateRequest;
 import com.gentics.mesh.core.rest.microschema.impl.MicroschemaResponse;
+import com.gentics.mesh.core.rest.node.NodeCreateRequest;
+import com.gentics.mesh.core.rest.node.NodeResponse;
 import com.gentics.mesh.core.rest.node.NodeUpdateRequest;
 import com.gentics.mesh.core.rest.node.field.impl.NodeFieldImpl;
 import com.gentics.mesh.core.rest.node.field.list.impl.MicronodeFieldListImpl;
@@ -24,6 +29,9 @@ import com.gentics.mesh.core.rest.schema.impl.NodeFieldSchemaImpl;
 import com.gentics.mesh.core.rest.schema.impl.SchemaCreateRequest;
 import com.gentics.mesh.core.rest.schema.impl.SchemaResponse;
 import com.gentics.mesh.rest.client.MeshResponse;
+import com.gentics.mesh.test.context.AbstractMeshTest;
+import com.gentics.mesh.test.context.MeshTestSetting;
+
 import io.reactivex.Completable;
 import io.reactivex.CompletableObserver;
 import io.reactivex.Single;
@@ -31,186 +39,157 @@ import io.reactivex.disposables.Disposable;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import com.syncleus.ferma.tx.Tx;
-import com.gentics.mesh.FieldUtil;
-import com.gentics.mesh.core.data.node.Node;
-import com.gentics.mesh.core.rest.node.NodeCreateRequest;
-import com.gentics.mesh.core.rest.node.NodeResponse;
-import com.gentics.mesh.core.rest.schema.SchemaModel;
-import com.gentics.mesh.core.rest.schema.impl.SchemaReferenceImpl;
-import com.gentics.mesh.core.rest.user.NodeReference;
-import com.gentics.mesh.parameter.impl.NodeParametersImpl;
-import com.gentics.mesh.parameter.impl.PagingParametersImpl;
-import com.gentics.mesh.parameter.impl.VersioningParametersImpl;
-import com.gentics.mesh.test.context.AbstractMeshTest;
-import com.gentics.mesh.test.context.MeshTestSetting;
-import org.junit.runner.RunWith;
-import org.reactivestreams.Subscriber;
-
-import java.util.Arrays;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 @MeshTestSetting(useElasticsearch = false, testSize = FULL, startServer = true)
 @RunWith(VertxUnitRunner.class)
 public class NodeEndpointETagReferenceTest extends AbstractMeshTest {
 
-    @Before
-    public void createSchemas(TestContext context) {
-        createReferenceMicroschema().toCompletable()
-        .andThen(createReferenceSchema().toCompletable())
-        .subscribe(testSubscriber(context));
-    }
+	@Before
+	public void createSchemas(TestContext context) {
+		createReferenceMicroschema().toCompletable()
+			.andThen(createReferenceSchema().toCompletable())
+			.subscribe(testSubscriber(context));
+	}
 
-    private CompletableObserver testSubscriber(TestContext context) {
-        Async async = context.async();
-        return new CompletableObserver() {
-            @Override
-            public void onSubscribe(Disposable d) {
-            }
+	private CompletableObserver testSubscriber(TestContext context) {
+		Async async = context.async();
+		return new CompletableObserver() {
+			@Override
+			public void onSubscribe(Disposable d) {
+			}
 
-            @Override
-            public void onComplete() {
-                async.complete();
-            }
+			@Override
+			public void onComplete() {
+				async.complete();
+			}
 
-            @Override
-            public void onError(Throwable e) {
-                context.fail(e);
-            }
-        };
-    }
+			@Override
+			public void onError(Throwable e) {
+				context.fail(e);
+			}
+		};
+	}
 
-    @Test
-    public void testNodeField(TestContext context) {
-        assertChangedReference("Check Node Field", (nodeToCheck, nodeToDelete) -> {
-            nodeToCheck.getFields().put("node", new NodeFieldImpl().setUuid(nodeToDelete.getUuid()));
-        }).subscribe(testSubscriber(context));
-    }
+	@Test
+	public void testNodeField(TestContext context) {
+		assertChangedReference("Check Node Field", (nodeToCheck, nodeToDelete) -> {
+			nodeToCheck.getFields().put("node", new NodeFieldImpl().setUuid(nodeToDelete.getUuid()));
+		}).subscribe(testSubscriber(context));
+	}
 
-    @Test
-    public void testNodeListField(TestContext context) {
-        assertChangedReference("Check Node List Field", (nodeToCheck, nodeToDelete) -> {
-            nodeToCheck.getFields().put("nodeList",
-                new NodeFieldListImpl().setItems(Arrays.asList(new NodeFieldListItemImpl().setUuid(nodeToDelete.getUuid()))));
-        }).subscribe(testSubscriber(context));
-    }
+	@Test
+	public void testNodeListField(TestContext context) {
+		assertChangedReference("Check Node List Field", (nodeToCheck, nodeToDelete) -> {
+			nodeToCheck.getFields().put("nodeList",
+				new NodeFieldListImpl().setItems(Arrays.asList(new NodeFieldListItemImpl().setUuid(nodeToDelete.getUuid()))));
+		}).subscribe(testSubscriber(context));
+	}
 
-    @Test
-    public void testNodeFieldInMicronode(TestContext context) {
-        assertChangedReference("Check Node Field in Microschema Field", (nodeToCheck, nodeToDelete) -> {
-            MicronodeResponse micronode = new MicronodeResponse();
-            micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
-            micronode.getFields().put("m-node", new NodeFieldImpl().setUuid(nodeToDelete.getUuid()));
-            nodeToCheck.getFields().put("micronode", micronode);
-        }).subscribe(testSubscriber(context));
-    }
+	@Test
+	public void testNodeFieldInMicronode(TestContext context) {
+		assertChangedReference("Check Node Field in Microschema Field", (nodeToCheck, nodeToDelete) -> {
+			MicronodeResponse micronode = new MicronodeResponse();
+			micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
+			micronode.getFields().put("m-node", new NodeFieldImpl().setUuid(nodeToDelete.getUuid()));
+			nodeToCheck.getFields().put("micronode", micronode);
+		}).subscribe(testSubscriber(context));
+	}
 
-    @Test
-    public void testNodeListFieldInMicronode(TestContext context) {
-        assertChangedReference("Check Node List Field in Microschema Field", (nodeToCheck, nodeToDelete) -> {
-            MicronodeResponse micronode = new MicronodeResponse();
-            micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
-            micronode.getFields().put("m-nodeList",
-                new NodeFieldListImpl().setItems(Arrays.asList(new NodeFieldListItemImpl().setUuid(nodeToDelete.getUuid()))));
-            nodeToCheck.getFields().put("micronode", micronode);
-        }).subscribe(testSubscriber(context));
-    }
+	@Test
+	public void testNodeListFieldInMicronode(TestContext context) {
+		assertChangedReference("Check Node List Field in Microschema Field", (nodeToCheck, nodeToDelete) -> {
+			MicronodeResponse micronode = new MicronodeResponse();
+			micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
+			micronode.getFields().put("m-nodeList",
+				new NodeFieldListImpl().setItems(Arrays.asList(new NodeFieldListItemImpl().setUuid(nodeToDelete.getUuid()))));
+			nodeToCheck.getFields().put("micronode", micronode);
+		}).subscribe(testSubscriber(context));
+	}
 
-    @Test
-    public void testNodeFieldInMicronodeList(TestContext context) {
-        assertChangedReference("Check Node Field in Microschema Field List", (nodeToCheck, nodeToDelete) -> {
-            MicronodeResponse micronode = new MicronodeResponse();
-            micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
-            micronode.getFields().put("m-node", new NodeFieldImpl().setUuid(nodeToDelete.getUuid()));
-            nodeToCheck.getFields().put("micronodeList", new MicronodeFieldListImpl().setItems(Arrays.asList(micronode)));
-        }).subscribe(testSubscriber(context));
-    }
+	@Test
+	public void testNodeFieldInMicronodeList(TestContext context) {
+		assertChangedReference("Check Node Field in Microschema Field List", (nodeToCheck, nodeToDelete) -> {
+			MicronodeResponse micronode = new MicronodeResponse();
+			micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
+			micronode.getFields().put("m-node", new NodeFieldImpl().setUuid(nodeToDelete.getUuid()));
+			nodeToCheck.getFields().put("micronodeList", new MicronodeFieldListImpl().setItems(Arrays.asList(micronode)));
+		}).subscribe(testSubscriber(context));
+	}
 
-    @Test
-    public void testNodeListFieldinMicronodeList(TestContext context) {
-        assertChangedReference("Check Node List Field in Microschema Field List", (nodeToCheck, nodeToDelete) -> {
-            MicronodeResponse micronode = new MicronodeResponse();
-            micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
-            micronode.getFields().put("m-nodeList",
-                new NodeFieldListImpl().setItems(Arrays.asList(new NodeFieldListItemImpl().setUuid(nodeToDelete.getUuid()))));
-            nodeToCheck.getFields().put("micronodeList", new MicronodeFieldListImpl().setItems(Arrays.asList(micronode)));
-        }).subscribe(testSubscriber(context));
-    }
+	@Test
+	public void testNodeListFieldinMicronodeList(TestContext context) {
+		assertChangedReference("Check Node List Field in Microschema Field List", (nodeToCheck, nodeToDelete) -> {
+			MicronodeResponse micronode = new MicronodeResponse();
+			micronode.setMicroschema(new MicroschemaReferenceImpl().setName("RefMicroschema"));
+			micronode.getFields().put("m-nodeList",
+				new NodeFieldListImpl().setItems(Arrays.asList(new NodeFieldListItemImpl().setUuid(nodeToDelete.getUuid()))));
+			nodeToCheck.getFields().put("micronodeList", new MicronodeFieldListImpl().setItems(Arrays.asList(micronode)));
+		}).subscribe(testSubscriber(context));
+	}
 
-    private Single<MicroschemaResponse> createReferenceMicroschema() {
-        MicroschemaCreateRequest req = new MicroschemaCreateRequest();
-        req.setName("RefMicroschema");
-        req.setFields(Arrays.asList(
-            new NodeFieldSchemaImpl().setName("m-node"),
-            new ListFieldSchemaImpl().setListType("node").setName("m-nodeList")
-        ));
-        return client().createMicroschema(req).toSingle()
-            .flatMap(microSchema ->
-                client().assignMicroschemaToProject(PROJECT_NAME, microSchema.getUuid()).toCompletable()
-                    .andThen(Single.just(microSchema))
-            );
-    }
+	private Single<MicroschemaResponse> createReferenceMicroschema() {
+		MicroschemaCreateRequest req = new MicroschemaCreateRequest();
+		req.setName("RefMicroschema");
+		req.setFields(Arrays.asList(
+			new NodeFieldSchemaImpl().setName("m-node"),
+			new ListFieldSchemaImpl().setListType("node").setName("m-nodeList")));
+		return client().createMicroschema(req).toSingle()
+			.flatMap(microSchema -> client().assignMicroschemaToProject(PROJECT_NAME, microSchema.getUuid()).toCompletable()
+				.andThen(Single.just(microSchema)));
+	}
 
-    private Single<SchemaResponse> createReferenceSchema() {
-        SchemaCreateRequest req = new SchemaCreateRequest();
-        req.setName("RefSchema");
-        req.setFields(Arrays.asList(
-            new NodeFieldSchemaImpl().setName("node"),
-            new ListFieldSchemaImpl().setListType("node").setName("nodeList"),
-            new MicronodeFieldSchemaImpl().setAllowedMicroSchemas("RefMicroschema").setName("micronode"),
-            new ListFieldSchemaImpl().setListType("micronode").setName("micronodeList")
-        ));
-        return client().createSchema(req).toSingle()
-            .flatMap(schema ->
-                client().assignSchemaToProject(PROJECT_NAME, schema.getUuid()).toCompletable()
-                    .andThen(Single.just(schema))
-            );
-    }
+	private Single<SchemaResponse> createReferenceSchema() {
+		SchemaCreateRequest req = new SchemaCreateRequest();
+		req.setName("RefSchema");
+		req.setFields(Arrays.asList(
+			new NodeFieldSchemaImpl().setName("node"),
+			new ListFieldSchemaImpl().setListType("node").setName("nodeList"),
+			new MicronodeFieldSchemaImpl().setAllowedMicroSchemas("RefMicroschema").setName("micronode"),
+			new ListFieldSchemaImpl().setListType("micronode").setName("micronodeList")));
+		return client().createSchema(req).toSingle()
+			.flatMap(schema -> client().assignSchemaToProject(PROJECT_NAME, schema.getUuid()).toCompletable()
+				.andThen(Single.just(schema)));
+	}
 
-    private Single<NodeResponse> createReferenceNode() {
-        return getRootUuid().flatMap(rootUuid -> {
-            NodeCreateRequest req = new NodeCreateRequest();
-            req.setLanguage("en");
-            req.setSchemaName("RefSchema");
-            req.setParentNodeUuid(rootUuid);
-            return client().createNode(PROJECT_NAME, req).toSingle();
-        });
-    }
+	private Single<NodeResponse> createReferenceNode() {
+		return getRootUuid().flatMap(rootUuid -> {
+			NodeCreateRequest req = new NodeCreateRequest();
+			req.setLanguage("en");
+			req.setSchemaName("RefSchema");
+			req.setParentNodeUuid(rootUuid);
+			return client().createNode(PROJECT_NAME, req).toSingle();
+		});
+	}
 
-    private Single<String> getRootUuid() {
-        return client().findProjects().toSingle()
-            .map(it -> it.getData().get(0).getRootNode().getUuid());
-    }
+	private Single<String> getRootUuid() {
+		return client().findProjects().toSingle()
+			.map(it -> it.getData().get(0).getRootNode().getUuid());
+	}
 
-    private Completable assertChangedReference(String assertionName, BiConsumer<NodeResponse, NodeResponse> fieldChanger) {
-        return Single.zip(createReferenceNode(), createReferenceNode(), (nodeToCheck, nodeToDelete) -> {
-            fieldChanger.accept(nodeToCheck, nodeToDelete);
-            return updateNode(nodeToCheck).toCompletable()
-                .andThen(getEtag(nodeToCheck))
-                .flatMapCompletable(oldETag -> client().deleteNode(PROJECT_NAME, nodeToDelete.getUuid()).toCompletable()
-                    .andThen(getEtag(nodeToCheck))
-                    .doOnSuccess(newEtag -> assertNotEquals(assertionName, oldETag, newEtag))
-                    .toCompletable());
-        }).flatMapCompletable(x -> x);
-    }
+	private Completable assertChangedReference(String assertionName, BiConsumer<NodeResponse, NodeResponse> fieldChanger) {
+		return Single.zip(createReferenceNode(), createReferenceNode(), (nodeToCheck, nodeToDelete) -> {
+			fieldChanger.accept(nodeToCheck, nodeToDelete);
+			return updateNode(nodeToCheck).toCompletable()
+				.andThen(getEtag(nodeToCheck))
+				.flatMapCompletable(oldETag -> client().deleteNode(PROJECT_NAME, nodeToDelete.getUuid()).toCompletable()
+					.andThen(getEtag(nodeToCheck))
+					.doOnSuccess(newEtag -> assertNotEquals(assertionName, oldETag, newEtag))
+					.toCompletable());
+		}).flatMapCompletable(x -> x);
+	}
 
-    private Single<NodeResponse> updateNode(NodeResponse node) {
-        NodeUpdateRequest req = new NodeUpdateRequest();
-        req.setLanguage(node.getLanguage());
-        req.setFields(node.getFields());
-        req.setVersion(node.getVersion());
-        return client().updateNode(PROJECT_NAME, node.getUuid(), req).toSingle();
-    }
+	private Single<NodeResponse> updateNode(NodeResponse node) {
+		NodeUpdateRequest req = new NodeUpdateRequest();
+		req.setLanguage(node.getLanguage());
+		req.setFields(node.getFields());
+		req.setVersion(node.getVersion());
+		return client().updateNode(PROJECT_NAME, node.getUuid(), req).toSingle();
+	}
 
-    private Single<String> getEtag(NodeResponse node) {
-        return Single.defer(() -> {
-            MeshResponse<NodeResponse> response = client().findNodeByUuid(PROJECT_NAME, node.getUuid()).invoke();
-            return response.rxSetHandler().map(ignore -> response.getRawResponse().getHeader(ETAG));
-        });
-    }
+	private Single<String> getEtag(NodeResponse node) {
+		return Single.defer(() -> {
+			MeshResponse<NodeResponse> response = client().findNodeByUuid(PROJECT_NAME, node.getUuid()).invoke();
+			return response.rxSetHandler().map(ignore -> response.getRawResponse().getHeader(ETAG));
+		});
+	}
 }
