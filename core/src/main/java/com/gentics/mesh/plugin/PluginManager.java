cf5120ee4e00fccf17fdcf9dbf2042e7ed95c1a4
Johannes Schüth <git@jotschi.de>
2018-03-05 21:02:47 +0100

    Add plugin manifest handling

--- core/src/main/java/com/gentics/mesh/plugin/PluginManager.java
+++ core/src/main/java/com/gentics/mesh/plugin/PluginManager.java
@@ -2,20 +2,28 @@ package com.gentics.mesh.plugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.apache.commons.io.IOUtils;
+
 import com.gentics.mesh.etc.config.MeshOptions;
+import com.gentics.mesh.json.JsonUtil;
 
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
 @Singleton
 public class PluginManager {
+
+	private static final String PLUGIN_MANIFEST_FILENAME = "mesh-plugin.json";
 
 	private static final Logger log = LoggerFactory.getLogger(PluginManager.class);
 
@@ -38,11 +46,30 @@ public class PluginManager {
 		} catch (IOException e) {
 			log.error("Error while reading plugins from plugin folder {" + pluginFolder + "}", e);
 		}
-
 	}
 
 	private void registerPlugin(File file) {
 		log.info("Registering plugin {" + file + "}");
+		try (ZipFile zipFile = new ZipFile(file)) {
+
+			// Load the manifest
+			ZipEntry entry = zipFile.getEntry(PLUGIN_MANIFEST_FILENAME);
+			if (entry == null) {
+				throw new PluginException("The plugin manifest file {" + PLUGIN_MANIFEST_FILENAME + "} could not be found in the plugin archive.");
+			}
+			try (InputStream ins = zipFile.getInputStream(entry)) {
+				String json = IOUtils.toString(ins);
+				PluginManifest manifest = JsonUtil.readValue(json, PluginManifest.class);
+				manifest.validate();
+			}
+
+			// Load the jar
+
+			// Extract the static resources
+		} catch (Exception e) {
+			log.error("Error while loading plugin from file {" + file + "}", e);
+		}
+
 	}
 
 }
--- plugin-api/src/main/java/com/gentics/mesh/plugin/rest/RestExtensionScope.java
+++ plugin-api/src/main/java/com/gentics/mesh/plugin/rest/RestExtensionScope.java
@@ -1,5 +1,9 @@
 package com.gentics.mesh.plugin.rest;
 
+/**
+ * Scope of the REST extension. The extension can either be scoped globally (e.g. /api/v1/plugins/hello-world) or scoped to a project (e.g:
+ * /api/v1/demo/plugins/hello-world).
+ */
 public enum RestExtensionScope {
 
 	GLOBAL,
