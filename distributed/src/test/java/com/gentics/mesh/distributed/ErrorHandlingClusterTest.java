f5025d0d1c3cc3ac88610e5278b37b4d82da7aee
Johannes Schüth <j.schueth@gentics.com>
2018-03-07 15:51:03 +0100

    Add additional cluster start/stop testcase

--- distributed/src/test/java/com/gentics/mesh/distributed/ErrorHandlingClusterTest.java
+++ distributed/src/test/java/com/gentics/mesh/distributed/ErrorHandlingClusterTest.java
@@ -5,6 +5,7 @@ import static com.gentics.mesh.util.TokenUtil.randomToken;
 import static com.gentics.mesh.util.UUIDUtil.randomUUID;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -98,6 +99,76 @@ public class ErrorHandlingClusterTest extends AbstractClusterTest {
 				call(() -> server.client().findProjectByUuid(project.getUuid()));
 			}
 		}
+	}
+
+	/**
+	 * Start and stop the instances in the cluster alternately.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testRestartingTwoSlaves() throws Exception {
+		List<ProjectResponse> responses = new ArrayList<>();
+
+		// Node A: Create Project
+		ProjectCreateRequest request = new ProjectCreateRequest().setName("initial").setSchemaRef("folder");
+		responses.add(call(() -> serverA.client().createProject(request)));
+		System.out.println(call(() -> serverA.client().me()).toJson());
+
+		String dataPathPostfix = randomToken();
+		MeshDockerServer serverB = addSlave("dockerCluster" + clusterPostFix, "nodeB", dataPathPostfix, true)
+			.awaitStartup(20).login();
+
+		// Stop and restart each of the nodes alternatively and create projects in between each phase of the start/stop actions.
+		for (int i = 0; i < 10; i++) {
+			boolean handleFirst = i % 2 == 0;
+			MeshDockerServer server = handleFirst ? serverA : serverB;
+			MeshDockerServer otherServer = handleFirst ? serverB : serverA;
+			System.out.println("Run {" + i + "} handling node {" + server.getNodeName() + "}");
+
+			// Before stop
+			ProjectCreateRequest request2 = new ProjectCreateRequest().setName(server.getNodeName() + "A" + i).setSchemaRef("folder");
+			responses.add(call(() -> server.client().createProject(request2)));
+
+			Thread.sleep(10_000);
+			System.out.println("Stopping {" + server.getNodeName() + "}");
+			server.stop();
+
+			// After stop
+			ProjectCreateRequest request3 = new ProjectCreateRequest().setName(otherServer.getNodeName() + "B" + i).setSchemaRef("folder");
+			responses.add(call(() -> otherServer.client().createProject(request3)));
+
+			// Now start the server again but clear the data directory before
+			Thread.sleep(10_000);
+			System.out.println("Starting server {" + server.getNodeName() + "}");
+			MeshDockerServer serverAfterRestart = addSlave("dockerCluster" + clusterPostFix, server.getNodeName(), server.getDataPathPostfix(), true)
+				.awaitStartup(30)
+				.login();
+			if (handleFirst) {
+				serverA = serverAfterRestart;
+			} else {
+				serverB = serverAfterRestart;
+			}
+
+			// After restart
+			ProjectCreateRequest request4 = new ProjectCreateRequest().setName(server.getNodeName() + "C" + i).setSchemaRef("folder");
+			responses.add(call(() -> serverAfterRestart.client().createProject(request4)));
+
+			// Check that all projects can be found across all nodes.
+			Thread.sleep(10_000);
+			List<MeshDockerServer> servers = Arrays.asList(serverA, serverB);
+			for (MeshDockerServer currentServer : servers) {
+				for (ProjectResponse project : responses) {
+					try {
+						call(() -> currentServer.client().findProjectByUuid(project.getUuid()));
+					} catch (AssertionError e) {
+						e.printStackTrace();
+						fail("Could not load project {" + project.getName() + "} from server {" + currentServer.getNodeName() + "}");
+					}
+				}
+			}
+		}
+
 	}
 
 	/**
