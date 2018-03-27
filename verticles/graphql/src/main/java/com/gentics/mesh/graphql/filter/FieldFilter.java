43090953b11040cb77e318b4a27410108dad8b82
Johannes Schüth <j.schueth@gentics.com>
2018-03-27 11:07:37 +0200

    Update packages to fix compile error

--- verticles/graphql/src/main/java/com/gentics/mesh/graphql/filter/FieldFilter.java
+++ verticles/graphql/src/main/java/com/gentics/mesh/graphql/filter/FieldFilter.java
@@ -9,12 +9,12 @@ import com.gentics.mesh.core.rest.common.FieldTypes;
 import com.gentics.mesh.core.rest.schema.FieldSchema;
 import com.gentics.mesh.core.rest.schema.SchemaModel;
 import com.gentics.mesh.graphql.context.GraphQLContext;
-import com.gentics.mesh.graphqlfilter.filter.BooleanFilter;
-import com.gentics.mesh.graphqlfilter.filter.DateFilter;
-import com.gentics.mesh.graphqlfilter.filter.FilterField;
-import com.gentics.mesh.graphqlfilter.filter.MainFilter;
-import com.gentics.mesh.graphqlfilter.filter.NumberFilter;
-import com.gentics.mesh.graphqlfilter.filter.StringFilter;
+import com.gentics.graphqlfilter.filter.BooleanFilter;
+import com.gentics.graphqlfilter.filter.DateFilter;
+import com.gentics.graphqlfilter.filter.FilterField;
+import com.gentics.graphqlfilter.filter.MainFilter;
+import com.gentics.graphqlfilter.filter.NumberFilter;
+import com.gentics.graphqlfilter.filter.StringFilter;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -27,79 +27,84 @@ import java.util.stream.Stream;
  * Filters by the fields of a node with a certain schema.
  */
 public class FieldFilter extends MainFilter<GraphFieldContainer> {
-    private static final String NAME_PREFIX = "FieldFilter.";
+	private static final String NAME_PREFIX = "FieldFilter.";
 
-    // TODO Remove this after all types are supported
-    private static final Set<String> availableTypes = Stream.of(
-        FieldTypes.STRING, FieldTypes.HTML, FieldTypes.DATE, FieldTypes.BOOLEAN, FieldTypes.NUMBER
-    ).map(FieldTypes::toString).collect(Collectors.toSet());
+	// TODO Remove this after all types are supported
+	private static final Set<String> availableTypes = Stream.of(
+		FieldTypes.STRING, FieldTypes.HTML, FieldTypes.DATE, FieldTypes.BOOLEAN, FieldTypes.NUMBER).map(FieldTypes::toString)
+		.collect(Collectors.toSet());
 
-    /**
-     * Creates a new filter for the provided schema
-     * @param context The context of the current query
-     * @param container The schema model to create the filter for
-     */
-    public static FieldFilter filter(GraphQLContext context, SchemaModel container) {
-        return context.getOrStore(NAME_PREFIX + container.getName(), () -> new FieldFilter(container));
-    }
+	/**
+	 * Creates a new filter for the provided schema
+	 * 
+	 * @param context
+	 *            The context of the current query
+	 * @param container
+	 *            The schema model to create the filter for
+	 */
+	public static FieldFilter filter(GraphQLContext context, SchemaModel container) {
+		return context.getOrStore(NAME_PREFIX + container.getName(), () -> new FieldFilter(container));
+	}
 
-    private final SchemaModel schema;
+	private final SchemaModel schema;
 
-    private FieldFilter(SchemaModel container) {
-        super(container.getName() + "FieldFilter", "Filters by fields");
-        this.schema = container;
-    }
+	private FieldFilter(SchemaModel container) {
+		super(container.getName() + "FieldFilter", "Filters by fields");
+		this.schema = container;
+	}
 
-    @Override
-    protected List<FilterField<GraphFieldContainer, ?>> getFilters() {
-        return schema.getFields().stream()
-            // filters fields where the Filter is not implemented
-            // TODO remove this after all types are supported
-            .filter(field -> availableTypes.contains(field.getType()))
-            .map(this::createFieldFilter)
-            .collect(Collectors.toList());
-    }
+	@Override
+	protected List<FilterField<GraphFieldContainer, ?>> getFilters() {
+		return schema.getFields().stream()
+			// filters fields where the Filter is not implemented
+			// TODO remove this after all types are supported
+			.filter(field -> availableTypes.contains(field.getType()))
+			.map(this::createFieldFilter)
+			.collect(Collectors.toList());
+	}
 
-    /**
-     * Creates a filter for a single field of a schema. Currently not all field types are supported.
-     * @param fieldSchema The field schema to create the filter for
-     */
-    private FilterField<GraphFieldContainer, ?> createFieldFilter(FieldSchema fieldSchema) {
-        String schemaName = schema.getName();
-        String name = fieldSchema.getName();
-        String description = "Filters by the field " + name;
-        FieldTypes type = FieldTypes.valueByName(fieldSchema.getType());
-        switch (type) {
-            case STRING:
-                return new FieldMappedFilter<>(name, description, StringFilter.filter(),
-                    node -> getOrNull(node.getString(name), StringGraphField::getString), schemaName);
-            case HTML:
-                return new FieldMappedFilter<>(name, description, StringFilter.filter(),
-                    node -> getOrNull(node.getHtml(name), HtmlGraphField::getHTML), schemaName);
-            case DATE:
-                return new FieldMappedFilter<>(name, description, DateFilter.filter(),
-                    node -> getOrNull(node.getDate(name), DateGraphField::getDate), schemaName);
-            case BOOLEAN:
-                return new FieldMappedFilter<>(name, description, BooleanFilter.filter(),
-                    node -> getOrNull(node.getBoolean(name), BooleanGraphField::getBoolean), schemaName);
-            case NUMBER:
-                return new FieldMappedFilter<>(name, description, NumberFilter.filter(),
-                    node -> getOrNull(node.getNumber(name), val -> new BigDecimal(val.getNumber().toString())), schemaName);
-            // TODO correctly implement other types
-            case BINARY:
-            case LIST:
-            case NODE:
-            case MICRONODE:
-            default:
-                throw new RuntimeException("Unexpected type " + type);
-        }
-    }
+	/**
+	 * Creates a filter for a single field of a schema. Currently not all field types are supported.
+	 * 
+	 * @param fieldSchema
+	 *            The field schema to create the filter for
+	 */
+	private FilterField<GraphFieldContainer, ?> createFieldFilter(FieldSchema fieldSchema) {
+		String schemaName = schema.getName();
+		String name = fieldSchema.getName();
+		String description = "Filters by the field " + name;
+		FieldTypes type = FieldTypes.valueByName(fieldSchema.getType());
+		switch (type) {
+		case STRING:
+			return new FieldMappedFilter<>(name, description, StringFilter.filter(),
+				node -> getOrNull(node.getString(name), StringGraphField::getString), schemaName);
+		case HTML:
+			return new FieldMappedFilter<>(name, description, StringFilter.filter(),
+				node -> getOrNull(node.getHtml(name), HtmlGraphField::getHTML), schemaName);
+		case DATE:
+			return new FieldMappedFilter<>(name, description, DateFilter.filter(),
+				node -> getOrNull(node.getDate(name), DateGraphField::getDate), schemaName);
+		case BOOLEAN:
+			return new FieldMappedFilter<>(name, description, BooleanFilter.filter(),
+				node -> getOrNull(node.getBoolean(name), BooleanGraphField::getBoolean), schemaName);
+		case NUMBER:
+			return new FieldMappedFilter<>(name, description, NumberFilter.filter(),
+				node -> getOrNull(node.getNumber(name), val -> new BigDecimal(val.getNumber().toString())), schemaName);
+		// TODO correctly implement other types
+		case BINARY:
+		case LIST:
+		case NODE:
+		case MICRONODE:
+		default:
+			throw new RuntimeException("Unexpected type " + type);
+		}
+	}
 
-    private static <T, R> R getOrNull(T nullableValue, Function<T, R> mapper) {
-        if (nullableValue == null) {
-            return null;
-        } else {
-            return mapper.apply(nullableValue);
-        }
-    }
+	private static <T, R> R getOrNull(T nullableValue, Function<T, R> mapper) {
+		if (nullableValue == null) {
+			return null;
+		} else {
+			return mapper.apply(nullableValue);
+		}
+	}
 }
--- verticles/graphql/src/main/java/com/gentics/mesh/graphql/filter/SchemaFilter.java
+++ verticles/graphql/src/main/java/com/gentics/mesh/graphql/filter/SchemaFilter.java
@@ -3,11 +3,11 @@ package com.gentics.mesh.graphql.filter;
 import com.gentics.mesh.core.data.Project;
 import com.gentics.mesh.core.data.schema.SchemaContainer;
 import com.gentics.mesh.graphql.context.GraphQLContext;
-import com.gentics.mesh.graphqlfilter.filter.DateFilter;
-import com.gentics.mesh.graphqlfilter.filter.FilterField;
-import com.gentics.mesh.graphqlfilter.filter.MainFilter;
-import com.gentics.mesh.graphqlfilter.filter.MappedFilter;
-import com.gentics.mesh.graphqlfilter.filter.StringFilter;
+import com.gentics.graphqlfilter.filter.DateFilter;
+import com.gentics.graphqlfilter.filter.FilterField;
+import com.gentics.graphqlfilter.filter.MainFilter;
+import com.gentics.graphqlfilter.filter.MappedFilter;
+import com.gentics.graphqlfilter.filter.StringFilter;
 import graphql.schema.GraphQLEnumType;
 import graphql.schema.GraphQLEnumValueDefinition;
 
@@ -21,38 +21,37 @@ import java.util.stream.StreamSupport;
  */
 public class SchemaFilter extends MainFilter<SchemaContainer> {
 
-    private static final String NAME = "SchemaFilter";
+	private static final String NAME = "SchemaFilter";
 
-    public static SchemaFilter filter(GraphQLContext context) {
-        return context.getOrStore(NAME, () -> new SchemaFilter(context));
-    }
+	public static SchemaFilter filter(GraphQLContext context) {
+		return context.getOrStore(NAME, () -> new SchemaFilter(context));
+	}
 
-    private final GraphQLContext context;
+	private final GraphQLContext context;
 
-    private SchemaFilter(GraphQLContext context) {
-        super(NAME, "Filters schemas");
-        this.context = context;
-    }
+	private SchemaFilter(GraphQLContext context) {
+		super(NAME, "Filters schemas");
+		this.context = context;
+	}
 
-    private GraphQLEnumType schemaEnum() {
-        Project project = context.getProject();
-        List<GraphQLEnumValueDefinition> values = StreamSupport.stream(project.getSchemaContainerRoot().findAllIt().spliterator(), false)
-            .map(schema -> {
-                String name = schema.getName();
-                return new GraphQLEnumValueDefinition(name, name, schema.getUuid());
-            }).collect(Collectors.toList());
+	private GraphQLEnumType schemaEnum() {
+		Project project = context.getProject();
+		List<GraphQLEnumValueDefinition> values = StreamSupport.stream(project.getSchemaContainerRoot().findAllIt().spliterator(), false)
+			.map(schema -> {
+				String name = schema.getName();
+				return new GraphQLEnumValueDefinition(name, name, schema.getUuid());
+			}).collect(Collectors.toList());
 
-        return new GraphQLEnumType("SchemaEnum", "Enumerates all schemas", values);
-    }
+		return new GraphQLEnumType("SchemaEnum", "Enumerates all schemas", values);
+	}
 
-    @Override
-    protected List<FilterField<SchemaContainer, ?>> getFilters() {
-        return Arrays.asList(
-            new MappedFilter<>("uuid", "Filters by uuid", StringFilter.filter(), SchemaContainer::getUuid),
-            new MappedFilter<>("name", "Filters by name", StringFilter.filter(), SchemaContainer::getName),
-            FilterField.create("is", "Filters by schema", schemaEnum(), uuid -> schema -> schema.getUuid().equals(uuid)),
-            new MappedFilter<>("created", "Filters by schema creation timestamp", DateFilter.filter(), SchemaContainer::getCreationTimestamp),
-            new MappedFilter<>("edited", "Filters by schema update timestamp", DateFilter.filter(), SchemaContainer::getLastEditedTimestamp)
-        );
-    }
+	@Override
+	protected List<FilterField<SchemaContainer, ?>> getFilters() {
+		return Arrays.asList(
+			new MappedFilter<>("uuid", "Filters by uuid", StringFilter.filter(), SchemaContainer::getUuid),
+			new MappedFilter<>("name", "Filters by name", StringFilter.filter(), SchemaContainer::getName),
+			FilterField.create("is", "Filters by schema", schemaEnum(), uuid -> schema -> schema.getUuid().equals(uuid)),
+			new MappedFilter<>("created", "Filters by schema creation timestamp", DateFilter.filter(), SchemaContainer::getCreationTimestamp),
+			new MappedFilter<>("edited", "Filters by schema update timestamp", DateFilter.filter(), SchemaContainer::getLastEditedTimestamp));
+	}
 }
