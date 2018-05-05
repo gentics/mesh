e7d80773e980c1e72e2be9f80ba89951a31ecadf
Johannes Schüth <git@jotschi.de>
2018-05-05 14:24:17 +0200

    Add working integrated keycloak test

--- services/keycloak-auth/src/test/java/com/gentics/mesh/auth/keycloak/KeycloakAuthTest.java
+++ services/keycloak-auth/src/test/java/com/gentics/mesh/auth/keycloak/KeycloakAuthTest.java
@@ -1,16 +1,54 @@
 package com.gentics.mesh.auth.keycloak;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.ClassRule;
 import org.junit.Test;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.oauth2.OAuth2Auth;
+import io.vertx.ext.auth.oauth2.OAuth2FlowType;
+import io.vertx.ext.auth.oauth2.providers.KeycloakAuth;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.OAuth2AuthHandler;
 
 public class KeycloakAuthTest {
 
 	@ClassRule
-	KeycloakContainer keycloak = new KeycloakContainer();
+	public static KeycloakContainer keycloak = new KeycloakContainer()
+		.withRealmFile("src/test/resources/realm.json")
+		.waitStartup();
 
 	@Test
-	public void testKeyCloak() {
+	public void testKeyCloak() throws IOException {
+		int keycloakPort = keycloak.getFirstMappedPort();
+		System.out.println("Port: " + keycloakPort);
+		Vertx vertx = Vertx.vertx();
 
+		Router router = Router.router(vertx);
+		JsonObject config = loadJson("/keycloak-installation.json").put("auth-server-url", "http://localhost:" + keycloakPort + "/auth");
+		OAuth2Auth keyCloakAuth = KeycloakAuth.create(vertx, OAuth2FlowType.AUTH_CODE, config);
+
+		OAuth2AuthHandler oauth2 = OAuth2AuthHandler.create(keyCloakAuth,
+			"http://localhost:8080");
+
+		oauth2.setupCallback(router.get("/callback"));
+
+		router.route("/protected/*").handler(oauth2);
+		router.route("/protected/*").handler(rc -> {
+			rc.response().end("Welcome to the protected resource!");
+		});
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+		System.in.read();
+
+	}
+
+	private JsonObject loadJson(String path) throws IOException {
+		return new JsonObject(IOUtils.toString(getClass().getResource(path), Charset.defaultCharset()));
 	}
 
 }
--- services/keycloak-auth/src/test/java/com/gentics/mesh/auth/keycloak/KeycloakContainer.java
+++ services/keycloak-auth/src/test/java/com/gentics/mesh/auth/keycloak/KeycloakContainer.java
@@ -1,15 +1,29 @@
 package com.gentics.mesh.auth.keycloak;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.Wait;
 
 public class KeycloakContainer extends GenericContainer<KeycloakContainer> {
 
 	private static final String VERSION = "3.4.3.Final";
 
+	private static final Logger log = LoggerFactory.getLogger(KeycloakContainer.class);
+
+	private Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(log);
+
+	private String realmPath = null;
+
 	public KeycloakContainer() {
-		super("jboss:keycloak:" + VERSION);
+		super("jboss/keycloak:" + VERSION);
 	}
 
 	@Override
@@ -18,6 +32,30 @@ public class KeycloakContainer extends GenericContainer<KeycloakContainer> {
 		addEnv("KEYCLOAK_PASSWORD", "admin");
 
 		setExposedPorts(Arrays.asList(8080));
+		setLogConsumers(Arrays.asList(logConsumer));
 		setStartupAttempts(1);
+
+		List<String> args = new ArrayList<>();
+		args.add("-b");
+		args.add("0.0.0.0");
+		args.add("-Dkeycloak.migration.usersExportStrategy=SAME_FILE");
+		args.add("-Dkeycloak.migration.strategy=OVERWRITE_EXISTING");
+
+		if (realmPath != null) {
+			String containerPath = "/opt/jboss/keycloak/realm.json";
+			addFileSystemBind(new File(realmPath).getAbsolutePath(), containerPath, BindMode.READ_ONLY);
+			args.add("-Dkeycloak.import=" + containerPath);
+		}
+		withCommand(args.stream().toArray(String[]::new));
+	}
+
+	public KeycloakContainer waitStartup() {
+		waitingFor(Wait.forListeningPort());
+		return this;
+	}
+
+	public KeycloakContainer withRealmFile(String path) {
+		this.realmPath = path;
+		return this;
 	}
 }
