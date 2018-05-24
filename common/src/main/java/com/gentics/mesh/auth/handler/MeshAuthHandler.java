cbef592545dcf29e4910652c48c9217de5674e8c
Johannes Schüth <j.schueth@gentics.com>
2018-05-24 19:09:29 +0200

    Add javadoc

--- common/src/main/java/com/gentics/mesh/auth/handler/MeshAuthHandler.java
+++ common/src/main/java/com/gentics/mesh/auth/handler/MeshAuthHandler.java
@@ -4,14 +4,23 @@ import io.vertx.ext.auth.User;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.AuthHandler;
 
+/**
+ * Common interface all all custom Gentics Mesh Auth handlers.
+ */
 public interface MeshAuthHandler extends AuthHandler {
 
+	/**
+	 * Finish the request with code 401.
+	 * 
+	 * @param context
+	 */
 	default void handle401(RoutingContext context) {
 		context.fail(401);
 	}
 
 	/**
 	 * Authorization is done internally via roles - No need to handle it here
+	 * 
 	 * @param user
 	 * @param ctx
 	 */
