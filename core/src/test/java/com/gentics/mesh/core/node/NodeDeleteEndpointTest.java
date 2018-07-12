4c69b7ce32ffac5db6d12c833167372190dd2a6b
Johannes Schüth <git@jotschi.de>
2018-07-12 21:46:31 +0200

    Add release deletion test

--- core/src/test/java/com/gentics/mesh/core/node/NodeDeleteEndpointTest.java
+++ core/src/test/java/com/gentics/mesh/core/node/NodeDeleteEndpointTest.java
@@ -1,10 +1,12 @@
 package com.gentics.mesh.core.node;
 
 import static com.gentics.mesh.test.ClientHelper.call;
+import static com.gentics.mesh.test.TestDataProvider.INITIAL_RELEASE_NAME;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 import static com.gentics.mesh.test.TestSize.FULL;
 import static com.gentics.mesh.test.util.MeshAssert.failingLatch;
 import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
+import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNull;
 
@@ -14,9 +16,15 @@ import java.util.concurrent.CountDownLatch;
 
 import org.junit.Test;
 
+import com.gentics.mesh.FieldUtil;
 import com.gentics.mesh.core.data.node.Node;
+import com.gentics.mesh.core.rest.admin.migration.MigrationStatus;
+import com.gentics.mesh.core.rest.node.NodeCreateRequest;
 import com.gentics.mesh.core.rest.node.NodeResponse;
+import com.gentics.mesh.core.rest.node.NodeUpdateRequest;
 import com.gentics.mesh.core.rest.release.ReleaseCreateRequest;
+import com.gentics.mesh.core.rest.schema.impl.SchemaReferenceImpl;
+import com.gentics.mesh.parameter.VersioningParameters;
 import com.gentics.mesh.parameter.impl.DeleteParametersImpl;
 import com.gentics.mesh.parameter.impl.PublishParametersImpl;
 import com.gentics.mesh.parameter.impl.VersioningParametersImpl;
@@ -55,12 +63,12 @@ public class NodeDeleteEndpointTest extends AbstractMeshTest {
 		call(() -> client().takeNodeOffline(PROJECT_NAME, uuid, new PublishParametersImpl().setRecursive(true)));
 
 		NodeResponse response = call(
-				() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(initialReleaseUuid())));
+			() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(initialReleaseUuid())));
 		assertThat(response.getAvailableLanguages()).as("The node should have two container").hasSize(2);
 
 		// Also verify that the node is loadable in the other release
 		NodeResponse responseForRelease = call(
-				() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease("newRelease")));
+			() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease("newRelease")));
 		assertThat(responseForRelease.getAvailableLanguages()).as("The node should have two container").hasSize(2);
 
 		// Delete first language container: german
@@ -68,8 +76,8 @@ public class NodeDeleteEndpointTest extends AbstractMeshTest {
 		call(() -> client().deleteNode(PROJECT_NAME, uuid, "de"));
 		response = call(() -> client().findNodeByUuid(PROJECT_NAME, uuid));
 		assertThat(response.getAvailableLanguages()).as("The node should only have a single container/language").hasSize(1);
-		assertThatSubNodesExist(childrenUuids,initialReleaseUuid());
-		assertThatSubNodesExist(childrenUuids,releaseName);
+		assertThatSubNodesExist(childrenUuids, initialReleaseUuid());
+		assertThatSubNodesExist(childrenUuids, releaseName);
 
 		// Delete the second language container (english) - The delete should fail since this would be the last language for this node
 		call(() -> client().deleteNode(PROJECT_NAME, uuid, "en"), BAD_REQUEST, "node_error_delete_failed_last_container_for_release");
@@ -78,32 +86,101 @@ public class NodeDeleteEndpointTest extends AbstractMeshTest {
 
 		// Delete the whole node without the recursive flag - This should fail due to the child element check
 		call(() -> client().deleteNode(PROJECT_NAME, uuid, new DeleteParametersImpl().setRecursive(false)), BAD_REQUEST,
-				"node_error_delete_failed_node_has_children");
+			"node_error_delete_failed_node_has_children");
 		assertThatSubNodesExist(childrenUuids, releaseName);
 		assertThatSubNodesExist(childrenUuids, initialReleaseUuid());
-		
+
 		// Delete the second language container (english) and use the recursive flag. The node and all subnodes should have been removed in the current release
 		call(() -> client().deleteNode(PROJECT_NAME, uuid, "en", new DeleteParametersImpl().setRecursive(true)));
 		// Verify that the node is still loadable in the initial release
 		call(() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(initialReleaseUuid())));
-		
+
 		// TODO BUG - Issue #119
 		NodeResponse nodeResponse = call(() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(releaseName)));
-		assertNull("We currently expect the node to be returned but without any contents.",nodeResponse.getLanguage());
-		//call(() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(releaseName)), NOT_FOUND,
-		//		"object_not_found_for_uuid", uuid);
+		assertNull("We currently expect the node to be returned but without any contents.", nodeResponse.getLanguage());
+		// call(() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(releaseName)), NOT_FOUND,
+		// "object_not_found_for_uuid", uuid);
 		assertThatSubNodesWereDeleted(childrenUuids, releaseName);
 		assertThatSubNodesExist(childrenUuids, initialReleaseUuid());
+	}
+
+	@Test
+	public void testDeleteNodeFromRelease() {
+		grantAdminRole();
+		final String schemaUuid = tx(() -> schemaContainer("content").getUuid());
+		final String parentNodeUuid = tx(() -> folder("news").getUuid());
+		final String SECOND_RELEASE_NAME = "release2";
+
+		// 1. Create node in release a
+		NodeCreateRequest request = new NodeCreateRequest();
+		SchemaReferenceImpl schemaReference = new SchemaReferenceImpl();
+		schemaReference.setName("content");
+		schemaReference.setUuid(schemaUuid);
+		request.getFields().put("teaser", FieldUtil.createStringField("some teaser"));
+		request.getFields().put("slug", FieldUtil.createStringField("new-page.html"));
+		request.getFields().put("content", FieldUtil.createStringField("Blessed mealtime again!"));
+		request.setSchema(schemaReference);
+		request.setLanguage("en");
+		request.setParentNodeUuid(parentNodeUuid);
+		NodeResponse response = call(() -> client().createNode(PROJECT_NAME, request));
+		String uuid = response.getUuid();
+
+		// 2. Create two drafts
+		updateNode(1, uuid, INITIAL_RELEASE_NAME);
+		updateNode(2, uuid, INITIAL_RELEASE_NAME);
+
+		// 3. Publish node
+		call(() -> client().publishNode(PROJECT_NAME, uuid));
+
+		// 4. Create two more drafts
+		updateNode(3, uuid, INITIAL_RELEASE_NAME);
+		updateNode(4, uuid, INITIAL_RELEASE_NAME);
+
+		// 5. Create new release
+		waitForJobs(() -> {
+			ReleaseCreateRequest releaseCreateRequest = new ReleaseCreateRequest();
+			releaseCreateRequest.setName(SECOND_RELEASE_NAME);
+			call(() -> client().createRelease(PROJECT_NAME, releaseCreateRequest));
+		}, MigrationStatus.COMPLETED, 1);
+
+		// Create two drafts in release A
+		updateNode(5, uuid, INITIAL_RELEASE_NAME);
+		updateNode(6, uuid, INITIAL_RELEASE_NAME);
+
+		// Create two drafts in release B
+		updateNode(5, uuid, SECOND_RELEASE_NAME);
+		updateNode(6, uuid, SECOND_RELEASE_NAME);
+
+		// Delete node in release A
+		call(() -> client().deleteNode(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(INITIAL_RELEASE_NAME)));
+
+		// Check that the node is still alive in the other release
+		call(() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(SECOND_RELEASE_NAME)));
+		// And deleted in the first release
+		call(() -> client().findNodeByUuid(PROJECT_NAME, uuid, new VersioningParametersImpl().setRelease(INITIAL_RELEASE_NAME)), NOT_FOUND,
+			"object_not_found_for_version");
+	}
+
+	private void updateNode(int i, String uuid, String release) {
+		VersioningParameters param = new VersioningParametersImpl().setRelease(release);
+		NodeResponse response = call(() -> client().findNodeByUuid(PROJECT_NAME, uuid, param));
+		NodeUpdateRequest nodeUpdateRequest = new NodeUpdateRequest();
+		nodeUpdateRequest.getFields().put("slug", FieldUtil.createStringField("new-page" + i + ".html"));
+		nodeUpdateRequest.setVersion(response.getVersion());
+		nodeUpdateRequest.setLanguage("en");
+		call(() -> client().updateNode(PROJECT_NAME, response.getUuid(), nodeUpdateRequest, param));
 	}
 
 	private void assertThatSubNodesWereDeleted(Set<String> childrenUuids, String releaseName) {
 		// Verify that all sub nodes have been deleted as well.
 		for (String childUuid : childrenUuids) {
 			System.out.println("Checking child: " + childUuid);
-			NodeResponse nodeResponse = call(() -> client().findNodeByUuid(PROJECT_NAME, childUuid, new VersioningParametersImpl().setRelease(releaseName)));
-			assertNull("We currently expect the node to be returned but without any contents.",nodeResponse.getLanguage());
+			NodeResponse nodeResponse = call(
+				() -> client().findNodeByUuid(PROJECT_NAME, childUuid, new VersioningParametersImpl().setRelease(releaseName)));
+			assertNull("We currently expect the node to be returned but without any contents.", nodeResponse.getLanguage());
 			// TODO BUG - Issue #119
-			// call(() -> client().findNodeByUuid(PROJECT_NAME, childUuid, new VersioningParametersImpl().setRelease(releaseName)), NOT_FOUND, "object_not_found_for_uuid", childUuid);
+			// call(() -> client().findNodeByUuid(PROJECT_NAME, childUuid, new VersioningParametersImpl().setRelease(releaseName)), NOT_FOUND,
+			// "object_not_found_for_uuid", childUuid);
 		}
 	}
 
