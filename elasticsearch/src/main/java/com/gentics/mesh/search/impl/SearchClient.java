94b805d85dfa12f0f6d736553b42010bf3bcfcda
Sebastian Vogel <s.vogel@gentics.com>
2018-11-09 10:53:17 +0100

    Improve formatting

--- elasticsearch/src/main/java/com/gentics/mesh/search/impl/SearchClient.java
+++ elasticsearch/src/main/java/com/gentics/mesh/search/impl/SearchClient.java
@@ -31,25 +31,25 @@ public class SearchClient extends ElasticsearchOkClient<JsonObject> {
 	public Single<Boolean> hasIngestProcessor(String... processorName) {
 		Set<String> processorNames = new HashSet<>(Arrays.asList(processorName));
 		return this.nodesInfo().async()
-				.filter(response -> response.containsKey("nodes"))
-				.map(response -> response.getJsonObject("nodes"))
-				// we need to find one node which contains
-				.map(nodes ->
-						nodes.stream()
-							.map(entry -> nodes.getJsonObject(entry.getKey()))
-							.filter(node -> node.containsKey("ingest"))
-							.map(node -> node.getJsonObject("ingest"))
-							.filter(ingest -> ingest.containsKey("processors"))
-							.flatMap(ingest -> {
-								JsonArray processors = ingest.getJsonArray("processors");
-								return IntStream.range(0, processors.size())
-										.mapToObj(processors::getJsonObject)
-										.filter(typeObj -> typeObj.containsKey("type"))
-										.map(typeObj -> typeObj.getString("type"));
-							})
-							.collect(Collectors.toSet())
-							.containsAll(processorNames)
-				).toSingle();
+			.filter(response -> response.containsKey("nodes"))
+			.map(response -> response.getJsonObject("nodes"))
+			// we need to find one node which contains
+			.map(nodes ->
+				nodes.stream()
+					.map(entry -> nodes.getJsonObject(entry.getKey()))
+					.filter(node -> node.containsKey("ingest"))
+					.map(node -> node.getJsonObject("ingest"))
+					.filter(ingest -> ingest.containsKey("processors"))
+					.flatMap(ingest -> {
+						JsonArray processors = ingest.getJsonArray("processors");
+						return IntStream.range(0, processors.size())
+							.mapToObj(processors::getJsonObject)
+							.filter(typeObj -> typeObj.containsKey("type"))
+							.map(typeObj -> typeObj.getString("type"));
+					})
+					.collect(Collectors.toSet())
+					.containsAll(processorNames)
+			).toSingle();
 	}
 
 	/**
