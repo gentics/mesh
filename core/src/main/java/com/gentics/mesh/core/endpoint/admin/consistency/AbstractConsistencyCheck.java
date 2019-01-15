dd4e0c3c9800df16425f9f6cf047b6b65f6a4e06
Johannes Schüth <j.schueth@gentics.com>
2019-01-15 14:39:08 +0100

    Remove bogus method

--- core/src/main/java/com/gentics/mesh/core/endpoint/admin/consistency/AbstractConsistencyCheck.java
+++ core/src/main/java/com/gentics/mesh/core/endpoint/admin/consistency/AbstractConsistencyCheck.java
@@ -1,9 +1,7 @@
 package com.gentics.mesh.core.endpoint.admin.consistency;
 
 import java.util.Iterator;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 import com.gentics.mesh.core.data.MeshVertex;
 import com.gentics.mesh.graphdb.spi.Database;
@@ -31,28 +29,25 @@ public abstract class AbstractConsistencyCheck implements ConsistencyCheck {
 	 * @param tx
 	 *            Current transaction
 	 */
-	protected <T extends MeshVertex> ConsistencyCheckResult processForType(Database db, Class<T> clazz, BiConsumer<T, ConsistencyCheckResult> action, boolean attemptRepair, Tx tx) {
+	protected <T extends MeshVertex> ConsistencyCheckResult processForType(Database db, Class<T> clazz, BiConsumer<T, ConsistencyCheckResult> action,
+		boolean attemptRepair, Tx tx) {
 		log.info("Processing elements of type {" + clazz.getSimpleName() + "}");
 		Iterator<? extends T> it = db.getVerticesForType(clazz);
 		ConsistencyCheckResult result = new ConsistencyCheckResult();
-		AtomicLong count = new AtomicLong(0);
+		long count = 0;
 		while (it.hasNext()) {
 			T element = it.next();
-			process(clazz, () -> action.accept(element, result), count, attemptRepair, tx);
-		}
-		log.info("Processed a total of {" + count.get() + "} " + clazz.getSimpleName() + " elements.");
-		return result;
-	}
-
-	protected <T extends MeshVertex> void process(Class<T> clazz, Runnable action, AtomicLong count, boolean attemptRepair, Tx tx) {
-		action.run();
-		if (count.get() != 0 && count.get() % BATCH_SIZE == 0) {
-			if (attemptRepair) {
-				tx.getGraph().commit();
+			action.accept(element, result);
+			if (count != 0 && count % BATCH_SIZE == 0) {
+				if (attemptRepair) {
+					tx.getGraph().commit();
+				}
+				log.info("Processed {" + count + "} " + clazz.getSimpleName() + " elements.");
 			}
-			log.info("Processed {" + count.get() + "} " + clazz.getSimpleName() + " elements.");
+			count++;
 		}
-		count.incrementAndGet();
+		log.info("Processed a total of {" + count + "} " + clazz.getSimpleName() + " elements.");
+		return result;
 	}
 
 }
