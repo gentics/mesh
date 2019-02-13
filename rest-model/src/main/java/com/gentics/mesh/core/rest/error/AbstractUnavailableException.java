b26ec4da361c9308fa2f851126e49cf7d24f3041
Patrick Klaffenböck <p.klaffenboeck@gentics.com>
2019-02-13 10:23:37 +0100

    Improve GraphQL error message when an element is not found

---
+++ rest-model/src/main/java/com/gentics/mesh/core/rest/error/AbstractUnavailableException.java
@@ -0,0 +1,70 @@
+package com.gentics.mesh.core.rest.error;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+/**
+ * The element with the specified ID and type is not available.
+ *
+ * <p>
+ * What the ID exactly is and the reason must be defined in concrete subclasses.
+ * </p>
+ */
+public abstract class AbstractUnavailableException extends AbstractRestException {
+
+	private static final long serialVersionUID = -7489334531936926971L;
+
+	protected final String elementType;
+	protected final String elementId;
+
+	/**
+	 * Creates an exception with the given message and message parameters and without a HTTP status.
+	 *
+	 * <p>
+	 * It is assumed that the <code>elementId</code> is the first parameter in the <code>i18nMessage</code>.
+	 * </p>
+	 *
+	 * @param i18nMessage The internationalized message for the exception
+	 * @param elementType The type of the unavailable object
+	 * @param elementId The ID of the unavailable object
+	 */
+	public AbstractUnavailableException(String i18nMessage, String elementType, String elementId) {
+		this(null, i18nMessage, elementId, elementType);
+	}
+
+	/**
+	 * Creates an exception with the given HTTP status, message and message parameters.
+	 *
+	 * <p>
+	 * It is assumed that the <code>elementId</code> is the first parameter in the <code>i18nMessage</code>.
+	 * </p>
+	 *
+	 * @param status The HTTP status to be used in the final response
+	 * @param i18nMessage The internationalized message for the exception
+	 * @param elementType The type of the unavailable object
+	 * @param elementId The ID of the unavailable object
+	 */
+	public AbstractUnavailableException(HttpResponseStatus status, String i18nMessage, String elementType, String elementId) {
+		super(status, i18nMessage, elementId, elementType);
+
+		this.elementType = elementType;
+		this.elementId = elementId;
+	}
+
+	/**
+	 * The type of he unavailable element.
+	 *
+	 * @return The type of the unavailable element
+	 */
+	public String getElementType() {
+		return elementType;
+	}
+
+	/**
+	 * The ID of the unavailable element.
+	 *
+	 * @return The ID of the unavailable element.
+	 */
+	public String getElementId() {
+		return elementId;
+	}
+}
--- rest-model/src/main/java/com/gentics/mesh/core/rest/error/PermissionException.java
+++ rest-model/src/main/java/com/gentics/mesh/core/rest/error/PermissionException.java
@@ -1,32 +1,25 @@
 package com.gentics.mesh.core.rest.error;
 
-public class PermissionException extends AbstractRestException {
+/**
+ * The element is not available because the user lacks permissions.
+ *
+ * <p>
+ * The {@link #getElementId() elementId} is the UUID of the element.
+ * </p>
+ */
+public class PermissionException extends AbstractUnavailableException {
 
 	public static final String TYPE = "missing_perm";
 
 	private static final long serialVersionUID = 6097093959066715614L;
 	public static final String i18nKey = "graphql_error_missing_perm";
 
-	private String elementId;
-	private String elementType;
-
 	public PermissionException(String elementType, String elementId) {
 		super(null, i18nKey, elementId, elementType);
-		this.elementId = elementId;
-		this.elementType = elementType;
 	}
 
 	@Override
 	public String getType() {
 		return TYPE;
 	}
-
-	public String getElementType() {
-		return elementType;
-	}
-
-	public String getElementId() {
-		return elementId;
-	}
-
 }
