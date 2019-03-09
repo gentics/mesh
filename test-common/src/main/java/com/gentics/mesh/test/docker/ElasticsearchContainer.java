10f19471a815908fa93912b031d449e16f5bc67f
Johannes Schüth <git@jotschi.de>
2019-03-09 13:24:26 +0100

    Use pre-build docker image for ES and bump ES version to 6.6.1

--- test-common/src/main/java/com/gentics/mesh/test/docker/ElasticsearchContainer.java
+++ test-common/src/main/java/com/gentics/mesh/test/docker/ElasticsearchContainer.java
@@ -1,39 +1,20 @@
 package com.gentics.mesh.test.docker;
 
-import java.nio.charset.Charset;
 import java.time.Duration;
 import java.util.Collections;
 
-import org.apache.commons.io.IOUtils;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.Wait;
-import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 
 /**
  * Testcontainer for a non-clustered Elasticsearch instance.
  */
 public class ElasticsearchContainer extends GenericContainer<ElasticsearchContainer> {
 
-	public static final String VERSION = "6.3.1";
+	public static final String VERSION = "6.6.1";
 
 	public ElasticsearchContainer(boolean withIngestPlugin) {
-		super(prepareDockerImage(withIngestPlugin));
-	}
-
-	private static ImageFromDockerfile prepareDockerImage(boolean withIngestPlugin) {
-		try {
-			ImageFromDockerfile dockerImage = new ImageFromDockerfile("elasticsearch", false);
-			String dockerFile = IOUtils.toString(ElasticsearchContainer.class.getResourceAsStream("/elasticsearch/Dockerfile.ingest"),
-				Charset.defaultCharset());
-			dockerFile = dockerFile.replace("%VERSION%", VERSION);
-			if (!withIngestPlugin) {
-				dockerFile = dockerFile.replace("RUN", "#RUN");
-			}
-			dockerImage.withFileFromString("Dockerfile", dockerFile);
-			return dockerImage;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		super(withIngestPlugin ? "jotschi/elasticsearch-ingest:" + VERSION : "docker.elastic.co/elasticsearch/elasticsearch-oss:" + VERSION);
 	}
 
 	@Override
@@ -43,7 +24,7 @@ public class ElasticsearchContainer extends GenericContainer<ElasticsearchContai
 		// addEnv("xpack.security.enabled", "false");
 		withExposedPorts(9200);
 		withStartupTimeout(Duration.ofSeconds(250L));
-		waitingFor(Wait.forHttp("/"));
+		waitingFor(new HttpWaitStrategy().forPath("/"));
 	}
 
 }
