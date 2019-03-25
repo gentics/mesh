80df5d8e75de05438a5104e8b5daa2db7e200c29
Johannes Schüth <account-github@jotschi.de>
2019-03-25 16:44:48 +0100

    Fix GraphQL read published perm issue (#685)
    * Fix graphql read published perm issue
    
    * Add review changes

--- core/src/test/java/com/gentics/mesh/core/graphql/GraphQLAnonymousPermissionTest.java
+++ core/src/test/java/com/gentics/mesh/core/graphql/GraphQLAnonymousPermissionTest.java
@@ -1,7 +1,6 @@
 package com.gentics.mesh.core.graphql;
 
 import static com.gentics.mesh.assertj.MeshAssertions.assertThat;
-import static com.gentics.mesh.core.data.relationship.GraphPermission.READ_PERM;
 import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 
@@ -10,6 +9,7 @@ import org.junit.Test;
 import com.gentics.mesh.core.data.node.Node;
 import com.gentics.mesh.core.rest.graphql.GraphQLResponse;
 import com.gentics.mesh.core.rest.role.RolePermissionRequest;
+import com.gentics.mesh.parameter.client.PublishParametersImpl;
 import com.gentics.mesh.test.TestSize;
 import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
@@ -22,16 +22,19 @@ public class GraphQLAnonymousPermissionTest extends AbstractMeshTest {
 	@Test
 	public void testReadViaAnonymous() throws Throwable {
 		final String QUERY_NAME = "anonymous-perm-query";
-		Node node = folder("2015");
-		String nodeUuid = tx(() -> node.getUuid());
-		String anonRoleUuid = tx(() -> anonymousRole().getUuid());
+		final Node node = folder("news");
+		final String nodeUuid = tx(() -> node.getUuid());
+		final String anonRoleUuid = tx(() -> anonymousRole().getUuid());
+		final String baseNodeUuid = tx(() -> project().getBaseNode().getUuid());
 
 		RolePermissionRequest request = new RolePermissionRequest();
 		request.setRecursive(true);
 		request.getPermissions().setRead(false).setReadPublished(true);
 		call(() -> client().updateRolePermissions(anonRoleUuid, "projects/" + projectUuid(), request));
 
-		// Ensure that the node is published
+		// Ensure that only two nodes are published
+		call(() -> client().takeNodeOffline(PROJECT_NAME, baseNodeUuid, new PublishParametersImpl().setRecursive(true)));
+		call(() -> client().publishNode(PROJECT_NAME, baseNodeUuid));
 		call(() -> client().publishNode(PROJECT_NAME, nodeUuid));
 
 		client().logout();
@@ -39,6 +42,7 @@ public class GraphQLAnonymousPermissionTest extends AbstractMeshTest {
 		// Now execute the query and assert it
 		GraphQLResponse response = call(
 			() -> client().graphqlQuery(PROJECT_NAME, getGraphQLQuery(QUERY_NAME)));
+		//System.out.println(response.toJson());
 		JsonObject jsonResponse = new JsonObject(response.toJson());
 		assertThat(jsonResponse).compliesToAssertions(QUERY_NAME);
 	}
