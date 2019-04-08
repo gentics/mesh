786e70feaf9f619e8a93239adfea94ab1ff95255
Johannes Schüth <j.schueth@gentics.com>
2019-04-08 13:54:52 +0200

    Fix assertion for node deletion

--- core/src/test/java/com/gentics/mesh/core/node/NodeLanguagesEndpointTest.java
+++ core/src/test/java/com/gentics/mesh/core/node/NodeLanguagesEndpointTest.java
@@ -2,9 +2,9 @@ package com.gentics.mesh.core.node;
 
 import static com.gentics.mesh.assertj.MeshAssertions.assertThat;
 import static com.gentics.mesh.core.data.relationship.GraphPermission.DELETE_PERM;
+import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 import static com.gentics.mesh.test.TestSize.FULL;
-import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.context.ElasticsearchTestMode.TRACKING;
 import static io.netty.handler.codec.http.HttpResponseStatus.FORBIDDEN;
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
@@ -12,10 +12,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-import com.gentics.mesh.test.context.ElasticsearchTestMode;
 import org.junit.Test;
 
-import com.syncleus.ferma.tx.Tx;
 import com.gentics.mesh.core.data.node.Node;
 import com.gentics.mesh.core.rest.node.NodeResponse;
 import com.gentics.mesh.parameter.impl.DeleteParametersImpl;
@@ -23,6 +21,7 @@ import com.gentics.mesh.parameter.impl.NodeParametersImpl;
 import com.gentics.mesh.parameter.impl.VersioningParametersImpl;
 import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
+import com.syncleus.ferma.tx.Tx;
 
 @MeshTestSetting(elasticsearch = TRACKING, testSize = FULL, startServer = true)
 public class NodeLanguagesEndpointTest extends AbstractMeshTest {
@@ -49,6 +48,7 @@ public class NodeLanguagesEndpointTest extends AbstractMeshTest {
 		// Delete the english version again
 		call(() -> client().deleteNode(PROJECT_NAME, contentUuid(), "en"), NOT_FOUND, "node_no_language_found", "en");
 
+		waitForSearchIdleEvent();
 		try (Tx tx = tx()) {
 			// Check the deletion
 			assertThat(trackingSearchProvider()).recordedDeleteEvents(2);
@@ -58,6 +58,8 @@ public class NodeLanguagesEndpointTest extends AbstractMeshTest {
 
 		// Now delete the remaining german version
 		call(() -> client().deleteNode(PROJECT_NAME, contentUuid(), "de", new DeleteParametersImpl().setRecursive(true)));
+		waitForSearchIdleEvent();
+
 		assertThat(trackingSearchProvider()).recordedDeleteEvents(2 + 2);
 		// The node was removed since the node only existed in a single branch and had no other languages
 		call(() -> client().findNodeByUuid(PROJECT_NAME, contentUuid(), new VersioningParametersImpl().published()), NOT_FOUND,
