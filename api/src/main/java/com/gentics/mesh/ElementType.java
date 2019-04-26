ce558e6fef2596f67ab956042eda47a81400fec0
Johannes Schüth <j.schueth@gentics.com>
2019-04-26 11:36:11 +0200

    Add javadoc

--- api/src/main/java/com/gentics/mesh/ElementType.java
+++ api/src/main/java/com/gentics/mesh/ElementType.java
@@ -1,18 +1,35 @@
 package com.gentics.mesh;
 
+/**
+ * List of basic element types which are stored in Gentics Mesh.
+ */
 public enum ElementType {
+
 	JOB,
+
 	LANGUAGE,
+
 	USER,
+
 	GROUP,
+
 	ROLE,
+
 	SCHEMA,
+
 	SCHEMAVERSION,
+
 	MICROSCHEMA,
+
 	MICROSCHEMAVERSION,
+
 	PROJECT,
+
 	TAGFAMILY,
+
 	TAG,
+
 	BRANCH,
+
 	NODE;
 }
