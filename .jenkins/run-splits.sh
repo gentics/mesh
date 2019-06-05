d52b9b2ed3494843bc096385f51259bf9ea9bb51
Johannes Schüth <j.schueth@gentics.com>
2019-06-05 14:21:19 +0200

    Add checks for correct splits

--- .jenkins/run-splits.sh
+++ .jenkins/run-splits.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+cd $BASEDIR/..
 echo "Using includes: $1"
 tests=$(paste -sd "," $1 | sed 's/\.java//g' | sed 's/\//./g')
 
+if [ -z "$tests" ] ; then
+  echo "Did not collect valid test set. Set was empty."
+  exit 10
+fi
 echo "Running tests: $tests"
 
-
-time mvn -X -fae -Dmaven.javadoc.skip=true -Dskip.cluster.tests=true -Dmaven.test.failure.ignore=true -B -e -pl '!ferma,!demo,!doc,!performance-tests' test -Dtest=$tests -DfailIfNoTests=false
+time mvn -fae -Dmaven.javadoc.skip=true -Dskip.cluster.tests=true -Dmaven.test.failure.ignore=true -B -e -pl '!ferma,!demo,!doc,!performance-tests' test -Dtest=$tests -DfailIfNoTests=false
