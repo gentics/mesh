e9b2654d5d0f98dbc1072001b52cfd8f772c42a8
Johannes Schüth <j.schueth@gentics.com>
2019-06-05 12:52:03 +0200

    Cleanup packages

--- .jenkins/run-splits.sh
+++ .jenkins/run-splits.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Using includes: $1"
-tests=$(paste -sd "," $1)
+tests=$(paste -sd "," $1 | sed 's/\.java//g' | sed 's/\//./g')
 
 echo "Running tests: $tests"
 
-time mvn -fae -Dmaven.javadoc.skip=true -Dskip.cluster.tests=true -Dmaven.test.failure.ignore=true -B -e -pl '!ferma,!demo,!doc,!performance-tests' test -Dtest=$tests -DfailIfNoTests=false
+
+time mvn -X -fae -Dmaven.javadoc.skip=true -Dskip.cluster.tests=true -Dmaven.test.failure.ignore=true -B -e -pl '!ferma,!demo,!doc,!performance-tests' test -Dtest=$tests -DfailIfNoTests=false
