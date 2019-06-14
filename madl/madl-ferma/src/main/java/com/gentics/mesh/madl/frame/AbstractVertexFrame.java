026729bc8f52f571893ec4da72cd2e73866bed58
Johannes Schüth <j.schueth@gentics.com>
2019-06-14 11:11:33 +0200

    Fix compile error #3

--- madl/madl-ferma/src/main/java/com/gentics/mesh/madl/frame/AbstractVertexFrame.java
+++ madl/madl-ferma/src/main/java/com/gentics/mesh/madl/frame/AbstractVertexFrame.java
@@ -101,13 +101,13 @@ public abstract class AbstractVertexFrame extends com.syncleus.ferma.AbstractVer
 
 	@Override
 	public <T extends ElementFrame> TraversalResult<? extends T> out(String label, Class<T> clazz) {
-		TraversalResult<T> result = new TraversalResult<>(out(label).frameExplicit(clazz));
+		TraversalResult<? extends T> result = new TraversalResult<>(out(label).frameExplicit(clazz));
 		return result;
 	}
 
 	@Override
 	public <T extends ElementFrame> TraversalResult<? extends T> in(String label, Class<T> clazz) {
-		TraversalResult<T> result = new TraversalResult<>(in(label).frameExplicit(clazz));
+		TraversalResult<? extends T> result = new TraversalResult<>(in(label).frameExplicit(clazz));
 		return result;
 	}
 }
