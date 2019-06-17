653a4ac19d7a681f7460132a7372f1f7acb84f83
Philipp Gürtler <p.guertler@gentics.com>
2019-06-18 00:47:26 +0200

    Wrap all errors messages in JSON (#768)

--- common/src/main/java/com/gentics/mesh/router/route/FailureHandler.java
+++ common/src/main/java/com/gentics/mesh/router/route/FailureHandler.java
@@ -138,15 +138,14 @@ public class FailureHandler implements Handler<RoutingContext> {
 				String msg = I18NUtil.get(ac, "error_parse_request_json_error");
 				rc.response().end(new GenericMessageResponse(msg, failure.getMessage()).toJson());
 			}
-			if (failure != null && failure instanceof AbstractRestException) {
+			if (failure instanceof AbstractRestException) {
 				AbstractRestException error = (AbstractRestException) failure;
 				rc.response().setStatusCode(code);
 				translateMessage(error, rc);
 				rc.response().end(JsonUtil.toJson(error));
-			} else if (failure != null) {
-				rc.response().setStatusCode(code);
-				rc.response().end(JsonUtil.toJson(new GenericMessageResponse(failure.getMessage())));
 			} else {
+				// We don't want to output too much information when an unexpected error occurs.
+				// That's why we don't reuse the error message here.
 				InternalActionContext ac = new InternalRoutingActionContextImpl(rc);
 				String msg = I18NUtil.get(ac, "error_internal");
 				rc.response().setStatusCode(500);
