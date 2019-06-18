45b32d01b7e6b2ad1ff6a187d971f3156ea8cc8e
Johannes Schüth <git@jotschi.de>
2019-06-18 19:39:44 +0200

    Remove key injection which is no longer needed since we use disable ssl checks for our snakeoil tests

--- core/src/test/java/com/gentics/mesh/core/ssl/SSLServerTest.java
+++ core/src/test/java/com/gentics/mesh/core/ssl/SSLServerTest.java
@@ -7,15 +7,9 @@ import org.junit.Test;
 
 import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
-import com.gentics.mesh.util.SSLUtil;
 
 @MeshTestSetting(testSize = FULL, startServer = true, ssl = true)
 public class SSLServerTest extends AbstractMeshTest {
-
-	static {
-		// Add the snakeoil cert to the keystore
-		SSLUtil.updateKeyStore("/ssl/cert.pem");
-	}
 
 	@Test
 	public void testReadByUUID() throws Exception {
