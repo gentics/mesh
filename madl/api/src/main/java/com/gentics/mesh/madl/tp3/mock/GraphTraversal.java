695a80bb96e1e4adb05345e4b50901d523295986
Johannes Schüth <git@jotschi.de>
2019-06-26 21:43:14 +0200

    Add traversal methods

--- madl/api/src/main/java/com/gentics/mesh/madl/tp3/mock/GraphTraversal.java
+++ madl/api/src/main/java/com/gentics/mesh/madl/tp3/mock/GraphTraversal.java
@@ -1,6 +1,24 @@
 package com.gentics.mesh.madl.tp3.mock;
 
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Vertex;
+
 public interface GraphTraversal<S, E> extends Traversal<S, E> {
 
 	GraphTraversal<S, E> has(final String propertyKey, final Object value);
+
+	GraphTraversal<S, Vertex> in(final String... edgeLabels);
+
+	GraphTraversal<S, Vertex> out(final String... edgeLabels);
+
+	GraphTraversal<S, Vertex> outV();
+
+	GraphTraversal<S, Vertex> inV();
+
+	GraphTraversal<S, Edge> outE(final String... edgeLabels);
+
+	GraphTraversal<S, Edge> inE(final String... edgeLabels);
+
+	GraphTraversal<S, Long> count();
+
 }
--- madl/api/src/main/java/com/gentics/mesh/madl/tp3/mock/GraphTraversalSource.java
+++ madl/api/src/main/java/com/gentics/mesh/madl/tp3/mock/GraphTraversalSource.java
@@ -1,9 +1,12 @@
 package com.gentics.mesh.madl.tp3.mock;
 
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 
 public interface GraphTraversalSource {
 
 	GraphTraversal<Vertex, Vertex> V(final Object... vertexIds);
+
+	GraphTraversal<Edge, Edge> E(final Object... edgesIds);
 
 }
