4fa9108d7fd46312b1f3b057a70db50fe6e181e7
Jeremy Branham <Jeremy@Savantly.net>
2019-07-06 21:20:41 -0500

    using JUnit Rule for environment variables for Windows compatibility Moved dependency version into parent pom

--- elasticsearch/src/test/java/com/gentics/mesh/search/ElasticsearchProcessManagerTest.java
+++ elasticsearch/src/test/java/com/gentics/mesh/search/ElasticsearchProcessManagerTest.java
@@ -7,12 +7,11 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 import com.gentics.mesh.etc.config.search.ElasticSearchOptions;
 
@@ -20,12 +19,13 @@ import io.vertx.core.Vertx;
 import net.lingala.zip4j.exception.ZipException;
 
 public class ElasticsearchProcessManagerTest {
+	
+	@Rule
+	public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
 	@Test
 	public void testExecFailure() throws Exception {
-		Map<String, String> map = new HashMap<>();
-		map.put("JAVA_HOME", "/bogus");
-		set(map);
+		environmentVariables.set("JAVA_HOME", "/bogus");
 		ElasticsearchProcessManager manager = new ElasticsearchProcessManager(Vertx.vertx(), new ElasticSearchOptions());
 		try {
 			manager.start();
@@ -45,14 +45,12 @@ public class ElasticsearchProcessManagerTest {
 
 		// Start the watchdog and stop the process directly. The watchdog must restart the process.
 		manager.startWatchDog();
-		p.destroyForcibly();
-		Thread.sleep(14000);
+		p.destroyForcibly().waitFor(14, TimeUnit.SECONDS);
 		assertTrue("The process should have been restarted.", manager.getProcess().isAlive());
 
-		// Stop the watchdog and check that the proccess was not restarted
+		// Stop the watchdog and check that the process was not restarted
 		manager.stopWatchDog();
-		manager.getProcess().destroyForcibly();
-		Thread.sleep(6000);
+		manager.getProcess().destroyForcibly().waitFor(6, TimeUnit.SECONDS);
 		assertFalse("The process should be still stopped.", manager.getProcess().isAlive());
 
 		// Start again and assert that regular stop works
@@ -64,27 +62,6 @@ public class ElasticsearchProcessManagerTest {
 		Thread.sleep(2000);
 		assertNull("The process object should be null since the process has been stopped.", manager.getProcess());
 		assertFalse("The process should have been stopped.", p.isAlive());
-	}
-
-	/**
-	 * Override the environment variables.
-	 * 
-	 * @param newenv
-	 * @throws Exception
-	 */
-	public static void set(Map<String, String> newenv) throws Exception {
-		Class[] classes = Collections.class.getDeclaredClasses();
-		Map<String, String> env = System.getenv();
-		for (Class cl : classes) {
-			if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-				Field field = cl.getDeclaredField("m");
-				field.setAccessible(true);
-				Object obj = field.get(env);
-				Map<String, String> map = (Map<String, String>) obj;
-				map.clear();
-				map.putAll(newenv);
-			}
-		}
 	}
 
 }
