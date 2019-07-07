26ac47764ae11aee310c95a18e48f9b28c03977a
Jeremy Branham <Jeremy@Savantly.net>
2019-07-07 09:50:55 -0500

    reversed mistake

--- elasticsearch/src/test/java/com/gentics/mesh/search/ElasticsearchProcessManagerTest.java
+++ elasticsearch/src/test/java/com/gentics/mesh/search/ElasticsearchProcessManagerTest.java
@@ -7,8 +7,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
@@ -56,12 +54,14 @@ public class ElasticsearchProcessManagerTest {
 
 		// Start the watchdog and stop the process directly. The watchdog must restart the process.
 		manager.startWatchDog();
-		p.destroyForcibly().waitFor(14, TimeUnit.SECONDS);
+		p.destroyForcibly();
+		Thread.sleep(14000);
 		assertTrue("The process should have been restarted.", manager.getProcess().isAlive());
 
 		// Stop the watchdog and check that the process was not restarted
 		manager.stopWatchDog();
-		manager.getProcess().destroyForcibly().waitFor(6, TimeUnit.SECONDS);
+		manager.getProcess().destroyForcibly();
+		Thread.sleep(6000);
 		assertFalse("The process should be still stopped.", manager.getProcess().isAlive());
 
 		// Start again and assert that regular stop works
