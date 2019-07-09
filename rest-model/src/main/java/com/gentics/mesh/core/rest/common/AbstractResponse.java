df52819e46932cb1767fd1fff4a72123742a7385
Prashanth Reddy <prashanth@peoplecloudlabs.com>
2019-07-09 16:58:03 -0400

    Equals method for Rest Models

--- rest-model/src/main/java/com/gentics/mesh/core/rest/common/AbstractResponse.java
+++ rest-model/src/main/java/com/gentics/mesh/core/rest/common/AbstractResponse.java
@@ -3,6 +3,8 @@ package com.gentics.mesh.core.rest.common;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
+import java.util.Objects;
+
 /**
  * Abstract response which provides the uuid.
  */
@@ -27,4 +29,16 @@ public abstract class AbstractResponse implements RestResponse {
 		return getClass().getSimpleName() + " Uuid: " + getUuid();
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		AbstractResponse that = (AbstractResponse) o;
+		return Objects.equals(getUuid(), that.getUuid());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getUuid());
+	}
 }
