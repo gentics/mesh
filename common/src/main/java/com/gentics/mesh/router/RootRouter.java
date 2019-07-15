ec2ad29fd8d651fc3699c2d5cf3c57de050bafe2
Philipp Gürtler <p.guertler@gentics.com>
2019-07-15 09:00:33 +0200

    Show duration for request handling

--- common/src/main/java/com/gentics/mesh/router/RootRouter.java
+++ common/src/main/java/com/gentics/mesh/router/RootRouter.java
@@ -4,6 +4,7 @@ import com.gentics.mesh.router.route.DefaultNotFoundHandler;
 import com.gentics.mesh.router.route.FailureHandler;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.LoggerFormat;
 import io.vertx.ext.web.handler.LoggerHandler;
 
 import static com.gentics.mesh.handler.VersionHandler.API_MOUNTPOINT;
@@ -28,7 +29,7 @@ public class RootRouter {
 		this.vertx = vertx;
 		this.router = Router.router(vertx);
 		// Root handlersA
-		router.route().handler(LoggerHandler.create());
+		router.route().handler(LoggerHandler.create(LoggerFormat.SHORT));
 		// TODO add a dedicated error for api router that informs about
 		// APPLICATION_JSON requirements. This may not be true for other
 		// routes (eg. custom
