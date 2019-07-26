6693fbf53fe3a8c269d01a406a62db62ed4ff676
Johannes Schüth <git@jotschi.de>
2019-07-26 20:03:14 +0200

    Cleanup plugin

--- core/src/test/plugins/graphql/src/main/java/com/gentics/mesh/plugin/GraphQLTestPlugin.java
+++ core/src/test/plugins/graphql/src/main/java/com/gentics/mesh/plugin/GraphQLTestPlugin.java
@@ -9,32 +9,24 @@ import org.pf4j.PluginWrapper;
 import com.gentics.mesh.plugin.env.PluginEnvironment;
 
 import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLObjectType.Builder;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 
 public class GraphQLTestPlugin extends AbstractPlugin implements GraphQLPlugin {
-
-	private static final Logger log = LoggerFactory.getLogger(GraphQLTestPlugin.class);
 
 	public GraphQLTestPlugin(PluginWrapper wrapper, PluginEnvironment env) {
 		super(wrapper, env);
 	}
 
 	@Override
-	public void start() {
-		log.info("Starting GraphQL plugin");
-	}
-
-	@Override
-
 	public GraphQLObjectType createType() {
-		Builder root = newObject();
-		root.name("PluginDataType");
-		root.description("Dummy GraphQL Test");
-		root.field(newFieldDefinition().name("text").type(GraphQLString).description("The text field returns info text").dataFetcher(env -> {
-			return "hello-world";
-		}));
-		return root.build();
+		return newObject()
+			.name("PluginDataType")
+			.description("Dummy GraphQL Test")
+			.field(newFieldDefinition().name("text")
+				.type(GraphQLString)
+				.description("Say hello to the world of plugins")
+				.dataFetcher(env -> {
+					return "hello-world";
+				}))
+			.build();
 	}
 }
