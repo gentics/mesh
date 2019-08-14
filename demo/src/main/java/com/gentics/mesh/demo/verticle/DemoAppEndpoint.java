ef85ef93789ea77092aec9af0aae6c914b58d6eb
Johannes Schüth <j.schueth@gentics.com>
2019-08-14 10:09:47 +0200

    Fix demo serving

--- demo/src/main/java/com/gentics/mesh/demo/verticle/DemoAppEndpoint.java
+++ demo/src/main/java/com/gentics/mesh/demo/verticle/DemoAppEndpoint.java
@@ -2,6 +2,7 @@ package com.gentics.mesh.demo.verticle;
 
 import static io.vertx.core.http.HttpMethod.GET;
 
+import com.gentics.mesh.etc.config.MeshOptions;
 import com.gentics.mesh.router.RouterStorage;
 import com.gentics.mesh.router.route.AbstractInternalEndpoint;
 
@@ -14,7 +15,7 @@ import io.vertx.ext.web.handler.StaticHandler;
  */
 public class DemoAppEndpoint extends AbstractInternalEndpoint {
 
-	public DemoAppEndpoint() {
+	public DemoAppEndpoint(MeshOptions options) {
 		super("demo", null);
 	}
 
