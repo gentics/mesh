f55f1f45724a0ba52f0661a0e463a68e1ce551ed
Johannes Schüth <j.schueth@gentics.com>
2019-09-30 17:37:00 +0200

    Fix compile error and bump ui

--- server/src/main/java/com/gentics/mesh/server/ServerRunner.java
+++ server/src/main/java/com/gentics/mesh/server/ServerRunner.java
@@ -8,7 +8,6 @@ import com.gentics.mesh.context.impl.LoggingConfigurator;
 import com.gentics.mesh.dagger.MeshComponent;
 import com.gentics.mesh.etc.config.MeshOptions;
 import com.gentics.mesh.router.EndpointRegistry;
-import com.gentics.mesh.search.endpoint.ElasticsearchHeadEndpoint;
 import com.gentics.mesh.verticle.admin.AdminGUI2Endpoint;
 import com.gentics.mesh.verticle.admin.AdminGUIEndpoint;
 
