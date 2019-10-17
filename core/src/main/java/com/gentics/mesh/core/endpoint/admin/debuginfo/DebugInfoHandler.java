26974b803f3ae0a01d9a74681e3e7ca482a30756
Philipp Gürtler <p.guertler@gentics.com>
2019-10-17 09:02:13 +0200

    Add more debug info

--- core/src/main/java/com/gentics/mesh/core/endpoint/admin/debuginfo/DebugInfoHandler.java
+++ core/src/main/java/com/gentics/mesh/core/endpoint/admin/debuginfo/DebugInfoHandler.java
@@ -10,15 +10,20 @@ import java.util.zip.ZipOutputStream;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 import com.gentics.mesh.context.impl.InternalRoutingActionContextImpl;
 
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
 
 @Singleton
 public class DebugInfoHandler {
 	private final Set<DebugInfoProvider> debugInfoProviders;
+	private static final Logger log = LoggerFactory.getLogger(DebugInfoHandler.class);
 
 	@Inject
 	public DebugInfoHandler(Set<DebugInfoProvider> debugInfoProviders) {
@@ -30,18 +35,34 @@ public class DebugInfoHandler {
 		setHeaders(ac);
 		ZipOutputStream zipOutputStream = new ZipOutputStream(WrappedWriteStream.fromWriteStream(ac.response()));
 		Flowable.fromIterable(debugInfoProviders)
-			.flatMap(provider -> provider.debugInfoEntries(iac))
+			.flatMap(provider -> getDebugInfo(iac, provider))
 			.flatMapCompletable(entry -> {
-				System.out.println("start " + entry.getFileName());
+				log.debug("Start writing " + entry.getFileName());
 				zipOutputStream.putNextEntry(entry.createZipEntry());
 				zipOutputStream.write(entry.getData().getBytes());
-				System.out.println("end " + entry.getFileName());
+				log.debug("End writing" + entry.getFileName());
 				return Completable.complete();
 			})
 			.subscribe(() -> {
 				zipOutputStream.close();
 				ac.response().end();
 			}, ac::fail);
+	}
+
+	private Flowable<DebugInfoEntry> getDebugInfo(InternalRoutingActionContextImpl iac, DebugInfoProvider provider) {
+		if (!log.isDebugEnabled()) {
+			return provider.debugInfoEntries(iac);
+		}
+		StopWatch watch = new StopWatch();
+		return provider.debugInfoEntries(iac)
+			.doOnSubscribe(ignore -> {
+				log.debug("Starting " + provider.name());
+				watch.start();
+			})
+			.doOnComplete(() -> {
+				watch.stop();
+				log.debug(String.format("%s completed. Took %d ms. ", provider.name(), watch.getTime()));
+			});
 	}
 
 	private void setHeaders(RoutingContext ac) {
