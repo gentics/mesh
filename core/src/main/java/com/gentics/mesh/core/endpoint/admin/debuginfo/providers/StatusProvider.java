c84986655e70ee79d0d43c1d1656e0d759af2f21
Philipp Gürtler <p.guertler@gentics.com>
2019-10-17 14:25:43 +0200

    Add search status

--- core/src/main/java/com/gentics/mesh/core/endpoint/admin/debuginfo/providers/StatusProvider.java
+++ core/src/main/java/com/gentics/mesh/core/endpoint/admin/debuginfo/providers/StatusProvider.java
@@ -7,16 +7,19 @@ import com.gentics.mesh.context.InternalActionContext;
 import com.gentics.mesh.core.endpoint.admin.debuginfo.DebugInfoEntry;
 import com.gentics.mesh.core.endpoint.admin.debuginfo.DebugInfoProvider;
 import com.gentics.mesh.graphdb.spi.Database;
+import com.gentics.mesh.search.index.AdminIndexHandler;
 
 import io.reactivex.Flowable;
 
 @Singleton
 public class StatusProvider implements DebugInfoProvider {
 	private final Database db;
+	private final AdminIndexHandler adminIndexHandler;
 
 	@Inject
-	public StatusProvider(Database db) {
+	public StatusProvider(Database db, AdminIndexHandler adminIndexHandler) {
 		this.db = db;
+		this.adminIndexHandler = adminIndexHandler;
 	}
 
 	@Override
@@ -27,13 +30,20 @@ public class StatusProvider implements DebugInfoProvider {
 	@Override
 	public Flowable<DebugInfoEntry> debugInfoEntries(InternalActionContext ac) {
 		return Flowable.mergeArray(
-			getClusterStatus()
+			getClusterStatus(),
+			getElasticSearchStatus()
 		);
 	}
 
 	private Flowable<DebugInfoEntry> getClusterStatus() {
 		return db.singleTx(() -> db.clusterManager().getClusterStatus())
 			.map(status -> DebugInfoEntry.fromString("clusterStatus.json", status.toJson()))
+			.toFlowable();
+	}
+
+	private Flowable<DebugInfoEntry> getElasticSearchStatus() {
+		return adminIndexHandler.getSearchStatus()
+			.map(status -> DebugInfoEntry.fromString("searchStatus.json", status.toJson()))
 			.toFlowable();
 	}
 }
