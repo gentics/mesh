2241a45557fb0f0d81bb0442bf573c9064755638
Philipp Gürtler <p.guertler@gentics.com>
2019-10-18 15:43:51 +0200

    Fix hard coded log path

--- core/src/main/java/com/gentics/mesh/core/endpoint/admin/debuginfo/providers/LogProvider.java
+++ core/src/main/java/com/gentics/mesh/core/endpoint/admin/debuginfo/providers/LogProvider.java
@@ -1,5 +1,7 @@
 package com.gentics.mesh.core.endpoint.admin.debuginfo.providers;
 
+import java.nio.file.Paths;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -8,6 +10,7 @@ import com.gentics.mesh.core.endpoint.admin.debuginfo.DebugInfoBufferEntry;
 import com.gentics.mesh.core.endpoint.admin.debuginfo.DebugInfoEntry;
 import com.gentics.mesh.core.endpoint.admin.debuginfo.DebugInfoProvider;
 import com.gentics.mesh.core.endpoint.admin.debuginfo.DebugInfoUtil;
+import com.gentics.mesh.etc.config.MeshOptions;
 
 import io.reactivex.Flowable;
 import io.vertx.core.buffer.Buffer;
@@ -15,10 +18,12 @@ import io.vertx.core.buffer.Buffer;
 @Singleton
 public class LogProvider implements DebugInfoProvider {
 	private final DebugInfoUtil debugInfoUtil;
+	private final MeshOptions meshOptions;
 
 	@Inject
-	public LogProvider(DebugInfoUtil debugInfoUtil) {
+	public LogProvider(DebugInfoUtil debugInfoUtil, MeshOptions meshOptions) {
 		this.debugInfoUtil = debugInfoUtil;
+		this.meshOptions = meshOptions;
 	}
 
 	@Override
@@ -28,9 +33,10 @@ public class LogProvider implements DebugInfoProvider {
 
 	@Override
 	public Flowable<DebugInfoEntry> debugInfoEntries(InternalActionContext ac) {
+		String logFolder = meshOptions.getDebugInfoOptions().getLogFolder();
 		return Flowable.concatArray(
-			debugInfoUtil.readFileOrEmpty("debuginfo/debuginfo.1.log"),
-			debugInfoUtil.readFileOrEmpty("debuginfo/debuginfo.log")
+			debugInfoUtil.readFileOrEmpty(Paths.get(logFolder, "debuginfo.1.log").toString()),
+			debugInfoUtil.readFileOrEmpty(Paths.get(logFolder, "debuginfo.log").toString())
 		).reduce(this::concat)
 		.toFlowable()
 		.map(buffer -> DebugInfoBufferEntry.fromBuffer("log.txt", buffer));
