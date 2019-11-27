4bec68c81249af196e8ba2c20ccd65068dfaeaf8
Philipp Gürtler <p.guertler@gentics.com>
2019-11-27 08:58:08 +0100

    Restructure test

--- core/src/test/java/com/gentics/mesh/core/admin/ReadOnlyModeTest.java
+++ core/src/test/java/com/gentics/mesh/core/admin/ReadOnlyModeTest.java
@@ -3,20 +3,19 @@ package com.gentics.mesh.core.admin;
 import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 import static com.gentics.mesh.test.TestSize.PROJECT;
-import static org.junit.Assert.assertNotNull;
+
+import java.util.function.IntFunction;
 
 import org.junit.After;
 import org.junit.Test;
 
 import com.gentics.mesh.core.rest.admin.runtimeconfig.LocalConfigModel;
-import com.gentics.mesh.core.rest.graphql.GraphQLResponse;
 import com.gentics.mesh.core.rest.user.UserCreateRequest;
 import com.gentics.mesh.rest.client.MeshRequest;
 import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.vertx.core.json.pointer.JsonPointer;
 
 @MeshTestSetting(testSize = PROJECT, startServer = true)
 public class ReadOnlyModeTest extends AbstractMeshTest {
@@ -28,32 +27,37 @@ public class ReadOnlyModeTest extends AbstractMeshTest {
 
 	@Test
 	public void testCreateUser() {
-		createUser("test1");
-		setReadOnly(true);
-		call(
-			() -> client().createUser(new UserCreateRequest().setUsername("test2").setPassword("abc")),
-			HttpResponseStatus.METHOD_NOT_ALLOWED,
-			"error_readonly_mode"
-		);
-		setReadOnly(false);
-		createUser("test2");
+		testReadOnly(false, i -> client().createUser(new UserCreateRequest().setUsername("test" + i).setPassword("abc")));
 	}
 
 	@Test
 	public void testGraphQL() {
-		MeshRequest<GraphQLResponse> query = client().graphqlQuery(PROJECT_NAME, "{ me { uuid } }");
-		GraphQLResponse me;
+		testReadOnly(true, client().graphqlQuery(PROJECT_NAME, "{ me { uuid } }"));
+	}
 
-		me = call(() -> query);
-		assertNotNull(JsonPointer.from("/me/uuid").queryJson(me.getData()));
+	/**
+	 * Calls the request 3 times: Once without read only mode, once with read only mode, then once again without read only mode.
+	 * @param allowedInReadOnly If true, the request is expected to work in read only mode. Otherwise the request is expected to fail in read only mode.
+	 */
+	private void testReadOnly(boolean allowedInReadOnly, MeshRequest<?> request) {
+		testReadOnly(allowedInReadOnly, ignore -> request);
+	}
 
+	/**
+	 * Calls the request 3 times: Once without read only mode, once with read only mode, then once again without read only mode.
+	 * @param allowedInReadOnly If true, the request is expected to work in read only mode. Otherwise the request is expected to fail in read only mode.
+	 * @param request A function that returns the request to be tested. The parameter is the call count, starting with 0.
+	 */
+	private void testReadOnly(boolean allowedInReadOnly, IntFunction<MeshRequest<?>> request) {
+		request.apply(0).blockingAwait();
 		setReadOnly(true);
-		me = call(() -> query);
-		assertNotNull(JsonPointer.from("/me/uuid").queryJson(me.getData()));
-
+		if (allowedInReadOnly) {
+			request.apply(1).blockingAwait();
+		} else {
+			call(() -> request.apply(1), HttpResponseStatus.METHOD_NOT_ALLOWED, "error_readonly_mode");
+		}
 		setReadOnly(false);
-		me = call(() -> query);
-		assertNotNull(JsonPointer.from("/me/uuid").queryJson(me.getData()));
+		request.apply(2).blockingAwait();
 	}
 
 	private void setReadOnly(boolean readOnly) {
