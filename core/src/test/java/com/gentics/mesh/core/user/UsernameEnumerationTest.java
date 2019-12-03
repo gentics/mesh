bd6b9f2cff9ada3095e6d91297bafd71bbb0a274
Philipp Gürtler <p.guertler@gentics.com>
2019-12-03 12:57:14 +0100

    Add more tests and fix GraphQL enumeration

--- core/src/test/java/com/gentics/mesh/core/user/UsernameEnumerationTest.java
+++ core/src/test/java/com/gentics/mesh/core/user/UsernameEnumerationTest.java
@@ -2,6 +2,7 @@ package com.gentics.mesh.core.user;
 
 import static com.gentics.mesh.assertj.MeshAssertions.assertThat;
 import static com.gentics.mesh.test.ClientHelper.call;
+import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 import static com.gentics.mesh.test.TestSize.FULL;
 import static io.netty.handler.codec.http.HttpResponseStatus.FORBIDDEN;
 import static io.netty.handler.codec.http.HttpResponseStatus.UNAUTHORIZED;
@@ -9,7 +10,9 @@ import static io.netty.handler.codec.http.HttpResponseStatus.UNAUTHORIZED;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.gentics.mesh.core.rest.graphql.GraphQLResponse;
 import com.gentics.mesh.core.rest.user.UserCreateRequest;
+import com.gentics.mesh.core.rest.user.UserListResponse;
 import com.gentics.mesh.rest.client.MeshRestClientMessageException;
 import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
@@ -27,6 +30,24 @@ public class UsernameEnumerationTest extends AbstractMeshTest {
 	}
 
 	@Test
+	public void testNoUserAccess() {
+		UserListResponse userListResponse = client().findUsers().blockingGet();
+		assertThat(userListResponse.getData()).isEmpty();
+	}
+
+	@Test
+	public void testNoGraphQlUserAccess() {
+		GraphQLResponse graphQLResponse = client().graphqlQuery(PROJECT_NAME, "{\n" +
+			"  users {\n" +
+			"    elements {\n" +
+			"      username\n" +
+			"    }\n" +
+			"  }\n" +
+			"}").blockingGet();
+		assertThat(graphQLResponse.getData().getJsonObject("users").getJsonArray("elements").size()).isEqualTo(0);
+	}
+
+	@Test
 	public void testLogin() {
 		tryLogin("nonExistingUser", "wrongPassword", UNAUTHORIZED, "Login failed.");
 		tryLogin("admin", "wrongPassword", UNAUTHORIZED, "Login failed.");
@@ -41,6 +62,17 @@ public class UsernameEnumerationTest extends AbstractMeshTest {
 		call(() -> client().createUser(userCreateRequest), FORBIDDEN);
 		userCreateRequest.setUsername("admin");
 		call(() -> client().createUser(userCreateRequest), FORBIDDEN);
+	}
+
+	@Test
+	public void testGraphQl() {
+		GraphQLResponse graphQLResponse = client().graphqlQuery(PROJECT_NAME, "{user(name:\"nonExistingUser\") { username }}").blockingGet();
+		assertThat(graphQLResponse.getErrors()).isNullOrEmpty();
+		assertThat(graphQLResponse.getData().getJsonObject("user")).isNull();
+
+		graphQLResponse = client().graphqlQuery(PROJECT_NAME, "{user(name:\"admin\") { username }}").blockingGet();
+		assertThat(graphQLResponse.getErrors()).isNullOrEmpty();
+		assertThat(graphQLResponse.getData().getJsonObject("user")).isNull();
 	}
 
 	private void tryLogin(String username, String password, HttpResponseStatus expectedStatusCode, String expectedMessage) {
