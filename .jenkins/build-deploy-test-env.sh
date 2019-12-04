034fd0caa29287fc6c131eb6c39e6e6805285ce5
Johannes Schüth <j.schueth@gentics.com>
2019-12-04 14:12:12 +0100

    Upgrade build environment

--- .jenkins/build-deploy-test-env.sh
+++ .jenkins/build-deploy-test-env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $1 ] ; then
+  TAG=latest
+else
+  TAG=$1
+fi
 
-docker build -t gtx-docker-jenkinsbuilds.docker.apa-it.at/gentics/jenkinsbuilds/mesh-slave:latest .
-docker push gtx-docker-jenkinsbuilds.docker.apa-it.at/gentics/jenkinsbuilds/mesh-slave:latest
+docker build -t gtx-docker-jenkinsbuilds.docker.apa-it.at/gentics/jenkinsbuilds/mesh-slave:$TAG .
+docker push gtx-docker-jenkinsbuilds.docker.apa-it.at/gentics/jenkinsbuilds/mesh-slave:$TAG
