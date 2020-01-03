12030ba825c48aae3f505d7cfa857071c18a6d7a
Philipp Gürtler <p.guertler@gentics.com>
2020-01-03 10:27:58 +0100

    Add binary etag test

--- core/src/test/java/com/gentics/mesh/core/node/NodeEndpointFieldAPIeTagTest.java
+++ core/src/test/java/com/gentics/mesh/core/node/NodeEndpointFieldAPIeTagTest.java
@@ -6,6 +6,8 @@ import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.ClientHelper.callETag;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import com.gentics.madl.tx.Tx;
@@ -24,21 +26,46 @@ public class NodeEndpointFieldAPIeTagTest extends AbstractMeshTest {
 		int binaryLen = 8000;
 		String fileName = "somefile.dat";
 		Node node = folder("news");
+		String nodeUuid = node.getUuid();
 
 		try (Tx tx = tx()) {
 			prepareSchema(node, "", "binary");
 			tx.success();
 		}
 
-		try (Tx tx = tx()) {
-			call(() -> uploadRandomData(node, "en", "binary", binaryLen, contentType, fileName));
+		call(() -> uploadRandomData(node, "en", "binary", binaryLen, contentType, fileName));
 
-			// 2. Download the data using the field api
-			String etag = callETag(() -> client().downloadBinaryField(PROJECT_NAME, node.getUuid(), "en", "binary"));
-			assertNotNull("A etag should have been generated.", etag);
-			callETag(() -> client().downloadBinaryField(PROJECT_NAME, node.getUuid(), "en", "binary"), etag, false, 304);
-		}
-
+		// 2. Download the data using the field api
+		String etag = callETag(() -> client().downloadBinaryField(PROJECT_NAME, nodeUuid, "en", "binary"));
+		assertNotNull("A etag should have been generated.", etag);
+		callETag(() -> client().downloadBinaryField(PROJECT_NAME, nodeUuid, "en", "binary"), etag, false, 304);
 	}
 
+	@Test
+	public void testChangedBinary() throws Exception {
+		// 1. Upload some binary data
+		String contentType = "application/octet-stream";
+		int binaryLen = 8000;
+		String fileName = "somefile.dat";
+		Node node = folder("news");
+		String nodeUuid = node.getUuid();
+
+		try (Tx tx = tx()) {
+			prepareSchema(node, "", "binary");
+			tx.success();
+		}
+
+		call(() -> uploadRandomData(node, "en", "binary", binaryLen, contentType, fileName));
+
+		// 2. Download the data using the field api
+		String etag = callETag(() -> client().downloadBinaryField(PROJECT_NAME, nodeUuid, "en", "binary"));
+		assertNotNull("A etag should have been generated.", etag);
+		callETag(() -> client().downloadBinaryField(PROJECT_NAME, nodeUuid, "en", "binary"), etag, false, 304);
+
+		// 3. Change the binary data and try again
+		call(() -> uploadRandomData(node, "en", "binary", binaryLen, contentType, fileName));
+		etag = callETag(() -> client().downloadBinaryField(PROJECT_NAME, nodeUuid, "en", "binary"), etag, false, 200);
+		assertNotNull("A etag should have been generated.", etag);
+		callETag(() -> client().downloadBinaryField(PROJECT_NAME, nodeUuid, "en", "binary"), etag, false, 304);
+	}
 }
