0d0bd8586cc2a999cc0e395bc5f62e4b0c14ea4b
Johannes Schüth <git@jotschi.de>
2020-04-29 19:50:04 +0200

    Fix dep error

--- core/src/main/java/com/gentics/mesh/plugin/registry/PluginDeploymentLock.java
+++ core/src/main/java/com/gentics/mesh/plugin/registry/PluginDeploymentLock.java
@@ -5,8 +5,6 @@ import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import org.openjdk.tools.sjavac.Log;
-
 import com.gentics.mesh.etc.config.MeshOptions;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ILock;
@@ -40,7 +38,7 @@ public class PluginDeploymentLock implements AutoCloseable {
 	public void close() {
 		if (isClustered) {
 			if (clusterLock != null && clusterLock.isLockedByCurrentThread()) {
-				Log.info("Unlock");
+				System.out.println("Unlock");
 				clusterLock.unlock();
 			}
 		}
