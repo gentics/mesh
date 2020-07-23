676ef9076e1abcd1f922a047002000e250bddbc4
Johannes Schüth <git@jotschi.de>
2020-07-23 18:17:52 +0200

    Add testcase for graphql permissions regression

--- core/src/test/java/com/gentics/mesh/core/graphql/GraphQLPermissionQueryTest.java
+++ core/src/test/java/com/gentics/mesh/core/graphql/GraphQLPermissionQueryTest.java
@@ -5,22 +5,12 @@ import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Vector;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-import com.gentics.madl.tx.Tx;
-import com.gentics.mesh.core.data.node.Node;
-import com.gentics.mesh.core.data.relationship.GraphPermission;
 import com.gentics.mesh.core.rest.graphql.GraphQLRequest;
 import com.gentics.mesh.core.rest.graphql.GraphQLResponse;
 import com.gentics.mesh.core.rest.role.RoleResponse;
-import com.gentics.mesh.parameter.impl.PublishParametersImpl;
-import com.gentics.mesh.parameter.impl.VersioningParametersImpl;
 import com.gentics.mesh.test.TestSize;
 import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
--- core/src/test/java/com/gentics/mesh/core/graphql/GraphQLPermissionTest.java
+++ core/src/test/java/com/gentics/mesh/core/graphql/GraphQLPermissionTest.java
@@ -5,20 +5,16 @@ import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Vector;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 import com.gentics.madl.tx.Tx;
 import com.gentics.mesh.core.data.node.Node;
 import com.gentics.mesh.core.data.relationship.GraphPermission;
 import com.gentics.mesh.core.rest.graphql.GraphQLResponse;
+import com.gentics.mesh.parameter.ParameterProvider;
+import com.gentics.mesh.parameter.client.VersioningParametersImpl;
 import com.gentics.mesh.parameter.impl.PublishParametersImpl;
-import com.gentics.mesh.parameter.impl.VersioningParametersImpl;
 import com.gentics.mesh.test.TestSize;
 import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
@@ -26,21 +22,7 @@ import com.gentics.mesh.test.context.MeshTestSetting;
 import io.vertx.core.json.JsonObject;
 
 @MeshTestSetting(testSize = TestSize.FULL, startServer = true)
-@RunWith(Parameterized.class)
 public class GraphQLPermissionTest extends AbstractMeshTest {
-
-	private final String queryName;
-
-	public GraphQLPermissionTest(String queryName) {
-		this.queryName = queryName;
-	}
-
-	@Parameters(name = "query={0}")
-	public static Collection<Object[]> paramData() {
-		Collection<Object[]> testData = new Vector<>();
-		testData.add(new Object[] { "node-perm-children-query" });
-		return testData;
-	}
 
 	@Test
 	public void testReadPublishedNodeChildren() throws IOException {
@@ -49,7 +31,11 @@ public class GraphQLPermissionTest extends AbstractMeshTest {
 		String baseNodeUuid = tx(() -> project().getBaseNode().getUuid());
 		call(() -> client().publishNode(PROJECT_NAME, baseNodeUuid, new PublishParametersImpl().setRecursive(true)));
 
-		// 2. Revoke all read perm from all nodes also read_published from /News
+		// 2. Publish
+		String dealsUuid = tx(() -> folder("deals").getUuid());
+		call(() -> client().takeNodeOffline(PROJECT_NAME, dealsUuid, new PublishParametersImpl().setRecursive(true)));
+
+		// 3. Revoke all read perm from all nodes also read_published from /News
 		try (Tx tx = tx()) {
 			for (Node node : project().getNodeRoot().findAll()) {
 				role().revokePermissions(node, GraphPermission.READ_PERM);
@@ -59,14 +45,22 @@ public class GraphQLPermissionTest extends AbstractMeshTest {
 			tx.success();
 		}
 
-		// 3. Invoke the query and assert that the nodes can still be loaded (due to read published)
-		GraphQLResponse response = call(
-			() -> client().graphqlQuery(PROJECT_NAME, getGraphQLQuery(queryName), new VersioningParametersImpl().setVersion("published")));
-		JsonObject json = new JsonObject(response.toJson());
-		System.out.println(json.encodePrettily());
-		assertThat(json).compliesToAssertions(queryName);
+		// This assertion fails. The children list should not contain the Deals node since the user has no perms on it.
+		// Assert that it is not possible to load nodes draft version
+		assertQuery();
+
+		// Assert that using version param does not affect the handling
+		assertQuery(new VersioningParametersImpl().setVersion("published"));
+
 	}
 
-
+	private void assertQuery(ParameterProvider... parameters) throws IOException {
+		String queryName2 = "node-perm-children-query";
+		GraphQLResponse response2 = call(
+			() -> client().graphqlQuery(PROJECT_NAME, getGraphQLQuery(queryName2), parameters));
+		JsonObject json2 = new JsonObject(response2.toJson());
+		System.out.println(json2.encodePrettily());
+		assertThat(json2).compliesToAssertions(queryName2);
+	}
 
 }
