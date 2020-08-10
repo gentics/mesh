9c73dd74a029b1bfccf643835d93a401ca50cc0c
jurbunic <jbunic94@gmail.com>
2020-08-10 08:40:34 +0200

    add os check before unix utils test

--- test-common/src/test/java/com/gentics/mesh/test/util/UnixUtilsTest.java
+++ test-common/src/test/java/com/gentics/mesh/test/util/UnixUtilsTest.java
@@ -1,18 +1,25 @@
 package com.gentics.mesh.test.util;
 
+import static org.apache.commons.lang3.SystemUtils.IS_OS_UNIX;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assume.assumeTrue;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import com.gentics.mesh.test.util.UnixUtils;
-
 public class UnixUtilsTest {
+
+	@Before
+	public void isUnix() {
+		assumeTrue(IS_OS_UNIX);
+	}
 
 	@Test
 	public void testGetUid() throws IOException {
 		int uid = UnixUtils.getUid();
 		assertNotEquals(0, uid);
 	}
+
 }
