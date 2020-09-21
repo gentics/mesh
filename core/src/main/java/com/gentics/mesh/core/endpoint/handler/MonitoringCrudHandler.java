9930c635db53ac9c03863ad5b8798de6a738aa96
Johannes Schüth <git@jotschi.de>
2020-09-21 17:00:02 +0200

    Fix deployment issues in cluster setups

--- core/src/main/java/com/gentics/mesh/core/endpoint/handler/MonitoringCrudHandler.java
+++ core/src/main/java/com/gentics/mesh/core/endpoint/handler/MonitoringCrudHandler.java
@@ -43,8 +43,14 @@ public class MonitoringCrudHandler {
 	public void handleReady(RoutingContext rc) {
 		for (String id : pluginManager.getPluginIds()) {
 			PluginStatus status = pluginManager.getStatus(id);
-			if (status != PluginStatus.REGISTERED) {
-				log.warn("Plugin {" + id + "} is not ready. Got status {" + status + "}");
+			// TODO We need can't check for plugin registered since plugins will only be 
+			// registered once the write quorum has been reached.
+			// Thus we can only check for failed. Otherwise we would interrupt the
+			// K8S deployment process and prevent additional nodes from being added
+			// to the cluster. Without additional nodes the write quorum would never
+			// be reached.
+			if (status == PluginStatus.FAILED) {
+				log.warn("Plugin {" + id + "} is in status failed.");
 				throw error(SERVICE_UNAVAILABLE, "error_internal");
 			}
 		}
