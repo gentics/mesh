06c05761a0986e40f2adece77681c4c04bbb7c01
Serhii Plyhun <snuk182@live.com>
2021-01-22 19:51:06 +0100

    Unit tests fix #2

--- server/src/test/java/com/gentics/mesh/server/cluster/test/OrientDBClusterTestBackupNode.java
+++ server/src/test/java/com/gentics/mesh/server/cluster/test/OrientDBClusterTestBackupNode.java
@@ -2,7 +2,7 @@ package com.gentics.mesh.server.cluster.test;
 
 import org.junit.Before;
 
-import com.gentics.mesh.etc.config.MeshOptions;
+import com.gentics.mesh.etc.config.OrientDBMeshOptions;
 import com.gentics.mesh.server.cluster.test.task.BackupTask;
 
 /**
@@ -12,7 +12,7 @@ public class OrientDBClusterTestBackupNode extends AbstractClusterTest {
 
 	@Before
 	public void testServer() throws Exception {
-		MeshOptions options = init(null);
+		OrientDBMeshOptions options = init(null);
 		options.setNodeName("gentics-mesh-backup");
 		options.getStorageOptions().setDirectory("data4/graphdb");
 		options.getClusterOptions().setVertxPort(6154);
