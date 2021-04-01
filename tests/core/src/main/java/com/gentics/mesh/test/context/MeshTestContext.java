b9da2ca5b17d1d56e33fc4e2284c3ffe0207e597
Serhii Plyhun <snuk182@live.com>
2021-04-01 18:17:20 +0200

    Make unit tests using existing context, if available

--- tests/core/src/main/java/com/gentics/mesh/test/context/MeshTestContext.java
+++ tests/core/src/main/java/com/gentics/mesh/test/context/MeshTestContext.java
@@ -474,9 +474,9 @@ public class MeshTestContext extends TestWatcher {
 		
 		meshTestContextProvider = MeshTestContextProvider.getProvider();
 		
-		MeshInstanceProvider meshInstanceProvider = meshTestContextProvider.getInstanceProvider();
+		MeshInstanceProvider<? extends MeshOptions> meshInstanceProvider = meshTestContextProvider.getInstanceProvider();
 		
-		MeshOptions meshOptions = getOptions();
+		MeshOptions meshOptions = meshInstanceProvider.getOptions();
 
 		// Clustering options
 		if (settings.clusterMode()) {
@@ -500,7 +500,7 @@ public class MeshTestContext extends TestWatcher {
 		authOptions.setKeystorePath(keystoreFile.getAbsolutePath());
 		meshOptions.setNodeName("testNode");
 
-		initFolders(getOptions());
+		initFolders(meshOptions);
 
 		HttpServerConfig httpOptions = meshOptions.getHttpServerOptions();
 		httpOptions.setPort(httpPort);
@@ -607,9 +607,9 @@ public class MeshTestContext extends TestWatcher {
 			Set<JsonObject> jwks = KeycloakUtils.loadJWKs("http", keycloak.getHost(), keycloak.getMappedPort(8080), realmName);
 			meshOptions.getAuthenticationOptions().setPublicKeys(jwks);
 		}
-		settings.optionChanger().changer.accept(getOptions());
-		optionChanger.accept(getOptions());
-		return getOptions();
+		settings.optionChanger().changer.accept(meshOptions);
+		optionChanger.accept(meshOptions);
+		return meshOptions;
 	}
 
 	private void initFolders(MeshOptions meshOptions) throws IOException {
@@ -762,7 +762,7 @@ public class MeshTestContext extends TestWatcher {
 		return mesh;
 	}
 	
-	public MeshInstanceProvider getInstanceProvider() {
+	public MeshInstanceProvider<? extends MeshOptions> getInstanceProvider() {
 		return meshTestContextProvider.getInstanceProvider();
 	}
 	
