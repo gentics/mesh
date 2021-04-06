9f21fc45744b9449cff8750524466d86aa5a4616
apagtxhoxhaj <r.hoxhaj@gentics.com>
2021-04-06 13:43:54 +0200

    feature/MS3-7-WIP

--- mdm/common/src/main/java/com/gentics/mesh/storage/S3BinaryStorage.java
+++ mdm/common/src/main/java/com/gentics/mesh/storage/S3BinaryStorage.java
@@ -1,3 +1,54 @@
+<<<<<<< HEAD
+package com.gentics.mesh.storage;
+
+import com.gentics.mesh.core.data.s3binary.S3HibBinaryField;
+import com.gentics.mesh.core.rest.node.field.s3binary.S3RestResponse;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+import io.vertx.core.buffer.Buffer;
+
+/**
+ * A S3 binary storage provides means to store and retrieve binary data.
+ */
+public interface S3BinaryStorage {
+
+	/**
+	 * Read the binary data which is identified by the given binary uuid.
+	 *
+	 * @param uuid
+	 * @return
+	 */
+	Flowable<Buffer> read(String uuid);
+
+
+	/**
+	 * Create a presigned URL that can be used for a given time period.
+	 *
+	 * @param nodeUuid
+	 * @param fieldName
+	 * @return
+	 */
+	Single<S3RestResponse> createPresignedUrl(String nodeUuid, String fieldName);
+
+	/**
+	 * Creates a S3 bucket for a given name if doesn't exists..
+	 * 
+	 * @param bucketName
+	 * @return
+	 */
+	 void createBucket(String bucketName);
+
+	/**
+	 * Delete the binary with the given uuid.
+	 * 
+	 * @param uuid
+	 */
+	Completable delete(String uuid);
+
+}
+||||||| parent of 44b50193d... feature/MS3-7-WIP
+=======
 package com.gentics.mesh.storage;
 
 import com.gentics.mesh.core.data.s3binary.S3HibBinaryField;
