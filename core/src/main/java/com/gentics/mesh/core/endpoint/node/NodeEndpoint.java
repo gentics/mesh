e973d46cb5cc6a032b94cc62a84585c2348420f9
apagtxhoxhaj <r.hoxhaj@gentics.com>
2021-04-23 14:56:21 +0200

    MS3-13-WIP

--- core/src/main/java/com/gentics/mesh/core/endpoint/node/NodeEndpoint.java
+++ core/src/main/java/com/gentics/mesh/core/endpoint/node/NodeEndpoint.java
@@ -263,25 +263,6 @@ public class NodeEndpoint extends AbstractProjectEndpoint {
 			InternalActionContext ac = wrap(rc);
 			s3BinaryMetadataExtractionHandler.handleMetadataExtraction(ac, uuid, fieldName, attributes);
 		});
-
-		InternalEndpointRoute fieldMetadataExtraction = createRoute();
-		fieldMetadataExtraction.path("/:nodeUuid/s3binary/:fieldName/parseMetadata");
-		fieldMetadataExtraction.addUriParameter("nodeUuid", "Uuid of the node.", NODE_DELOREAN_UUID);
-		fieldMetadataExtraction.addUriParameter("fieldName", "Name of the field which should be created.", "stringField");
-		fieldMetadataExtraction.method(POST);
-		fieldMetadataExtraction.produces(APPLICATION_JSON);
-		fieldMetadataExtraction.exampleRequest(nodeExamples.getExampleBinaryUploadFormParameters());
-		fieldMetadataExtraction.exampleResponse(OK, nodeExamples.getNodeResponseWithAllFields(), "The response contains the updated node.");
-		fieldMetadataExtraction.exampleResponse(NOT_FOUND, miscExamples.createMessageResponse(), "The node or the field could not be found.");
-		fieldMetadataExtraction.description("Update the binaryfield with the given name.");
-		fieldMetadataExtraction.events(NODE_UPDATED);
-		fieldMetadataExtraction.blockingHandler(rc -> {
-			String uuid = rc.request().getParam("nodeUuid");
-			String fieldName = rc.request().getParam("fieldName");
-			MultiMap attributes = rc.request().formAttributes();
-			InternalActionContext ac = wrap(rc);
-			s3BinaryMetadataExtractionHandler.handleMetadataExtraction(ac, uuid, fieldName, attributes);
-		});
 	}
 
 	private void addMoveHandler() {
