850ffcf0f1f03c72e61a03e3ad3a018c19f023ab
Serhii Plyhun <snuk182@live.com>
2021-05-06 17:19:52 +0200

    All unit tests moved to the shared projects

--- tests/orientdb-runner/src/test/java/com/gentics/mesh/core/OrientDBMeshCoreTestsRunner.java
+++
@@ -1,9 +0,0 @@
-package com.gentics.mesh.core;
-
-import org.junit.extensions.cpsuite.ClasspathSuite;
-import org.junit.runner.RunWith;
-
-//@RunWith(ClasspathSuite.class)
-public class OrientDBMeshCoreTestsRunner {
-
-}
