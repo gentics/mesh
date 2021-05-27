ace39dffc72e5f1413374ce9dd8dd69abf517925
Norbert Pomaroli <n.pomaroli@gentics.com>
2021-05-27 09:27:23 +0200

    Implement topology change readonly behaviour (#1206)
    * Implement topology change readonly behaviour
    
    * Add Javadoc
    
    * Add test and changelog entry
    
    Co-authored-by: Serhii Plyhun II <snuk182@live.com>

--- mdm/orientdb-api/src/main/java/com/gentics/mesh/core/verticle/handler/WriteLockImpl.java
+++ mdm/orientdb-api/src/main/java/com/gentics/mesh/core/verticle/handler/WriteLockImpl.java
@@ -1,7 +1,9 @@
 package com.gentics.mesh.core.verticle.handler;
 
+import static com.gentics.mesh.core.rest.error.Errors.error;
 import static com.gentics.mesh.metric.SimpleMetric.WRITE_LOCK_TIMEOUT_COUNT;
 import static com.gentics.mesh.metric.SimpleMetric.WRITE_LOCK_WAITING_TIME;
+import static io.netty.handler.codec.http.HttpResponseStatus.SERVICE_UNAVAILABLE;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +13,7 @@ import javax.inject.Singleton;
 
 import com.gentics.mesh.context.InternalActionContext;
 import com.gentics.mesh.etc.config.MeshOptions;
+import com.gentics.mesh.graphdb.cluster.ClusterManager;
 import com.gentics.mesh.metric.MetricsService;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ILock;
@@ -32,14 +35,16 @@ public class WriteLockImpl implements WriteLock {
 	private final boolean isClustered;
 	private final Timer writeLockTimer;
 	private final Counter timeoutCount;
+	private final ClusterManager clusterManager;
 
 	@Inject
-	public WriteLockImpl(MeshOptions options, Lazy<HazelcastInstance> hazelcast, MetricsService metricsService) {
+	public WriteLockImpl(MeshOptions options, Lazy<HazelcastInstance> hazelcast, MetricsService metricsService, ClusterManager clusterManager) {
 		this.options = options;
 		this.hazelcast = hazelcast;
 		this.isClustered = options.getClusterOptions().isEnabled();
 		this.writeLockTimer = metricsService.timer(WRITE_LOCK_WAITING_TIME);
 		this.timeoutCount = metricsService.counter(WRITE_LOCK_TIMEOUT_COUNT);
+		this.clusterManager = clusterManager;
 	}
 
 	@Override
@@ -61,6 +66,12 @@ public class WriteLockImpl implements WriteLock {
 		if (ac != null && ac.isSkipWriteLock()) {
 			return this;
 		} else {
+			// throw an error, if the cluster topology is currently locked and the option "topology change readonly" is activated
+			if (options.getClusterOptions().isTopologyChangeReadOnly() && clusterManager != null
+					&& clusterManager.isClusterTopologyLocked()) {
+				throw error(SERVICE_UNAVAILABLE, "error_cluster_topology_readonly").setLogStackTrace(false);
+			}
+
 			boolean syncWrites = options.getStorageOptions().isSynchronizeWrites();
 			if (syncWrites) {
 				Timer.Sample timer = Timer.start();
