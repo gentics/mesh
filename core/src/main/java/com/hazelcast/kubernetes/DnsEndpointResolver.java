e65ffa6bf738e802b3344d771af2b3bfc624586b
Norbert Pomaroli <n.pomaroli@gentics.com>
2021-06-11 09:48:29 +0200

    Add Note about modification

--- core/src/main/java/com/hazelcast/kubernetes/DnsEndpointResolver.java
+++ core/src/main/java/com/hazelcast/kubernetes/DnsEndpointResolver.java
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * 
+ * NOTE: this class was originally taken from https://github.com/hazelcast/hazelcast-kubernetes and modified in order to fix the DNS Resolving issues
+ * we noticed in OpenShift
  */
 
 package com.hazelcast.kubernetes;
