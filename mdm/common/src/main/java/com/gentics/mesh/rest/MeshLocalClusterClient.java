405973622639171142d04c25ada93904ea1d0182
Serhii Plyhun II <snuk182@live.com>
2021-11-03 11:53:00 +0100

    Leftovers: javadoc

--- mdm/common/src/main/java/com/gentics/mesh/rest/MeshLocalClusterClient.java
+++ mdm/common/src/main/java/com/gentics/mesh/rest/MeshLocalClusterClient.java
@@ -2,6 +2,12 @@ package com.gentics.mesh.rest;
 
 import com.gentics.mesh.rest.client.method.ClusterAdminClientMethods;
 
+/**
+ * A cluster functions-extended {@link MeshLocalClient}.
+ * 
+ * @author plyhun
+ *
+ */
 public interface MeshLocalClusterClient extends MeshLocalClient, ClusterAdminClientMethods {
 
 }
