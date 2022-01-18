3a09ee1e6afa21d7714c7b69ba89e62eb63524d2
Umberto D'Ovidio <u.dovidio@gentics.com>
2022-01-18 14:50:37 +0100

    remove unused interfaces

--- mdm/orientdb-wrapper/src/main/java/com/gentics/mesh/core/data/job/impl/BranchMigrationJobImpl.java
+++ mdm/orientdb-wrapper/src/main/java/com/gentics/mesh/core/data/job/impl/BranchMigrationJobImpl.java
@@ -3,12 +3,12 @@ package com.gentics.mesh.core.data.job.impl;
 import com.gentics.madl.index.IndexHandler;
 import com.gentics.madl.type.TypeHandler;
 import com.gentics.mesh.core.data.generic.MeshVertexImpl;
-import com.gentics.mesh.core.data.job.BranchMigrationJob;
+import com.gentics.mesh.core.data.job.JobCore;
 
 /**
  * Job implementation to be used for persisting and invoking branch migrations.
  */
-public class BranchMigrationJobImpl extends JobImpl implements BranchMigrationJob {
+public class BranchMigrationJobImpl extends JobImpl implements JobCore {
 
 	/**
 	 * Initialize the vertex type and index.
