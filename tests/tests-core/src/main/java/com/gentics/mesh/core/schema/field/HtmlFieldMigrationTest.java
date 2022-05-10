d0a362f75c1b665a1efa5f587627325c5062003d
Serhii Plyhun <S.Plyhun@gentics.com>
2022-05-10 14:39:36 +0200

    Fix for type migration + tests

--- tests/tests-core/src/main/java/com/gentics/mesh/core/schema/field/HtmlFieldMigrationTest.java
+++ tests/tests-core/src/main/java/com/gentics/mesh/core/schema/field/HtmlFieldMigrationTest.java
@@ -25,7 +25,6 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
-import java.util.stream.Collectors;
 
 import org.junit.Test;
 
