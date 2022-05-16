4b6d6d1e4058d1e70920cfe795d8a96376389ed1
Serhii Plyhun II <snuk182@live.com>
2022-05-16 14:07:49 +0200

    Test leftovers (#1373)
    * DB test fixes
    
    * Deprecate the dup UUID test
    
    * Complex Provider class structure allowed
    
    * Initialization fixes. Test fixes.
    
    * Cleanup
    
    * Test fixes
    
    * Generalize the cache usage pt.1
    
    * Test fixes
    
    * Test fixes
    
    * Test fixes
    
    * Test fixes
    
    * Node field test fixes
    
    * Test fixes: content tables, binaries
    
    * Test fixes
    
    * Test fixes
    
    * Tests fix: create tables in time
    
    * Metrics resolution fixed. Test fixes.
    
    * Number list test fix
    
    * Move entity caches from core to ODB wrapper
    
    * Extract binary upload load test into parameterized
    
    * Tests fixes
    
    * Generalize local write lock
    
    * Add cluster processing to the write lock
    
    * Plugin tests work for all server impls
    
    * Test fixes for field transformation / resources
    
    * Plugin tests fix pt.2
    
    * Last tests fix
    
    * Number list test fixes
    
    * Fix for type migration + tests
    
    * ES migration fix
    
    * Dedicated exception for no test context
    
    * More complex languages test
    
    * Cleanup 3
    
    * Minor test fix
    
    * Revert "Cleanup 3"
    
    This reverts commit 932e6bd51cea617dd49aa98bacbc993f87dcd3fd.
    
    * Minor fixes
    
    * Minor test fix
    
    Co-authored-by: Serhii Plyhun <S.Plyhun@gentics.com>

--- tests/tests-api/src/main/java/com/gentics/mesh/util/UUIDUtilTest.java
+++ tests/tests-api/src/main/java/com/gentics/mesh/util/UUIDUtilTest.java
@@ -1,6 +1,7 @@
 package com.gentics.mesh.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.UUID;
 
@@ -12,8 +13,13 @@ public class UUIDUtilTest {
 	public void testToFromBytes() {
 		UUID uuid = UUIDUtil.toJavaUuid(UUIDUtil.randomUUID());
 		byte[] bytes = UUIDUtil.toBytes(uuid);
+		assertNotNull(bytes);
 		UUID copyUUid = UUIDUtil.toJavaUuid(bytes);
 		assertEquals(uuid, copyUUid);
 	}
 
+	@Test
+	public void testUuidGenerator() {
+		assertNotNull(UUIDUtil.randomUUID().toUpperCase());
+	}
 }
