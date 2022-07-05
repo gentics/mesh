2784328633a71443bc5452d1de290823b141d9a9
Serhii Plyhun <S.Plyhun@gentics.com>
2022-07-05 14:02:26 +0200

    Revert "Add Javadoc"
    This reverts commit a896fc0c574a038c5f1276b3974690e9b5a4afee.

--- tests/api/src/main/java/com/gentics/mesh/test/MeshProviderOrder.java
+++ tests/api/src/main/java/com/gentics/mesh/test/MeshProviderOrder.java
@@ -6,16 +6,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-/**
- * Annotation for adding an order to instances of {@link MeshOptionsProvider}, so that that automatic
- * selection of the implementation can be controlled
- */
 @Retention(RUNTIME)
 @Target(TYPE)
 public @interface MeshProviderOrder {
-	/**
-	 * Order of the implementation (smaller numbers take precedence over higher numbers)
-	 * @return order
-	 */
 	int value();
 }
