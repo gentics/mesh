b46c1fa52725457f185a26f391783cd135660c56
Umberto D'Ovidio <u.dovidio@gentics.com>
2022-10-03 11:33:03 +0200

    fix compile issue, add test for clustered webroot cache initialization

---
+++ core/src/test/java/com/gentics/mesh/core/webroot/WebRootClusteredEndpointTest.java
@@ -0,0 +1,39 @@
+package com.gentics.mesh.core.webroot;
+
+import com.gentics.mesh.FieldUtil;
+import com.gentics.mesh.core.rest.node.NodeCreateRequest;
+import com.gentics.mesh.core.rest.node.NodeResponse;
+import com.gentics.mesh.test.context.AbstractMeshTest;
+import com.gentics.mesh.test.context.MeshTestSetting;
+import org.junit.Test;
+
+import static com.gentics.mesh.test.ClientHelper.call;
+import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
+import static com.gentics.mesh.test.TestSize.FULL;
+import static org.junit.Assert.assertEquals;
+
+@MeshTestSetting(testSize = FULL, startServer = true, clusterMode = true)
+public class WebRootClusteredEndpointTest extends AbstractMeshTest {
+
+    @Test
+    public void testNodeCreateUpdate() {
+        NodeCreateRequest nodeCreateRequest = new NodeCreateRequest();
+        nodeCreateRequest.setLanguage("en");
+        nodeCreateRequest.setSchemaName("content");
+        nodeCreateRequest.getFields().put("teaser", FieldUtil.createStringField("some teaser"));
+        nodeCreateRequest.getFields().put("slug", FieldUtil.createStringField("new-page.html"));
+        nodeCreateRequest.getFields().put("content", FieldUtil.createStringField("Blessed mealtime again!"));
+
+        NodeResponse response = call(() -> client().webrootCreate(PROJECT_NAME, "/new-page.html", nodeCreateRequest));
+        assertEquals("0.1", response.getVersion());
+
+        NodeCreateRequest nodeUpdateRequest = new NodeCreateRequest();
+        nodeUpdateRequest.setLanguage("en");
+        nodeUpdateRequest.setSchemaName("content");
+        nodeUpdateRequest.getFields().put("teaser", FieldUtil.createStringField("some teaser"));
+        nodeUpdateRequest.getFields().put("slug", FieldUtil.createStringField("new-page.html"));
+        nodeUpdateRequest.getFields().put("content", FieldUtil.createStringField("Blessed mealtime again 2!"));
+
+        call(() -> client().webrootCreate(PROJECT_NAME, "/new-page.html", nodeUpdateRequest));
+    }
+}
