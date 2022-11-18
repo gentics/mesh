8bc27e26a2026e325ea05b31004a59c6f2bc8889
Patrick Klaffenböck <p.klaffenboeck@gentics.com>
2022-11-18 12:53:55 +0100

    SUP-14417 Remove superfluous error logs for 4xx errors

--- common/src/main/java/com/gentics/mesh/router/route/FailureHandler.java
+++ common/src/main/java/com/gentics/mesh/router/route/FailureHandler.java
@@ -4,10 +4,6 @@ import static com.gentics.mesh.core.rest.error.Errors.error;
 import static com.gentics.mesh.http.HttpConstants.APPLICATION_JSON_UTF8;
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
 
-import java.nio.file.NoSuchFileException;
-import java.util.MissingResourceException;
-import java.util.Optional;
-
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.gentics.mesh.context.InternalActionContext;
 import com.gentics.mesh.context.impl.InternalRoutingActionContextImpl;
@@ -19,12 +15,15 @@ import com.gentics.mesh.error.MeshSchemaException;
 import com.gentics.mesh.json.JsonUtil;
 import com.gentics.mesh.json.MeshJsonException;
 import com.gentics.mesh.monitor.liveness.LivenessManager;
-
 import io.vertx.core.Handler;
 import io.vertx.core.impl.NoStackTraceThrowable;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
+
+import java.nio.file.NoSuchFileException;
+import java.util.MissingResourceException;
+import java.util.Optional;
 
 /**
  * Central failure handler for REST API routes.
@@ -37,7 +36,7 @@ public class FailureHandler implements Handler<RoutingContext> {
 
 	/**
 	 * Create a new failure handler.
-	 * 
+	 *
 	 * @param livenessBean liveness bean
 	 * @return created failure handler
 	 */
@@ -55,7 +54,7 @@ public class FailureHandler implements Handler<RoutingContext> {
 
 	/**
 	 * Return the response status that may be stored within the exception.
-	 * 
+	 *
 	 * @param failure
 	 * @param code
 	 * @return
@@ -132,21 +131,17 @@ public class FailureHandler implements Handler<RoutingContext> {
 			int code = getResponseStatusCode(failure, rc.statusCode());
 			String failureMsg = failure != null ? failure.getMessage() : "-";
 			switch (code) {
+			case 400:
 			case 401:
-				log.error("Unauthorized - Request for path {" + toPath(rc) + "} was not authorized.");
-				break;
 			case 404:
-				log.error("Could not find resource for path {" + toPath(rc) + "}");
+				// No special handling needed, the Vert.x logger handler will
+				// output a single warning line with the status code.
 				break;
 			case 403:
 				ac.getSecurityLogger().info("Non-authorized access for path " + toPath(rc));
-				log.error("Request for request in path: " + toPath(rc) + " is not authorized.");
-				break;
-			case 400:
-				log.error("Bad request in path: " + toPath(rc) + " with message " + failureMsg);
 				break;
 			case 413:
-				log.error("Entity too large to be processed for path: " + toPath(rc));
+				// Payload Too Large will be handled later.
 				rc.next();
 				return;
 			default:
@@ -214,7 +209,7 @@ public class FailureHandler implements Handler<RoutingContext> {
 
 	/**
 	 * Try to translate the nested i18n message key.
-	 * 
+	 *
 	 * @param error
 	 * @param rc
 	 */
