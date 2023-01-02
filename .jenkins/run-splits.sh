8366203390545bc24d874013f134cbd8931e4be0
Norbert Pomaroli <n.pomaroli@gentics.com>
2023-01-02 14:38:58 +0100

    Restrict to other test

--- .jenkins/run-splits.sh
+++ .jenkins/run-splits.sh
@@ -5,7 +5,8 @@ cd $BASEDIR/..
 echo "Using includes: $1"
 tests=$(paste -sd "," $1 | sed 's/\.java//g' | sed 's/\//./g')
 
-tests=com.gentics.mesh.core.monitoring.MetricsLabelTest
+# tests=com.gentics.mesh.core.monitoring.MetricsLabelTest
+tests=com.gentics.mesh.core.data.fieldhandler.schema.SchemaComparatorSchemaTest
 
 if [ -z "$tests" ] ; then
   echo "Did not collect valid test set. Set was empty."
