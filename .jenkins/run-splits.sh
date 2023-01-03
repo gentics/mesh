2148faec2c51a9f8ed42638e4e32c278f07136b9
Norbert Pomaroli <n.pomaroli@gentics.com>
2023-01-03 09:35:29 +0100

    Add sleep after test execution

--- .jenkins/run-splits.sh
+++ .jenkins/run-splits.sh
@@ -16,3 +16,6 @@ echo "Running tests: $tests"
 jacoco=$2
 echo "Using jacoco: $jacoco"
 time mvn -fae -Dsurefire.excludedGroups=com.gentics.mesh.test.category.FailingTests -Dmaven.javadoc.skip=true -Dskip.cluster.tests=true -Dmaven.test.failure.ignore=true -B -e -pl '!demo,!doc,!performance-tests' test -Dtest=$tests -DfailIfNoTests=false -Djacoco.skip=$jacoco
+
+echo "Going to sleep now"
+sleep 3600
