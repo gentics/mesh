b69dd939f6f5ef0516a11be61749f62ea5334f24
Serhii Plyhun <S.Plyhun@gentics.com>
2023-02-01 11:12:01 +0100

    MR fixes

--- test-common/src/main/java/com/gentics/mesh/test/docker/MeshContainer.java
+++ test-common/src/main/java/com/gentics/mesh/test/docker/MeshContainer.java
@@ -241,19 +241,14 @@ public class MeshContainer extends GenericContainer<MeshContainer> {
 
 		String javaOpts = "-Dmemory.directMemory.preallocate=false ";
 		if (debugPort != null) {
-			javaOpts = "-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n ";
+			javaOpts += "-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n ";
 			exposedPorts.add(8000);
 			setPortBindings(Arrays.asList("8000:8000"));
 		}
 		if (extraOpts != null) {
-			if (javaOpts == null) {
-				javaOpts = "";
-			}
 			javaOpts += extraOpts + " ";
 		}
-		if (javaOpts != null) {
-			addEnv("JAVAOPTS", javaOpts);
-		}
+		addEnv("JAVAOPTS", javaOpts);
 
 		exposedPorts.add(8600);
 		exposedPorts.add(8080);
@@ -686,17 +681,13 @@ public class MeshContainer extends GenericContainer<MeshContainer> {
 	}
 
 	@Override
-	public String getContainerIpAddress() {
+	public String getHost() {
 		String containerHost = System.getenv("CONTAINER_HOST");
 		if (containerHost != null) {
 			return containerHost;
 		} else {
-			return DockerClientFactory.instance().dockerHostIpAddress();
+			return super.getHost();
 		}
-	}
-
-	public String getHost() {
-		return getContainerIpAddress();
 	}
 
 	public int getPort() {
