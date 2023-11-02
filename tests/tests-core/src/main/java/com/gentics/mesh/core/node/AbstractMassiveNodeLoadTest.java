e90c7812e2f5608ec5a8e06f119d912b85acac49
Serhii Plyhun <S.Plyhun@gentics.com>
2023-11-02 17:59:16 +0100

    Better test environment

--- tests/tests-core/src/main/java/com/gentics/mesh/core/node/AbstractMassiveNodeLoadTest.java
+++ tests/tests-core/src/main/java/com/gentics/mesh/core/node/AbstractMassiveNodeLoadTest.java
@@ -4,6 +4,7 @@ import static com.gentics.mesh.test.ClientHelper.call;
 import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 
 import com.gentics.mesh.FieldUtil;
@@ -13,15 +14,17 @@ import com.gentics.mesh.core.rest.node.FieldMapImpl;
 import com.gentics.mesh.core.rest.node.NodeCreateRequest;
 import com.gentics.mesh.core.rest.node.NodeListResponse;
 import com.gentics.mesh.core.rest.schema.impl.SchemaReferenceImpl;
+import com.gentics.mesh.parameter.impl.DeleteParametersImpl;
 import com.gentics.mesh.parameter.impl.VersioningParametersImpl;
 import com.gentics.mesh.test.context.AbstractMeshTest;
 
 public abstract class AbstractMassiveNodeLoadTest extends AbstractMeshTest {
 
 	protected final long numOfNodesPerLevel;
+	protected String parentFolderUuid = null;
 
 	public AbstractMassiveNodeLoadTest() {
-		this(9000);
+		this(5000);
 	}
 
 	public AbstractMassiveNodeLoadTest(long numOfNodesPerLevel) {
@@ -33,10 +36,31 @@ public abstract class AbstractMassiveNodeLoadTest extends AbstractMeshTest {
 		makeEmAll(numOfNodesPerLevel, tx(() -> project().getBaseNode().getUuid()));
 	}
 
+	@After
+	public void nukeEmAll() {
+		if (parentFolderUuid != null) {
+			call(() -> client().deleteNode(PROJECT_NAME, parentFolderUuid, new DeleteParametersImpl().setRecursive(true)));
+		}
+	}
+
 	protected void makeEmAll(long howMany, String parentNodeUuid) {
 		try (Tx tx = tx()) {
-			NodeListResponse nodeList = call(() -> client().findNodeChildren(PROJECT_NAME, parentNodeUuid, new VersioningParametersImpl().draft()));
-			assertEquals(3, nodeList.getData().size());
+			if (parentFolderUuid == null) {
+				SchemaReferenceImpl schemaReference = new SchemaReferenceImpl();
+				schemaReference.setName("folder");
+				NodeCreateRequest create2 = new NodeCreateRequest();
+				FieldMap fields = new FieldMapImpl();
+				fields.put("name", FieldUtil.createStringField("MassiveParentFolder"));
+				fields.put("slug", FieldUtil.createStringField("massiveparentfolder"));
+				create2.setFields(fields);
+				create2.setSchema(schemaReference);
+				create2.setLanguage("en");
+				create2.setParentNodeUuid(parentNodeUuid);
+				parentFolderUuid = call(() -> client().createNode(PROJECT_NAME, create2)).getUuid();
+			}
+			
+			NodeListResponse nodeList = call(() -> client().findNodeChildren(PROJECT_NAME, parentFolderUuid, new VersioningParametersImpl().draft()));
+			assertEquals(0, nodeList.getData().size());
 
 			SchemaReferenceImpl schemaReference = new SchemaReferenceImpl();
 			schemaReference.setName("folder");
@@ -49,7 +73,7 @@ public abstract class AbstractMassiveNodeLoadTest extends AbstractMeshTest {
 				create2.setFields(fields);
 				create2.setSchema(schemaReference);
 				create2.setLanguage("en");
-				create2.setParentNodeUuid(parentNodeUuid);
+				create2.setParentNodeUuid(parentFolderUuid);
 				call(() -> client().createNode(PROJECT_NAME, create2));
 			}
 		}
