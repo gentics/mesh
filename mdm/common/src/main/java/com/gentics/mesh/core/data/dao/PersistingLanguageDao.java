2e6ea5cb752d0c661219fae3b894ac4954b9ded2
Serhii Plyhun <S.Plyhun@gentics.com>
2023-12-29 12:31:21 +0100

    SUP-15643: Fix the  param docu and applications

--- mdm/common/src/main/java/com/gentics/mesh/core/data/dao/PersistingLanguageDao.java
+++ mdm/common/src/main/java/com/gentics/mesh/core/data/dao/PersistingLanguageDao.java
@@ -3,8 +3,12 @@ package com.gentics.mesh.core.data.dao;
 import com.gentics.mesh.context.BulkActionContext;
 import com.gentics.mesh.context.InternalActionContext;
 import com.gentics.mesh.core.data.HibLanguage;
+import com.gentics.mesh.core.db.Tx;
 import com.gentics.mesh.core.rest.lang.LanguageResponse;
+import com.gentics.mesh.core.rest.tag.TagFamilyResponse;
 import com.gentics.mesh.event.EventQueueBatch;
+import com.gentics.mesh.parameter.GenericParameters;
+import com.gentics.mesh.parameter.value.FieldsSet;
 
 /**
  * A persisting extension to {@link LanguageDao}
@@ -26,11 +30,38 @@ public interface PersistingLanguageDao extends LanguageDao, PersistingDaoGlobal<
 	@Override
 	default LanguageResponse transformToRestSync(HibLanguage element, InternalActionContext ac, int level,
 			String... languageTags) {
+		GenericParameters generic = ac.getGenericParameters();
+		FieldsSet fields = generic.getFields();
+
 		LanguageResponse model = new LanguageResponse();
-		model.setUuid(element.getUuid());
-		model.setLanguageTag(element.getLanguageTag());
-		model.setName(element.getName());
-		model.setNativeName(element.getNativeName());
+		// TODO align fields.isEmpty() with other DAOs on a major update - this is breaking at the moment.
+		if (fields.isEmpty() || fields.has("uuid")) {
+			model.setUuid(element.getUuid());
+
+			// Performance shortcut to return now and ignore the other checks
+			if (fields.size() == 1) {
+				return model;
+			}
+		}
+		if (fields.isEmpty() || fields.has("name")) {
+			model.setName(element.getName());
+
+			// Performance shortcut to return now and ignore the other checks
+			if (fields.size() == 1) {
+				return model;
+			}
+		}
+		if (fields.isEmpty() || fields.has("tag")) {
+			model.setLanguageTag(element.getLanguageTag());
+
+			// Performance shortcut to return now and ignore the other checks
+			if (fields.size() == 1) {
+				return model;
+			}
+		}
+		if (fields.isEmpty() || fields.has("nativeName")) {
+			model.setNativeName(element.getNativeName());
+		}		
 		return model;
 	}
 
