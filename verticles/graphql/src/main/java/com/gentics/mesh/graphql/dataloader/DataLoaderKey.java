2b153d2ab17ba04ad6af374adbd387e82efd7b4e
Serhii Plyhun <S.Plyhun@gentics.com>
2024-05-08 15:24:44 +0200

    Javadoc

--- verticles/graphql/src/main/java/com/gentics/mesh/graphql/dataloader/DataLoaderKey.java
+++ verticles/graphql/src/main/java/com/gentics/mesh/graphql/dataloader/DataLoaderKey.java
@@ -18,15 +18,35 @@ public final class DataLoaderKey<T> {
 	private final String path;
 	private final T value;
 
+	/**
+	 * New from raw path and value.
+	 * 
+	 * @param path
+	 * @param value
+	 */
 	public DataLoaderKey(String path, T value) {
 		this.path = path;
 		this.value = value;
 	}
 
+	/**
+	 * New from {@link ResultPath} and value. The input path is generalized by removal all indices, if applicable, 
+	 * e.g. the lookup will be common for all the result entities of the same parent request.
+	 * 
+	 * @param path
+	 * @param value
+	 */
 	public DataLoaderKey(ResultPath path, T value) {
 		this(path.toString().replaceAll("\\[[\\d+]\\]", StringUtils.EMPTY), value);
 	}
 
+	/**
+	 * New from the current @link {@link DataFetchingEnvironment}. The input path is taken from the environment, 
+	 * and generalized by removal all indices, if applicable, e.g. the lookup will be common for all the result entities of the same parent request.
+	 * 
+	 * @param env
+	 * @param value
+	 */
 	public DataLoaderKey(DataFetchingEnvironment env, T value) {
 		this(env.getExecutionStepInfo().getPath(), value);
 	}
