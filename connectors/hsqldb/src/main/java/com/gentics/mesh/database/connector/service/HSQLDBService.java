1b69f28d9a6bf00c8f6dab294f69c4212611d70f
Serhii Plyhun <S.Plyhun@gentics.com>
2024-08-13 13:30:06 +0200

    CI fix pt3

--- connectors/hsqldb/src/main/java/com/gentics/mesh/database/connector/service/HSQLDBService.java
+++ connectors/hsqldb/src/main/java/com/gentics/mesh/database/connector/service/HSQLDBService.java
@@ -2,7 +2,6 @@ package com.gentics.mesh.database.connector.service;
 
 import com.gentics.mesh.database.connector.DatabaseConnector;
 import com.gentics.mesh.database.connector.HSQLDBConnector;
-import com.gentics.mesh.database.connector.service.DatabaseConnectorService;
 import com.gentics.mesh.etc.config.HibernateMeshOptions;
 
 public class HSQLDBService implements DatabaseConnectorService {
