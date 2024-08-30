088e205db360a4278fd8bd2df2526180da9ca7f2
Serhii Plyhun <S.Plyhun@gentics.com>
2024-08-30 15:26:08 +0200

    Docu fixes. Test run fixes.

--- .jenkins/run-splits.sh
+++ .jenkins/run-splits.sh
@@ -17,5 +17,9 @@ nomariadb=true
 if [ "$3" = "mariadb" ]; then
    nomariadb=false
 fi
+nohsqldb=true
+if [ "$3" = "hsqlm" ]; then
+   nohsqldb=false
+fi
 echo "Skipping databases: MariaDB=$nomariadb"
-time mvn -fae -U -Dsurefire.excludedGroups=com.gentics.mesh.test.category.FailingTests,com.gentics.mesh.test.category.ClusterTests -Dmaven.javadoc.skip=true -Dskip.cluster.tests=true -Dmaven.test.failure.ignore=true -Dmesh.container.image.prefix=docker.apa-it.at/ -B -e -pl '!doc,!performance-tests' test -Dtest=$tests -DfailIfNoTests=false -Djacoco.skip=$jacoco -Dskip.mariadb.tests=$nomariadb -Dmesh.testdb.manager.host=localhost -Dmesh.testdb.manager.port=8080 | ts "$4 [%Y-%m-%d %H:%M:%S]"
+time mvn -fae -U -Dsurefire.excludedGroups=com.gentics.mesh.test.category.FailingTests,com.gentics.mesh.test.category.ClusterTests -Dmaven.javadoc.skip=true -Dskip.cluster.tests=true -Dmaven.test.failure.ignore=true -Dmesh.container.image.prefix=docker.apa-it.at/ -B -e -pl '!doc,!performance-tests' test -Dtest=$tests -DfailIfNoTests=false -Djacoco.skip=$jacoco -Dskip.mariadb.tests=$nomariadb -Dskip.hsqlmemory.tests=$nohsqldb -Dmesh.testdb.manager.host=localhost -Dmesh.testdb.manager.port=8080 | ts "$4 [%Y-%m-%d %H:%M:%S]"
