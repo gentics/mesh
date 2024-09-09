c9b932c0db638818ec1c0fc2a163d43b7d25fb79
Norbert Pomaroli <n.pomaroli@gentics.com>
2024-09-09 14:08:01 +0200

    Rename repo to docker-jenkinsbuilds

--- .jenkins/build-deploy-test-env.sh
+++ .jenkins/build-deploy-test-env.sh
@@ -6,5 +6,5 @@ else
   TAG=$1
 fi
 
-docker build -t push.docker.gentics.com/jenkinsbuilds/jenkinsbuilds/mesh-slave:$TAG .
-docker push push.docker.gentics.com/jenkinsbuilds/jenkinsbuilds/mesh-slave:$TAG
+docker build -t push.docker.gentics.com/docker-jenkinsbuilds/jenkinsbuilds/mesh-slave:$TAG .
+docker push push.docker.gentics.com/docker-jenkinsbuilds/jenkinsbuilds/mesh-slave:$TAG
--- .jenkins/build-env-nexus.sh
+++ .jenkins/build-env-nexus.sh
@@ -2,5 +2,5 @@
 
 # This will build the build environment image for nexus by extending the old build environment image and copying the new settings.xml file into it
 
-docker build -f Dockerfile.nexus -t push.docker.gentics.com/jenkinsbuilds/jenkinsbuilds/mesh-slave:java11-nexus .
-docker push push.docker.gentics.com/jenkinsbuilds/jenkinsbuilds/mesh-slave:java11-nexus
+docker build -f Dockerfile.nexus -t push.docker.gentics.com/docker-jenkinsbuilds/jenkinsbuilds/mesh-slave:java11-nexus .
+docker push push.docker.gentics.com/docker-jenkinsbuilds/jenkinsbuilds/mesh-slave:java11-nexus
