9c43e74da64f82ca7051c1c8a70bda1de47cf2bb
Serhii Plyhun <S.Plyhun@gentics.com>
2024-09-20 15:42:59 +0200

    Upload script, untested

--- doc/uploadSnapshotApidocs.sh
+++ doc/uploadSnapshotApidocs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Name: Gentics CMS Guide Upload Script
-# Description: This script will upload the guides to the website
+# Name: Gentics Mesh API docs Upload Script
+# Description: This script will upload the apidocs to the website
 
 function print_usage() {
   echo "Usage: $0"
   echo " Example: $0"
-  echo " This will upload the previous build guides"
+  echo " This will upload the previous build apidocs"
 }
 
 # Generic error check and abort method
@@ -27,22 +27,22 @@ TMPDIR=/tmp
 version=SNAPSHOT
 
 function uploadGuides() {
-  echo -e "\n * Guides"
+  echo -e "\n * API docs"
     echo -e "\n * Cleanup"
-      echo "Deleting: $REMOTE_DEST/guides-history/$version/"
+      echo "Deleting: $REMOTE_DEST/apidocs-history/$version/"
       echo "Press enter to continue"
       read 
-      ssh $REMOTE_HOST "rm -rf $REMOTE_DEST/guides-history/$version/"
+      ssh $REMOTE_HOST "rm -rf $REMOTE_DEST/apidocs-history/$version/"
     echo "Done."
 
-    echo -e "\n * Transfering and extracting guides"
-      ssh $REMOTE_HOST mkdir -p $REMOTE_DEST/guides-history/$version
-      scp target/contentnode-doc*.zip  $REMOTE_HOST:$REMOTE_DEST/guides-history/$version
-      handleError $? "Could not transfer guides $version"
-      ssh $REMOTE_HOST "cd $REMOTE_DEST/guides-history/$version ; unzip -o *.zip"
+    echo -e "\n * Transfering and extracting apidocs"
+      ssh $REMOTE_HOST mkdir -p $REMOTE_DEST/apidocs-history/$version
+      scp target/mesh-doc*.zip  $REMOTE_HOST:$REMOTE_DEST/apidocs-history/$version
+      handleError $? "Could not transfer apidocs $version"
+      ssh $REMOTE_HOST "cd $REMOTE_DEST/apidocs-history/$version ; unzip -o *.zip"
       echo "Creating symlink from latest to $version"
-      ssh $REMOTE_HOST "cd $REMOTE_DEST/guides-history ; unlink latest ; ln -s $version latest"
-      handleError $? "Could not extract guides"
+      ssh $REMOTE_HOST "cd $REMOTE_DEST/apidocs-history ; unlink latest ; ln -s $version latest"
+      handleError $? "Could not extract apidocs"
     echo "Done."
 
   echo "Done."
