1d3bbdc6c145305a05e3c5b3ca0015fe61a3a0cf
Serhii Plyhun <S.Plyhun@gentics.com>
2025-04-23 10:43:26 +0200

    CR fix 3

--- rest-client/src/main/java/com/gentics/mesh/rest/client/impl/MeshRestHttpClientImpl.java
+++ rest-client/src/main/java/com/gentics/mesh/rest/client/impl/MeshRestHttpClientImpl.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
