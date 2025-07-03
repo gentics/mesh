51b892c7c9c061ed17a36b75f651ec7c009c1935
Norbert Pomaroli <n.pomaroli@gentics.com>
2025-07-03 15:12:52 +0200

    Test is valid

--- tests/tests-core/src/main/java/com/gentics/mesh/core/graphql/nativefilter/NativeGraphQLEndpointFilterTest.java
+++ tests/tests-core/src/main/java/com/gentics/mesh/core/graphql/nativefilter/NativeGraphQLEndpointFilterTest.java
@@ -1,6 +1,5 @@
 package com.gentics.mesh.core.graphql.nativefilter;
 
-import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import com.gentics.mesh.core.graphql.GraphQLEndpointFilterTest;
@@ -13,12 +12,4 @@ import com.gentics.mesh.test.category.NativeGraphQLFilterTests;
 @MeshTestSetting(testSize = TestSize.FULL, startServer = true, optionChanger = MeshCoreOptionChanger.GRAPHQL_FORCE_NATIVE_FILTER)
 public class NativeGraphQLEndpointFilterTest extends GraphQLEndpointFilterTest {
 
-	/**
-	 * IsContainer filter is not supported natively, so we expect a NPE at parsing out the results.
-	 */
-	@Test(expected = NullPointerException.class)
-	@Override
-	public void testIsContainerFilter() {
-		super.testIsContainerFilter();
-	}
 }
