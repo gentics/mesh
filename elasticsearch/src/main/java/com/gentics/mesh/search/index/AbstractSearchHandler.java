7e594a69263c3718c9f7138007094eef52d9f22f
Serhii Plyhun <S.Plyhun@gentics.com>
2026-01-08 11:01:45 +0100

    SUP-19450: Fix the HTTP 500 of ES timeout with no body

--- elasticsearch/src/main/java/com/gentics/mesh/search/index/AbstractSearchHandler.java
+++ elasticsearch/src/main/java/com/gentics/mesh/search/index/AbstractSearchHandler.java
@@ -188,10 +188,15 @@ public abstract class AbstractSearchHandler<T extends HibCoreElement<RM>, RM ext
 				log.error("Error at: " + error.toString());
 				log.error("Search query failed", error);
 				try {
-					ac.send(he.getBody(), HttpResponseStatus.BAD_REQUEST);
+					String body = he.getBody();
+					if (StringUtils.isNotBlank(body)) {
+						ac.send(he.getBody(), HttpResponseStatus.BAD_REQUEST);
+					} else {
+						ac.send(HttpResponseStatus.BAD_REQUEST);
+					}
 				} catch (Exception e1) {
 					log.error("Error while converting es error to response", e1);
-					throw error(INTERNAL_SERVER_ERROR, "Error while converting error.", e1);
+					ac.fail(error(INTERNAL_SERVER_ERROR, "Error while converting error.", e1));
 				}
 			} else if (error instanceof TimeoutException) {
 				ac.fail(error(INTERNAL_SERVER_ERROR, "search_error_timeout"));
