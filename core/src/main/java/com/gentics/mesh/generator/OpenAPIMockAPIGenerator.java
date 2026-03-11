9019a8b012b5bae72f3324caedeb4d1476018a7e
Serhii Plyhun <S.Plyhun@gentics.com>
2026-03-11 11:53:09 +0100

    Update usages of OpenAPI generator

--- core/src/main/java/com/gentics/mesh/generator/OpenAPIMockAPIGenerator.java
+++ core/src/main/java/com/gentics/mesh/generator/OpenAPIMockAPIGenerator.java
@@ -44,17 +44,14 @@ public class OpenAPIMockAPIGenerator extends AbstractEndpointGenerator<OpenAPIv3
 	}
 
 	public OpenAPIMockAPIGenerator(File outputFolder, String fileName, boolean cleanup) throws IOException {
-		super(new File(outputFolder, "api"), false);
+		super(new File(outputFolder, "api"), cleanup);
 		this.fileName = fileName;
 		this.generator = new MeshOpenAPIv3Generator(MeshVersion.getPlainVersion(), Collections.emptyList(),
 				Optional.empty(), Optional.empty());
 	}
 
 	public OpenAPIMockAPIGenerator(File outputFolder, String fileName) throws IOException {
-		super(new File(outputFolder, "api"), false);
-		this.fileName = fileName;
-		this.generator = new MeshOpenAPIv3Generator(MeshVersion.getPlainVersion(), Collections.emptyList(),
-				Optional.empty(), Optional.empty());
+		this(outputFolder, fileName, false);
 	}
 
 	public String generate(String format) throws IOException, OpenAPIGenerationException {
