10ee0bca06d9067bbf06427a9bc36a221c1966a8
Serhii Plyhun <S.Plyhun@gentics.com>
2026-04-15 19:21:51 +0200

    Revert limit violation test

--- tests/tests-hibernate/src/main/java/com/gentics/mesh/database/DatabaseLimitsViolatedTest.java
+++ tests/tests-hibernate/src/main/java/com/gentics/mesh/database/DatabaseLimitsViolatedTest.java
@@ -1,38 +1,29 @@
 package com.gentics.mesh.database;
 
 import org.junit.Test;
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.experimental.categories.Category;
 
 import com.gentics.mesh.etc.config.HibernateMeshOptions;
 import com.gentics.mesh.etc.config.MeshOptions;
 import com.gentics.mesh.test.MeshOptionChanger;
 import com.gentics.mesh.test.MeshTestSetting;
 import com.gentics.mesh.test.TestSize;
+import com.gentics.mesh.test.category.FailingTests;
 
 @MeshTestSetting(testSize = TestSize.PROJECT, customOptionChanger = DatabaseLimitsViolatedTest.class)
-// Jenkins behaves oddly with this condition set on a whole class
-//@EnabledIfSystemProperty(named = DatabaseLimitsViolatedTest.ENV_SQL_LIMITS_MAY_VIOLATE, matches = "true", disabledReason = "The connected database is not prone to the SQL parameters limits violation.")
+@Category(FailingTests.class)
 public class DatabaseLimitsViolatedTest extends DatabaseLimitsTest implements MeshOptionChanger {
-
-	public static final String ENV_SQL_LIMITS_MAY_VIOLATE = "sqlLimitsMayViolate";
 
 	@Override
 	@Test(expected = RuntimeException.class)
-	@EnabledIfSystemProperty(named = DatabaseLimitsViolatedTest.ENV_SQL_LIMITS_MAY_VIOLATE, matches = "true", disabledReason = "The connected database is not prone to the SQL parameters limits violation.")
 	public void testParamsOverflowLimit() {
 		super.testParamsOverflowLimit();
 	}
 
 	@Override
 	@Test(expected = RuntimeException.class)
-	@EnabledIfSystemProperty(named = DatabaseLimitsViolatedTest.ENV_SQL_LIMITS_MAY_VIOLATE, matches = "true", disabledReason = "The connected database is not prone to the SQL parameters limits violation.")
 	public void testParamsFitLimit() {
 		super.testParamsFitLimit();
-	}
-
-	@Test
-	public void testJustToPleaseCi() {
-		System.out.println("DEar CI, please pass this test");
 	}
 
 	@Override
