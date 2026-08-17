b83e8203677c95a962e0f1b0f6f665ff31d8902f
Serhii Plyhun <S.Plyhun@gentics.com>
2026-08-17 18:11:05 +0200

    Cleanup

--- mdm/hibernate-core/src/main/java/com/gentics/mesh/hibernate/data/dao/NodeDaoImpl.java
+++ mdm/hibernate-core/src/main/java/com/gentics/mesh/hibernate/data/dao/NodeDaoImpl.java
@@ -1240,7 +1240,7 @@ public class NodeDaoImpl extends AbstractHibRootDao<HibNode, NodeResponse, HibNo
 					.setParameter("schemaVersion", schemaVersion)
 					.getResultList();		
 			List<HibNodeFieldContainerImpl> schemaContent = contentStorage.findMany(edges);
-		Map<UUID, HibNodeFieldContainer> contentByNode = schemaContent.stream()
+			Map<UUID, HibNodeFieldContainer> contentByNode = schemaContent.stream()
 					.collect(collectingByNodeWithLanguageFallback(languageTags));
 
 			return contentByNode.values().stream().map(c -> new NodeContent(c.getNode(), c, languageTags, type));
